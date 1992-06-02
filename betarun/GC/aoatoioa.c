6bf951ee6c9ec720884ebc0856f3ee1fa7002a1c
beta <beta@daimi.au.dk>
1992-06-02 11:27:45 +0000

    added AOAtoIOAReAlloc
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@74 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/GC/aoatoioa.c
+++ betarun/GC/aoatoioa.c
@@ -1,9 +1,67 @@
 /*
  * BETA RUNTIME SYSTEM, Copyright (C) 1991 Mjolner Informatics Aps.
- * Mod: $RCSfile: aoatoioa.c,v $, rel: %R%, date: $Date: 1992-03-03 14:02:26 $, SID: $Revision: 1.3 $
+ * Mod: aoatoioa.c, rel: 1, date: 2/6/91, SID: 1.2
  * by Lars Bak
  */
 #include "beta.h"
+
+/* Some primes to determine the size of the AOAtoIOAtable. */
+/* primes(n+1) ~~ primes(n) * 1.5                          */
+static int primes[] = { 5879,  8821, 13241, 19867, 29803, 44711, 0 };
+
+ref(Block) newBlock(); /* Extern routine in block.c */
+
+static int prim_index = 0;
+/* Allocates the initial AOAtoIOAtable. */
+int AOAtoIOAAlloc()
+{
+  AOAtoIOAtableSize = primes[0];
+  if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
+    AOAtoIOAtable->top = AOAtoIOAtable->limit;
+    AOAtoIOAClear();
+    INFO_AOA( fprintf( output, "#(AOA: AOAtoIOAtable allocated %d longs.)\n",
+		      AOAtoIOAtableSize));
+    return 1;
+  } else
+    return 0;
+}
+
+/* Allocate a larger AOAtoIOAtable based on the next entry in primes. */
+AOAtoIOAReAlloc()
+{
+  /* Save the old table. */
+  ref(Block) oldBlock      = AOAtoIOAtable;
+  int        oldBlockSize  = AOAtoIOAtableSize;
+
+  /* Exit if we can't find a new entry in prims. */
+  if( primes[++prim_index] == 0 ) BetaError(-33, 0);
+
+  /* Allocate a new and larger block to hold AOAtoIOA references. */
+  AOAtoIOAtableSize = primes[prim_index];
+  if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
+    AOAtoIOAtable->top = AOAtoIOAtable->limit;
+    AOAtoIOAClear();
+    INFO_AOA( fprintf( output, "#(AOAtoIOAtable resized %d longs.)\n",
+		      AOAtoIOAtableSize));
+  } else {
+    /* If the allocation of the new AOAtoIOAtable failed please
+       terminate the program execution. */
+    BetaError( -36, 0);
+  }
+    
+  /* Move all entries from the old table into to new. */
+  { int i, counter = 0;  ptr(long) pointer = BlockStart( oldBlock);
+    for(i=0; i < oldBlockSize; i++){
+      if( *pointer ) AOAtoIOAInsert( *pointer );
+      pointer++;
+    }
+  }
+
+  /* Deallocate the old table. */
+  /* NOTE: Jeg er ikke sikker paa at dette er den rigtige maade at
+           deallokere en blok paa. */
+  free( oldBlock);    
+}
 
 AOAtoIOAInsert( theCell)
   handle( Object) theCell;
@@ -29,7 +87,8 @@ AOAtoIOAInsert( theCell)
     if( table[index] == 0 ){ table[index] = (long) theCell; return; }
     if( table[index] == (long) theCell ) return;
   }
-  BetaError(-33,0);
+  AOAtoIOAReAlloc();
+  AOAtoIOAInsert( theCell);
 }
 
 AOAtoIOAClear()
@@ -51,7 +110,7 @@ AOAtoIOAReport()
 { int used = 0;
   if( AOAtoIOAtable ){
     MACRO_ScanBlock( AOAtoIOAtable, if( *thisCell) used++ );
-    fprintf( output, "#AOAtoIOATable size=%d filled=%d%%\n",
+    printf( output, "#AOAtoIOATable size=%d filled=%d%%\n",
 	   AOAtoIOAtableSize, (100*used)/AOAtoIOAtableSize);
   }
 }
