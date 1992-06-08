336510b74ad4f9940a6f256d4509ee5883c7e217
beta <beta@daimi.au.dk>
1992-06-08 23:54:01 +0000

    Initial revision
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@112 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/CRUN/CheckReferenceAssignment.c
@@ -0,0 +1,22 @@
+/*
+ * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
+ * Mod: $RCSfile: CheckReferenceAssignment.c,v $, rel: %R%, date: $Date: 1992-06-08 23:54:01 $, SID: $Revision: 1.1 $
+ * by Peter Andersen and Tommy Thorn.
+ */
+
+#include "beta.h"
+#include "crun.h"
+
+void CheckReferenceAssignment(handle(Object) theObjHandle)
+{
+  /* The assignment *theObjHandle = theObj has just been
+   * done. We know the theObjHandle is in AOA, now check if
+   * *theObjHandle is in IOA.
+   */
+   
+  if (!inIOA(*theObjHandle))
+    return; /* It wasn't. */
+
+  /* Remember this target cell. */
+  AOAtoIOAInsert(theObjHandle);
+}
