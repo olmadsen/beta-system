586f7ba331be7d3b1907dcd5f39c683fef5f739f
beta <beta@daimi.au.dk>
1992-06-08 15:17:24 +0000

    changes unit back to LONGS. ByteReps are allocated as multiple of 4 bytes
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@107 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/GC/objectsize.c
+++ betarun/GC/objectsize.c
@@ -1,11 +1,11 @@
 /*
  * BETA RUNTIME SYSTEM, Copyright (C) 1991 Mjolner Informatics Aps.
- * Mod: $RCSfile: objectsize.c,v $, rel: %R%, date: $Date: 1992-06-04 14:28:09 $, SID: $Revision: 1.6 $
+ * Mod: $RCSfile: objectsize.c,v $, rel: %R%, date: $Date: 1992-06-08 15:17:24 $, SID: $Revision: 1.7 $
  * by Lars Bak
  */
 #include "beta.h"
 
-/* ObjectSize returns the size of an object in number of BYTES (used to be longs). */
+/* ObjectSize returns the size of an object in number of LONGS. */
 long ObjectSize(theObj)
   ref(Object) theObj;
 { 
@@ -14,22 +14,22 @@ long ObjectSize(theObj)
   if( (long) theProto < 0 ){
     switch((long) theProto){
     case ByteRepPTValue:
-      /* BodySize= ((Range+1+3)/4)*4: One byte extra for \0 in strings and multiple of 4 */
-      return headsize(ValRep) + (((toValRep(theObj)->HighBorder+4) >> 2) << 2);
+      /* BodySize= ((Range+1+3)/4): One byte extra for \0 in strings and multiple of 4 */
+      return (headsize(ValRep) + (toValRep(theObj)->HighBorder+4)) >> 2;
     case ValRepPTValue:
-      return headsize(ValRep) + toValRep(theObj)->HighBorder*4;
+      return (headsize(ValRep)>>2) + toValRep(theObj)->HighBorder;
     case RefRepPTValue:
-      return headsize(RefRep) + toRefRep(theObj)->HighBorder*4;
+      return (headsize(RefRep)>>2) + toRefRep(theObj)->HighBorder;
     case ComponentPTValue:
-      return headsize(Component)
-	+ (ComponentItem(theObj)->Proto->Size + 1)*4;
+      return (headsize(Component)>>2)
+	+ (ComponentItem(theObj)->Proto->Size + 1);
     case StackObjectPTValue:
-      return headsize(StackObject)
-	+ toStackObject(theObj)->ObjectSize*4;
+      return (headsize(StackObject)>>2)
+	+ toStackObject(theObj)->ObjectSize;
     case StructurePTValue:
-      return headsize(Structure);
+      return (headsize(Structure)>>2);
     }
   }else
     /* This is an item, so find the size in the protoType. */
-    return (theProto->Size + 1)*4;
+    return (theProto->Size + 1);
 }
