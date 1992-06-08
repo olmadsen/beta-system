c31c544b572467ac4586cd50d62e2fcbe813330b
beta <beta@daimi.au.dk>
1992-06-08 23:54:46 +0000

    moved CheckReferenceAssignment to CheckReferenceAssignment.c
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@113 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Misc.c
+++ betarun/CRUN/Misc.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-06-06 03:57:34 $, SID: $Revision: 1.2 $
+ * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-06-08 23:54:46 $, SID: $Revision: 1.3 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -8,28 +8,6 @@
 #include "crun.h"
 
 asmlabel(Return, "retl; nop");
-
-void CheckReferenceAssignment(handle(Object) theObjHandle)
-{
-  /* The assignment *theObjHandle = theObj has just been
-   * done. We know the theObjHandle is in AOA, now check if
-   * *theObjHandle is in IOA.
-   */
-   
-  if ((long *) *theObjHandle < IOA || (long *) *theObjHandle >= IOATop)
-    return; /* It wasn't. */
-
-  /* Remember this target cell. */
-  AOAtoIOAInsert(theObjHandle);
-
-/*    fprintf(stderr, "CheckReferenceAssignment: Unfinished code, doesn't work\n");
-      See also AOAtoIOAInsert in aoatoioa.c, which does most of the work!
-      I don't know why it isn't used in CheckReferenceAssignment.run, i'm trying to
-      find out ...
-      / peter
- */
-  return;
-}
 
 void ReferenceIsNone(ref(Object) theObj)
 {
