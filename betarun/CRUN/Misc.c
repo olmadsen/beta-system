ffd7205ddcefb9fb66375cf69b321554f2b48078
beta <beta@daimi.au.dk>
1992-06-11 17:14:13 +0000

    moved SetArgsVal from Structure.c
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@125 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Misc.c
+++ betarun/CRUN/Misc.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-06-08 23:54:46 $, SID: $Revision: 1.3 $
+ * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-06-11 17:14:13 $, SID: $Revision: 1.4 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -14,3 +14,12 @@ void ReferenceIsNone(ref(Object) theObj)
     BetaError(-1, theObj);
 }
 
+/* Need to do this in assembler, as the arguments to
+   my caller normally isn't accesseable */
+asmlabel(SetArgValues, "
+	set _ArgCount, %g1
+	st %i0, [%g1]
+	set _ArgVector, %g1
+	retl
+	st %i1, [%g1]
+");
