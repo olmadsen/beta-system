b384942b91c1bd3261351f45eac743af2164e899
beta <beta@daimi.au.dk>
1992-06-12 19:01:16 +0000

    more of the missing code
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@149 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Structure.c
+++ betarun/CRUN/Structure.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: Structure.c,v $, rel: %R%, date: $Date: 1992-06-12 14:58:17 $, SID: $Revision: 1.5 $
+ * Mod: $RCSfile: Structure.c,v $, rel: %R%, date: $Date: 1992-06-12 19:01:16 $, SID: $Revision: 1.6 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -18,7 +18,7 @@ ref(Structure) AllocateStruc(ref(ProtoType) proto,
     newStruct = cast(Structure) IOAalloc(headsize(Structure));
     newStruct->Proto = StructurePTValue;
     newStruct->GCAttr = 1;
-    newStruct->iOrigin = origin; /* er dette rigtigt ?? */
+    newStruct->iOrigin = origin;
     newStruct->iProto = proto;
     return newStruct;
 }
@@ -48,22 +48,17 @@ ref(Structure) ThisStruc(ref(Object) this)
     return newStruct;
 }
 
-
-/* params ?? */
 ref(Item)
 AllocateStrucItem(ref(Structure) theStruct)
 {
-    return AllocateItem(theStruct->iProto, cast(Object) theStruct->iOrigin);
+  return AllocateItem(theStruct->iProto, cast(Object) theStruct->iOrigin);
 }
 
-
-/* params ?? */
 ref(Component)
 AllocateStrucComponent(ref(Structure) theStruct)
 {
-    return AllocateComponent(theStruct->iProto, cast(Object) theStruct->iOrigin);
+  return AllocateComponent(theStruct->iProto, cast(Object) theStruct->iOrigin);
 }
-
 
 asmlabel(eqStruc, "
 	ld	[%sp+18*4], %o0
@@ -117,8 +112,34 @@ int CltStruc(ref(Structure) arg1,
 	     ref(Structure) arg2
 	     )
 {
-    fprintf(stderr, "ltStruc: Unfinished code, doesn't work\n");
-    return 0; /* Hmm, I got bored (??) */
+  /* Test if arg1 is a substructure of arg1 
+   * like in the following situation:
+   * T: (##); T1: T(##);
+   * Here T1## < T##
+   */
+
+  ref(ProtoType) proto1;
+  ref(ProtoType) proto2;
+
+  if (!arg1) return 0;
+  if (!arg2) return 0;
+
+  proto1 = arg1->Proto;
+  proto2 = arg2->Proto;
+  
+  if (proto1 == proto2) return 0;
+  
+  /* Prefix of proto2 is the first try */
+
+  proto2 = proto2->Prefix;
+  while (1) {
+    if (proto2 == proto2->Prefix) return 0; /* ?? How can this happen? */
+    if (proto1 == proto2) {
+      /* Now there is some hope, now we need to check is origin are equal. */
+      break; /* fix it later */
+    }
+  }
+  return 0; 
 }
 
 
