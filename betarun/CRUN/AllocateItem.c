e3a0f4871c98b335824204340d78e00056217df6
beta <beta@daimi.au.dk>
1992-06-16 23:49:08 +0000

    Minor formatting change
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@163 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/AllocateItem.c
+++ betarun/CRUN/AllocateItem.c
@@ -1,16 +1,13 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: AllocateItem.c,v $, rel: %R%, date: $Date: 1992-06-11 17:13:24 $, SID: $Revision: 1.3 $
+ * Mod: $RCSfile: AllocateItem.c,v $, rel: %R%, date: $Date: 1992-06-16 23:49:08 $, SID: $Revision: 1.4 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
 
-ref(Item)
-AllocateItem(ref(ProtoType) prototype,
-	     ref(Object) origin
-	     )
+ref(Item) AllocateItem(ref(ProtoType) prototype, ref(Object) origin)
 {
     register ref(Item) theItem;
 
