de91ac336f7367bc9ed2e24a7d01267fed479fd4
beta <beta@daimi.au.dk>
1992-07-20 11:48:04 +0000

    changed to new short names, dispathces on prototype
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@172 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CopyValRep.c
+++ betarun/CRUN/CopyValRep.c
@@ -1,36 +1,42 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CopyValRep.c,v $, rel: %R%, date: $Date: 1992-06-09 15:19:15 $, SID: $Revision: 1.3 $
+ * Mod: $RCSfile: CopyValRep.c,v $, rel: %R%, date: $Date: 1992-07-20 11:48:04 $, SID: $Revision: 1.4 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
 
-void CopyValRep(ref(ValRep) theRep,
-		ref(Object) theObj,
-		unsigned offset /* i ints */
-		)
+void CopyVR(ref(ValRep) theRep,
+	    ref(Object) theObj,
+	    unsigned offset /* i ints */
+	    )
 {
     register unsigned range, i;
     register ref(ValRep) newRep = NULL;
 
     range = theRep->HighBorder;
     if (range > LARGE_REP_SIZE)
+      switch(theRep->Proto){
+      case ByteRepPTValue:
+	newRep = cast(ValRep) LVRAByteAlloc(range);
+	break;
+      default:
 	newRep = cast(ValRep) LVRAAlloc(range);
-
+      }
     if (newRep) {
 	/* Make the LVRA-cycle: theCell -> newRep.Age */
 	newRep->GCAttr = (int) ((int *) theObj + offset);
       }
     else
       {
-	  newRep = cast(ValRep) IOAcalloc(range*4 + headsize(ValRep));
+	bodysize = (theRep->Proto==ByteRepPTValue)? ((range+4)/4)*4 : range*4;
+	newRep = cast(ValRep) IOAcalloc(bodysize + headsize(ValRep));
 
-	  newRep->Proto = ValRepPTValue;
-	  newRep->GCAttr = 1;
-	  newRep->LowBorder = 1;
-	  newRep->HighBorder = range;
+	newRep->Proto = theRep->Proto;
+	newRep->GCAttr = 1;
+	newRep->LowBorder = 1;
+	newRep->HighBorder = range;
       }
 
     /* Copy theRep to newRep */
@@ -38,5 +44,8 @@ void CopyValRep(ref(ValRep) theRep,
       newRep->Body[i] = theRep->Body[i];	/* Beautiful! */
 
     (casthandle(ValRep) theObj)[offset] = newRep;
+
+    if (!inIOA((int *)theObj + offset))
+      ChkRA((int *)theObj + offset);
 }
 
