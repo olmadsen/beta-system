8dffa0aee7b4245d23b0bf45610a40f92a1392f5
beta <beta@daimi.au.dk>
1992-07-20 11:46:07 +0000

    changed to new short names, added AlloVR1
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@170 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/AllocateValRep.c
+++ betarun/CRUN/AllocateValRep.c
@@ -1,20 +1,62 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: AllocateValRep.c,v $, rel: %R%, date: $Date: 1992-06-09 15:19:05 $, SID: $Revision: 1.3 $
+ * Mod: $RCSfile: AllocateValRep.c,v $, rel: %R%, date: $Date: 1992-07-20 11:46:07 $, SID: $Revision: 1.4 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
 
-asmlabel(AllocateValRep, "
+asmlabel(AlloVR1, "
 	mov %i0, %o0
         mov %l0, %o1
-	ba _CAllocateValRep
+	ba _CAlloVR1
 	mov %l1, %o2
 ");
 
-ref(ValRep) CAllocateValRep(ref(Object) theObj,
+ref(ValRep) CAlloVR1(ref(Object) theObj,
+			   unsigned offset, /* i bytes */
+			   unsigned range
+			   )
+{
+  register ref(ValRep) theRep;
+  register unsigned BodySize;
+
+  if (range < 0)
+    range = 0;
+
+  BodySize = ((range+4)/4)*4;
+
+  if (range > LARGE_REP_SIZE) {
+    theRep = cast(ValRep) LVRAByteAlloc(range);
+    if (theRep) {
+      /* Make the LVRA-cycle: theCell -> theRep.GCAttr */
+      theRep->GCAttr = (int) ((char *) theObj + offset);
+      *casthandle(ValRep)((char *)theObj + offset) = theRep;
+      int_clear((char*)theRep->Body, BodySize);
+      return theRep;
+    }
+  }
+
+  theRep = cast(ValRep) IOAcalloc(BodySize + headsize(ValRep));
+
+  theRep->Proto = ValRepPTValue;
+  theRep->GCAttr = 1;
+  theRep->LowBorder = 1;
+  theRep->HighBorder = range;
+
+  *casthandle(ValRep)((char *)theObj + offset) = theRep;
+  return theRep;
+}
+
+asmlabel(AlloVR4, "
+	mov %i0, %o0
+        mov %l0, %o1
+	ba _CAlloVR4
+	mov %l1, %o2
+");
+
+ref(ValRep) CAlloVR4(ref(Object) theObj,
 			   unsigned offset, /* i bytes */
 			   unsigned range
 			   )
