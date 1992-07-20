f4ee9f1125a23d817539618e42cfa2aca15ddc34
beta <beta@daimi.au.dk>
1992-07-20 11:48:53 +0000

    changed to new short names, dispatches on prototype
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@173 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CopySliceValRep.c
+++ betarun/CRUN/CopySliceValRep.c
@@ -1,27 +1,28 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CopySliceValRep.c,v $, rel: %R%, date: $Date: 1992-06-09 15:19:21 $, SID: $Revision: 1.5 $
+ * Mod: $RCSfile: CopySliceValRep.c,v $, rel: %R%, date: $Date: 1992-07-20 11:48:53 $, SID: $Revision: 1.6 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
 
-asmlabel(CopySliceValRep, "
+asmlabel(CopySVR, "
 	mov	%l7, %o3
-	ba	_CCopySliceValRep
+	ba	_CCopySVR
 	mov	%l6, %o4
 ");
 
-void CCopySliceValRep(ref(ValRep) theRep,
+void CCopySVR(ref(ValRep) theRep,
 		      ref(Item) theItem,
 		      unsigned offset, /* i ints */
 		      unsigned low,
 		      unsigned high
 		      )
 {
-    register unsigned size;
+    register unsigned BodySize;
     register int i;
+    register char *b;
     register ref(ValRep) newRep;
     
     /* Copy a slice of a Value Repetition. */
@@ -34,26 +35,33 @@ void CCopySliceValRep(ref(ValRep) theRep,
       BetaError(-7, theItem);
     
     /* Calculate the range of the new repetition. */
-    size = high - low + 1;
-    if (size < 0)
-      size = 0;
-    
-    /* size is now converted to the range of the resulting repetition. */
-    
-    newRep = cast(ValRep) IOAalloc(headsize(ValRep) + size*4);
+    high =  high - low + 1;
+    if (high < 0) high = 0;
+
+    BodySize = (theRep->Proto==ByteRepPTValue) ? ((high+4)/4)*4 : high*4;
+      
+    newRep = cast(ValRep) IOAalloc(headsize(ValRep) + BodySize);
     
     /* The new Object is now allocated, but not assigned yet! */
     
     /* Initialize the structual part of the repetition. */
-    newRep->Proto = ValRepPTValue;
+    newRep->Proto = theRep->Proto;
     newRep->GCAttr = 1;
     newRep->LowBorder = 1;
-    newRep->HighBorder = size;
+    newRep->HighBorder = high;
     
     /* Copy the body part of the repetition. */
-    
-    for (i = 0; i < size; ++i)
-      newRep->Body[i] = theRep->Body[i+low-theRep->LowBorder];
+    switch (theRep->Proto){
+    case ByteRepPTValue:
+      /* Since the slice may start on any byte we copy it byte by byte */
+      for (i = 0; i < high; ++i)
+	newRep->Body[i] = theRep->Body[i+low-theRep->LowBorder];
+      break;
+    default:
+      for (b = 0; b < high; ++b)
+	newRep->Body[b] = theRep->Body[b+low-theRep->LowBorder];
+      break;
+    }
     
     /* stack[8] -> theItem;
      * stack[4] -> offset
