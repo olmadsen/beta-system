4f0c2af6f030da8c13644a058636ff4b493ee276
beta <beta@daimi.au.dk>
1992-07-21 17:15:08 +0000

    continued conversion to shorter entrypoints and byte-texts
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@181 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CheckReferenceAssignment.c
+++ betarun/CRUN/CheckReferenceAssignment.c
@@ -1,11 +1,17 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CheckReferenceAssignment.c,v $, rel: %R%, date: $Date: 1992-07-20 11:49:42 $, SID: $Revision: 1.2 $
+ * Mod: $RCSfile: CheckReferenceAssignment.c,v $, rel: %R%, date: $Date: 1992-07-21 17:15:08 $, SID: $Revision: 1.3 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
+
+extern void CheckReferenceAssignment() asm("CheckReferenceAssignment"); /* Alias */
+void CheckReferenceAssignment(handle(Object) theObjHandle)
+{
+  ChkRA(theObjHandle);
+}
 
 void ChkRA(handle(Object) theObjHandle)
 {
