ea9a8859b1a349df75013f10d72753adffc48335
beta <beta@daimi.au.dk>
1992-07-23 17:18:18 +0000

    took into account that size in prototype now really is size!
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@199 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/AllocateItem.c
+++ betarun/CRUN/AllocateItem.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: AllocateItem.c,v $, rel: %R%, date: $Date: 1992-07-20 11:44:37 $, SID: $Revision: 1.5 $
+ * Mod: $RCSfile: AllocateItem.c,v $, rel: %R%, date: $Date: 1992-07-23 17:18:18 $, SID: $Revision: 1.6 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -11,7 +11,7 @@ ref(Item) AlloI(ref(ProtoType) prototype, ref(Object) origin)
 {
     register ref(Item) theItem;
 
-    theItem = cast(Item) IOAcalloc((prototype->Size+1)*4);
+    theItem = cast(Item) IOAcalloc(4*prototype->Size);
 
     /* The new Object is now allocated, but not initialized yet! */
 
