0bcfd04cd2bfb456bf7b4256c746d4bc7a10f74e
beta <beta@daimi.au.dk>
1992-07-24 17:10:34 +0000

    Initial revision
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@203 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/CRUN/CopyRefRep.c
@@ -0,0 +1,32 @@
+/*
+ * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
+ * Mod: $RCSfile: CopyRefRep.c,v $, rel: %R%, date: $Date: 1992-07-24 17:10:34 $, SID: $Revision: 1.1 $
+ * by Peter Andersen and Tommy Thorn.
+ */
+
+#include "beta.h"
+#include "crun.h"
+
+void CopyRR(ref(ValRep) theRep,
+	    ref(Object) theObj,
+	    unsigned offset /* i ints */
+	    )
+{
+  register unsigned range, i;
+  register ref(RefRep) newRep = NULL;
+  
+  range = theRep->HighBorder;
+  newRep = cast(RefRep) IOAcalloc(RefRepSize(range));
+  
+  newRep->Proto = theRep->Proto;
+  newRep->GCAttr = 1;
+  newRep->LowBorder = 1;
+  newRep->HighBorder = range;
+  
+  /* Copy theRep to newRep */
+  for (i = 0; i < range; ++i)
+    newRep->Body[i] = theRep->Body[i];	/* Beautiful! */
+  
+  AssignReference((long *)theObj + offset, cast(Item) newRep);
+}
+
