01d863cc736ed6398b143c58571ea8edbe71e423
beta <beta@daimi.au.dk>
1992-07-24 17:08:34 +0000

    added CpkSVT
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@201 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CInterface.c
+++ betarun/CRUN/CInterface.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CInterface.c,v $, rel: %R%, date: $Date: 1992-07-23 15:04:19 $, SID: $Revision: 1.6 $
+ * Mod: $RCSfile: CInterface.c,v $, rel: %R%, date: $Date: 1992-07-24 17:08:34 $, SID: $Revision: 1.7 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -13,10 +13,14 @@ void CinitT(void)
     CTextPoolEnd = CTextPool;
 }
 
-extern char *CpkVT() asm("CpkVT");
-char *CpkVT(ref(Object) theObj, handle(ValRep) theRepHandle)
+asmlabel(CpkVT, "
+	mov %o0, %o1
+        ba _CCpkVT
+	mov %i0, %o0
+");
+
+char *CCpkVT(ref(Object) theObj, ref(ValRep) theRep)
 {
-    ref(ValRep) theRep = *theRepHandle;
     long bodysize = ByteRepBodySize(theRep->HighBorder);
     long i;
 
@@ -30,6 +34,55 @@ char *CpkVT(ref(Object) theObj, handle(ValRep) theRepHandle)
     /* Copy the contents of the repetition to the CTextPool. */
     for (i = 0; i < bodysize/4; ++i)
       *((long *)CTextPoolEnd)++ = theRep->Body[i];
+
+    return CTextPoolEnd - bodysize;
+}
+
+/* Temporary alias */
+asmlabel(CpkVTS, "
+	mov %o2, %o3
+	mov %o1, %o2
+	mov %o0, %o1
+        ba _CCpkSVT
+	mov %i0, %o0
+");
+
+asmlabel(CpkSVT, "
+	mov %o2, %o3
+	mov %o1, %o2
+	mov %o0, %o1
+        ba _CCpkSVT
+	mov %i0, %o0
+");
+
+/* CCpkSVT: Copy Slice of variable text (byte rep) to C */
+char *CCpkSVT(ref(Object) currentObj, ref(ValRep) theRep, unsigned low, unsigned high)
+{
+    long bodysize;
+    long i;
+    unsigned char *oldBody;
+
+    if (low<theRep->LowBorder) BetaError(-6, currentObj);
+    if (high<theRep->LowBorder) BetaError(-7, currentObj);
+    if (low>theRep->HighBorder) BetaError(-6, currentObj);
+    if (high>theRep->HighBorder) BetaError(-7, currentObj);
+    high = high - low + 1;
+    if (high<0) high=0;
+
+    bodysize = ByteRepBodySize(high);
+    /* Check range overflow on CTextPool.
+     * nextText is used as a tmp. register only.
+     * Size_left_in_CTextPool = (CTextPool + MAXCTEXTPOOL) - CTextPoolEnd.
+     */
+    if (bodysize > (CTextPool + MAXCTEXTPOOL) - CTextPoolEnd)
+      BetaError(-35, currentObj);
+    
+    /* Copy the contents of the repetition to the CTextPool. */
+    oldBody = (unsigned char *)((unsigned)theRep->Body+(low-theRep->LowBorder));
+    for (i = 0;  i < high; i+=1)
+      *(((unsigned char *)CTextPoolEnd)++) = *(unsigned char *)((unsigned)oldBody+i);
+    *(((unsigned char *)CTextPoolEnd)++) = 0; /* NULL termination */
+    (long)CTextPoolEnd = (((long)CTextPoolEnd+3)/4)*4; /* long align next text */
 
     return CTextPoolEnd - bodysize;
 }
