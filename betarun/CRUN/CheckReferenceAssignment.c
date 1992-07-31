edeedb6c708e9a438f7f3db3531fb3afb0b8fae7
beta <beta@daimi.au.dk>
1992-07-31 16:35:57 +0000

    The argument is passed in %g1
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@207 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CheckReferenceAssignment.c
+++ betarun/CRUN/CheckReferenceAssignment.c
@@ -1,15 +1,19 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CheckReferenceAssignment.c,v $, rel: %R%, date: $Date: 1992-07-23 15:12:21 $, SID: $Revision: 1.4 $
+ * Mod: $RCSfile: CheckReferenceAssignment.c,v $, rel: %R%, date: $Date: 1992-07-31 16:35:57 $, SID: $Revision: 1.5 $
  * by Peter Andersen and Tommy Thorn.
  */
 
 #include "beta.h"
 #include "crun.h"
 
-void ChkRA(handle(Object) theObjHandle)
+/* This is called with target in %g1, which is a super temp. */
+
+void ChkRA()
 {
-  /* The assignment *theObjHandle = theObj has just been
+  register handle(Object) theObjHandle asm("%g1");
+
+  /* The Assignment *theObjHandle = theObj has just been
    * done. We know the theObjHandle is in AOA, now check if
    * *theObjHandle is in IOA.
    */
