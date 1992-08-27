c3763392cc88ba41728448a7876efba8fcb02754
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:50:11 +0000

    Uses DeclReference1 and GCalble_Entry.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@269 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/NewValRep.c
+++ betarun/CRUN/NewValRep.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: NewValRep.c,v $, rel: %R%, date: $Date: 1992-08-22 02:08:50 $, SID: $Revision: 1.5 $
+ * Mod: $RCSfile: NewValRep.c,v $, rel: %R%, date: $Date: 1992-08-27 15:50:11 $, SID: $Revision: 1.6 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -20,32 +20,30 @@ void CNewVR(int range,
 	    int offset /* in ints */
 	    )
 {
-    GCable_Entry
-
-
-#define theRep (cast(ValRep) GCreg3)
-  
-  Ck(theObj);
-  theRep = (casthandle(ValRep)theObj)[offset];
-  
-  switch( (int) theRep->Proto){
-  case (int) ByteRepPTValue:
-    CAlloVR1(theObj, offset*4, range); /* MP MP !! */
-    break;
-  case (int) WordRepPTValue:
-    CAlloVR2(theObj, offset*4, range); /* MP MP !! */
-    break;
-  case (int) ValRepPTValue:
-    CAlloVR4(theObj, offset*4, range); /* MP MP !! */
-    break;
-  case (int) DoubleRepPTValue:
-    CAlloVR8(theObj, offset*4, range); /* MP MP !! */
-    break;
-  default:
-    fprintf(output, "NewValRep: wrong prototype\n");
-    exit(1);
-  }
-  if (!inIOA((long *)theObj+offset))
-    CCheckRefAsgn((struct Object **)theObj+offset);
+    DeclReference1(struct ValRep *theRep);
+    GCable_Entry();
+    
+    Ck(theObj);
+    theRep = (casthandle(ValRep)theObj)[offset];
+    
+    switch( (int) theRep->Proto){
+      case (int) ByteRepPTValue:
+	CAlloVR1(theObj, offset*4, range); /* MP MP !! */
+	break;
+      case (int) WordRepPTValue:
+	CAlloVR2(theObj, offset*4, range); /* MP MP !! */
+	break;
+      case (int) ValRepPTValue:
+	CAlloVR4(theObj, offset*4, range); /* MP MP !! */
+	break;
+      case (int) DoubleRepPTValue:
+	CAlloVR8(theObj, offset*4, range); /* MP MP !! */
+	break;
+      default:
+	fprintf(output, "NewValRep: wrong prototype\n");
+	exit(1);
+    }
+    if (!inIOA((long *)theObj+offset))
+      CCheckRefAsgn((struct Object **)theObj+offset);
 }
-  
+
