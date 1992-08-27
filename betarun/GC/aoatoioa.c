97185978332ef141592ca8e8ca86e8a9ab21eb7b
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:56:22 +0000

    Code style change.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@275 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/GC/aoatoioa.c
+++ betarun/GC/aoatoioa.c
@@ -16,102 +16,108 @@ static int prim_index = 0;
 /* Allocates the initial AOAtoIOAtable. */
 int AOAtoIOAAlloc()
 {
-  AOAtoIOAtableSize = primes[0];
-  if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
-    AOAtoIOAtable->top = AOAtoIOAtable->limit;
-    AOAtoIOAClear();
-    INFO_AOA( fprintf( output, "#(AOA: AOAtoIOAtable allocated %d longs.)\n",
-		      AOAtoIOAtableSize));
-    return 1;
-  } else
-    return 0;
+    AOAtoIOAtableSize = primes[0];
+    if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
+	AOAtoIOAtable->top = AOAtoIOAtable->limit;
+	AOAtoIOAClear();
+	INFO_AOA( fprintf( output, "#(AOA: AOAtoIOAtable allocated %d longs.)\n",
+			  AOAtoIOAtableSize));
+	return 1;
+    } else
+      return 0;
 }
 
 /* Allocate a larger AOAtoIOAtable based on the next entry in primes. */
 AOAtoIOAReAlloc()
 {
-  /* Save the old table. */
-  ref(Block) oldBlock      = AOAtoIOAtable;
-  int        oldBlockSize  = AOAtoIOAtableSize;
-
-  /* Exit if we can't find a new entry in prims. */
-  if( primes[++prim_index] == 0 ) BetaError(-33, 0);
-
-  /* Allocate a new and larger block to hold AOAtoIOA references. */
-  AOAtoIOAtableSize = primes[prim_index];
-  if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
-    AOAtoIOAtable->top = AOAtoIOAtable->limit;
-    AOAtoIOAClear();
-    INFO_AOA( fprintf( output, "#(AOAtoIOAtable resized %d longs.)\n",
-		      AOAtoIOAtableSize));
-  } else {
-    /* If the allocation of the new AOAtoIOAtable failed please
-       terminate the program execution. */
-    BetaError( -36, 0);
+    /* Save the old table. */
+    ref(Block) oldBlock      = AOAtoIOAtable;
+    int        oldBlockSize  = AOAtoIOAtableSize;
+    
+    /* Exit if we can't find a new entry in prims. */
+    if( primes[++prim_index] == 0 ) BetaError(-33, 0);
+    
+    /* Allocate a new and larger block to hold AOAtoIOA references. */
+    AOAtoIOAtableSize = primes[prim_index];
+    if( AOAtoIOAtable = newBlock(AOAtoIOAtableSize * sizeof(long)) ){
+	AOAtoIOAtable->top = AOAtoIOAtable->limit;
+	AOAtoIOAClear();
+	INFO_AOA( fprintf( output, "#(AOAtoIOAtable resized %d longs.)\n",
+			  AOAtoIOAtableSize));
+    } else {
+	/* If the allocation of the new AOAtoIOAtable failed please
+	   terminate the program execution. */
+	BetaError( -36, 0);
+    }
+    
+    /* Move all entries from the old table into to new. */
+    { int i, counter = 0;  ptr(long) pointer = BlockStart( oldBlock);
+      for(i=0; i < oldBlockSize; i++){
+	  if( *pointer ) AOAtoIOAInsert( *pointer );
+	  pointer++;
+      }
   }
     
-  /* Move all entries from the old table into to new. */
-  { int i, counter = 0;  ptr(long) pointer = BlockStart( oldBlock);
-    for(i=0; i < oldBlockSize; i++){
-      if( *pointer ) AOAtoIOAInsert( *pointer );
-      pointer++;
-    }
-  }
-
-  /* Deallocate the old table. */
-  freeBlock( oldBlock);    
+    /* Deallocate the old table. */
+    freeBlock( oldBlock);    
 }
 
 AOAtoIOAInsert( theCell)
-  handle( Object) theCell;
+     handle( Object) theCell;
 {
-  ptr(long) table = BlockStart( AOAtoIOAtable);
-  long      index, count;
-
-  DEBUG_AOA( Claim( inAOA( theCell),"AOAtoIOAInsert: theCell in AOA"));
-
-  /* First Hash function. */
-  index = ((long) theCell) % AOAtoIOAtableSize;
-  if( table[index] == 0){ table[index] = (long) theCell; return; }
-  if( table[index] == (long) theCell ) return;
-
-  /* Second Hash function. */
-  index = (((long) theCell)<<4) % AOAtoIOAtableSize;
-  if( table[index] == 0 ){ table[index] = (long) theCell; return; }
-  if( table[index] == (long) theCell ) return;
-
-  count = 0;
-  while( count < 100 ){
-    count++; index = (count + (long) theCell) % AOAtoIOAtableSize;
+    ptr(long) table = BlockStart( AOAtoIOAtable);
+    long      index, count;
+    
+    DEBUG_AOA( Claim( inAOA( theCell),"AOAtoIOAInsert: theCell in AOA"));
+    
+    /* First Hash function. */
+    index = ((long) theCell) % AOAtoIOAtableSize;
+    if( table[index] == 0){ table[index] = (long) theCell; return; }
+    if( table[index] == (long) theCell ) return;
+    
+    /* Second Hash function. */
+    index = (((long) theCell)<<4) % AOAtoIOAtableSize;
     if( table[index] == 0 ){ table[index] = (long) theCell; return; }
     if( table[index] == (long) theCell ) return;
-  }
-  AOAtoIOAReAlloc();
-  AOAtoIOAInsert( theCell);
+    
+    count = 0;
+    while( count < 100 ){
+	count++; index = (count + (long) theCell) % AOAtoIOAtableSize;
+	if( table[index] == 0 ){ table[index] = (long) theCell; return; }
+	if( table[index] == (long) theCell ) return;
+    }
+    AOAtoIOAReAlloc();
+    AOAtoIOAInsert( theCell);
 }
 
 AOAtoIOAClear()
-{ int i; ptr(long) pointer = BlockStart( AOAtoIOAtable);
-  for(i=0;i<AOAtoIOAtableSize;i++) *pointer++ = 0;
+{ 
+    int i; ptr(long) pointer = BlockStart( AOAtoIOAtable);
+    for(i=0;i<AOAtoIOAtableSize;i++) *pointer++ = 0;
 }
 
 #ifdef RTDEBUG
 AOAtoIOACheck()
-{ int i; ptr(long) pointer = BlockStart( AOAtoIOAtable);
-  for(i=0;i<AOAtoIOAtableSize;i++){
-    if( *pointer )
-      Claim( inAOA( *pointer),"AOAtoIOACheck: *pointer in AOA" );
-    pointer++;
-  }
+{ 
+    int i; ptr(long) pointer = BlockStart( AOAtoIOAtable);
+    
+    /* fprintf(output, "#AOAtoIOACheck: AOAtoIOAtableSize: %d\n", AOAtoIOAtableSize); */ 
+    for(i=0; i<AOAtoIOAtableSize; i++){
+	if( pointer[i] ){
+	    /* fprintf( output, "0x%x\n", pointer[i]); */
+	    Claim( inAOA( pointer[i]),"AOAtoIOACheck: *pointer in AOA" );
+	}
+    }
 }
 
 AOAtoIOAReport()
-{ int used = 0;
-  if( AOAtoIOAtable ){
-    MACRO_ScanBlock( AOAtoIOAtable, if( *thisCell) used++ );
-    printf( output, "#AOAtoIOATable size=%d filled=%d%%\n",
-	   AOAtoIOAtableSize, (100*used)/AOAtoIOAtableSize);
-  }
+{ 
+    int used = 0;
+    if( AOAtoIOAtable ){
+	MACRO_ScanBlock( AOAtoIOAtable, if( *thisCell) used++ );
+	printf( output, "#AOAtoIOATable size=%d filled=%d%%\n",
+	       AOAtoIOAtableSize, (100*used)/AOAtoIOAtableSize);
+    }
 }
 
 #endif
