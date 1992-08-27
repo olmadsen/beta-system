e6f3719250434d6d56d7b205736a224cff9bd602
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:45:40 +0000

    Fixed some indention. Uses DeclReference.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@265 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CopyCtext.c
+++ betarun/CRUN/CopyCtext.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: CopyCtext.c,v $, rel: %R%, date: $Date: 1992-08-25 09:31:20 $, SID: $Revision: 1.9 $
+ * Mod: $RCSfile: CopyCtext.c,v $, rel: %R%, date: $Date: 1992-08-27 15:45:40 $, SID: $Revision: 1.10 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -13,33 +13,34 @@ asmlabel(CopyCT, "ba _CCopyCT; mov %l0, %o0");
 
 ref(ValRep) CCopyCT(unsigned char *textPtr)
 {
-  register unsigned range, size, i;
+    DeclReference1(struct ValRep *theRep);
+    register unsigned range, size, i;
 
-  GCable_Entry
-#define theRep (cast(ValRep) GCreg3)
+    GCable_Entry();
 
-  /* Allocate a ValueRepetition and initialize it with some text.    */
+      /* Allocate a ValueRepetition and initialize it with some text.    */
 
-  range = strlen(textPtr);
-  size = ByteRepSize(range);
+    range = strlen(textPtr);
+    size = ByteRepSize(range);
 
-  /* LVRA ?? */
-  theRep = cast(ValRep) IOAalloc(size);
+    /* LVRA ?? */
+    theRep = cast(ValRep) IOAalloc(size);
 
-  theRep->Proto = ByteRepPTValue;
-  theRep->GCAttr = 1;
-  theRep->LowBorder = 1;
-  theRep->HighBorder = range;
+    theRep->Proto = ByteRepPTValue;
+    theRep->GCAttr = 1;
+    theRep->LowBorder = 1;
+    theRep->HighBorder = range;
 
-  /* Assign the text to the body part of the repetition. */
+    /* Assign the text to the body part of the repetition. */
 
-  for (i = 0; i < (size-headsize(ValRep)); i++){
-    theRep->Body[i] = *((long *)textPtr + i);
-  }
+    for (i = 0; i < (size-headsize(ValRep)); i++) {
+	theRep->Body[i] = *((long *)textPtr + i);
+    }
 
-  __asm__("ret;restore %0, 0, %%i2"::"r" (theRep)); /* hack hack. Olm wants the
-				        result in %i2 */
-  return theRep; /* dummy, keeps gcc happy */
+    /* hack hack. Olm wants the result in %i2 */
+    __asm__("ret;restore %0, 0, %%i2"::"r" (theRep));
+
+    return theRep; /* dummy, keeps gcc happy */
 }
 
 
