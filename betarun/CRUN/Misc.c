1e13709f867ce756ab51debcf7dfa3afd227259d
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:49:00 +0000

    Didn't set ReqObjectSize.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@268 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Misc.c
+++ betarun/CRUN/Misc.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-08-19 15:45:06 $, SID: $Revision: 1.6 $
+ * Mod: $RCSfile: Misc.c,v $, rel: %R%, date: $Date: 1992-08-27 15:49:00 $, SID: $Revision: 1.7 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -32,10 +32,11 @@ IOAalloc(unsigned size)
 {
   register char *p;
 
-  GCable_Entry;
+  GCable_Entry();
 
   while ((char *)IOATop+size > (char *)IOALimit) {
-    DoGC();
+      ReqObjectSize = size / 4;
+      DoGC();
   }
 
   p = (char *)IOATop;
@@ -48,10 +49,11 @@ char *IOAcalloc(unsigned size)
 {
   register char *p;
 
-  GCable_Entry;
+  GCable_Entry();
 
   while ((char *) IOATop+size > (char *)IOALimit) {
-    DoGC();
+      ReqObjectSize = size / 4;
+      DoGC();
   }
 
   p = (char *)IOATop;
