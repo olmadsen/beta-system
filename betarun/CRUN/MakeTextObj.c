ff61ea80b37bb6f1f3cd2d7a757f8cba8bbc0517
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:48:22 +0000

    Uses DeclReference1 and GCable_Entry.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@267 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/MakeTextObj.c
+++ betarun/CRUN/MakeTextObj.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: MakeTextObj.c,v $, rel: %R%, date: $Date: 1992-08-22 02:09:03 $, SID: $Revision: 1.6 $
+ * Mod: $RCSfile: MakeTextObj.c,v $, rel: %R%, date: $Date: 1992-08-27 15:48:22 $, SID: $Revision: 1.7 $
  * by Peter Andersen and Tommy Thorn.
  */
 
@@ -18,24 +18,23 @@ void MkTO(char *cText,
 	  ref(Item) theItem,
 	  unsigned offset /* i ints */ )
 {
-  GCable_Entry
-
-#define theText (cast(Item) GCreg3)
-
-  Ck(theItem); Ck(BasicItem);
-  theText = CAlloI(BasicItem, TextProto);
-  
-  AssignReference((long *)theItem + offset, theText);
-
-  /* Prepare for copying of the asciz into the repetition of the text object */
-  
-  CopyT(cText, theText, REP_OFF); 
-
-  /* store range in lgth. TEXT ATTRIBUTE HARDCODED:16.
-   * store range in pos. TEXT ATTRIBUTE HARDCODED:20.
-   * (headsize(Item) + 2*sizeof(long) == 16)
-   */
-  theText->Body[2] =
-    theText->Body[3] = (casthandle(ValRep)theText)[REP_OFF]->HighBorder;
+    DeclReference1(struct Item *theText);
+    GCable_Entry();
+    
+    Ck(theItem); Ck(BasicItem);
+    theText = CAlloI(BasicItem, TextProto);
+    
+    AssignReference((long *)theItem + offset, theText);
+    
+    /* Prepare for copying of the asciz into the repetition of the text object */
+    
+    CopyT(cText, theText, REP_OFF); 
+    
+    /* store range in lgth. TEXT ATTRIBUTE HARDCODED:16.
+     * store range in pos. TEXT ATTRIBUTE HARDCODED:20.
+     * (headsize(Item) + 2*sizeof(long) == 16)
+     */
+    theText->Body[2] =
+      theText->Body[3] = (casthandle(ValRep)theText)[REP_OFF]->HighBorder;
 }
 
