0cf503dfe59b5214099891f1d08af815cbdd6df6
Tommy Thorn <tthorn@daimi.au.dk>
1992-08-27 15:12:23 +0000

    Sanity checks for CBFABlockSize
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@252 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/cbfa.c
+++ betarun/C/cbfa.c
@@ -1,44 +1,61 @@
 /*
  * BETA RUNTIME SYSTEM, Copyright (C) 1990 Mjolner Informatics Aps.
- * Mod: $RCSfile: cbfa.c,v $, rel: %R%, date: $Date: 1992-07-23 15:11:01 $, SID: $Revision: 1.3 $
+ * Mod: $RCSfile: cbfa.c,v $, rel: %R%, date: $Date: 1992-08-27 15:12:23 $, SID: $Revision: 1.4 $
  * by Lars Bak.
  */
 #include "beta.h"
 
 void CBFAAlloc()
 {
-  /* Allocate the Call Back Functions Area. */
-  if ( ! (CBFA = cast(CallBackArea) MALLOC(sizeof(struct CallBackArea))) ) {
-    fprintf(output,"#Beta: Couldn't allocate CBFA\n");
-    exit(1);
-  }
-  lastCBFA = CBFA;
-  if ( ! (lastCBFA->entries = cast(CallBackEntry) MALLOC(CBFABlockSize)) ) {
-    fprintf(output,"#Beta: Couldn't allocate CBFA (%dKb)\n", CBFABlockSize/Kb);
-    exit(1);
-  }
-  lastCBFA->next = 0;
-  CBFATop = lastCBFA->entries;
-  CBFALimit = cast(CallBackEntry) ((long) lastCBFA->entries + CBFABlockSize);
+    /* Allocate the Call Back Functions Area. */
+    if ( CBFABlockSize < 0 ) {
+	fprintf(output,"#Beta: Too small CBFA specified %dKb\n", CBFABlockSize/Kb);
+	fprintf(output,"#Beta: Check your BETART environment variable\n");
+	exit(1);
+    }
+    if ( CBFABlockSize == 0 ) {
+	INFO_CBFA( fprintf(output,"#CBFA: Warning, CBFA size of 0Kb specified\n"));
+	INFO_CBFA( fprintf(output,"#CBFA: Check your BETART environment variable\n"));
+    } else {
+	if ( ! (CBFA = cast(CallBackArea) MALLOC(sizeof(struct CallBackArea))) ) {
+	    fprintf(output,"#Beta: Couldn't allocate CBFA\n");
+	    exit(1);
+	}
+	lastCBFA = CBFA;
+	if ( ! (lastCBFA->entries = cast(CallBackEntry) MALLOC(CBFABlockSize)) ) {
+	    fprintf(output,"#Beta: Couldn't allocate CBFA (%dKb)\n", CBFABlockSize/Kb);
+	    exit(1);
+	}
+	lastCBFA->next = 0;
+	CBFATop = lastCBFA->entries;
+	CBFALimit = cast(CallBackEntry) ((long) lastCBFA->entries + CBFABlockSize);
+    }
 }
 
 void CBFArelloc ()
 {
-  /* BetaError(-11, 0); */
-
-  /* Allocate new CBFA block */
-  if ( ! (CBFA->next = cast(CallBackArea) MALLOC(sizeof(struct CallBackArea))) ) {
-    fprintf(output,"#Beta: Couldn't allocate CBFA\n");
-    exit(1);
-  }
-  lastCBFA = CBFA->next;
-  if ( ! (lastCBFA->entries = cast(CallBackEntry) MALLOC(CBFABlockSize)) ) {
-    fprintf(output,"#Beta: Couldn't allocate CBFA (%dKb)\n", CBFABlockSize/Kb);
-    exit(1);
-  }
-  lastCBFA->next = 0;
-  CBFATop = lastCBFA->entries;
-  CBFALimit = cast(CallBackEntry) ((long) lastCBFA->entries + CBFABlockSize);
-
-  INFO_CBFA( fprintf(output, "#(CBFA: new block allocated %dKb.)\n", CBFABlockSize/Kb); );
+    /* BetaError(-11, 0); */
+    
+    if ( CBFABlockSize == 0 ) {
+	fprintf(output,"#CBFA: Using callbacks and CBFA size of 0Kb specified\n");
+	fprintf(output,"#Beta: Check your BETART environment variable\n");
+	exit(1);
+    }
+	
+    /* Allocate new CBFA block */
+    if ( ! (CBFA->next = cast(CallBackArea) MALLOC(sizeof(struct CallBackArea))) ) {
+	fprintf(output,"#Beta: Couldn't allocate CBFA\n");
+	exit(1);
+    }
+    lastCBFA = CBFA->next;
+    if ( ! (lastCBFA->entries = cast(CallBackEntry) MALLOC(CBFABlockSize)) ) {
+	fprintf(output,"#Beta: Couldn't allocate CBFA (%dKb)\n", CBFABlockSize/Kb);
+	exit(1);
+    }
+    lastCBFA->next = 0;
+    CBFATop = lastCBFA->entries;
+    CBFALimit = cast(CallBackEntry) ((long) lastCBFA->entries + CBFABlockSize);
+    
+    INFO_CBFA( fprintf(output, "#(CBFA: new block allocated %dKb.)\n", CBFABlockSize/Kb); );
+    
 }
