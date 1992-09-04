c5524900b10ac078c2a13fd7ebc4545652a08ce7
beta <beta@daimi.au.dk>
1992-09-04 14:34:58 +0000

    Added support for sparc
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@347 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/HandleIndexErr.c
+++ betarun/CRUN/HandleIndexErr.c
@@ -1,18 +1,17 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: HandleIndexErr.c,v $, rel:$R$, date: $Date: 1992-09-04 07:47:51 $,
- * SID: $Revision: 1.1 $
+ * Mod: $RCSfile: HandleIndexErr.c,v $, rel:$R$, date: $Date: 1992-09-04 14:34:58 $,
+ * SID: $Revision: 1.2 $
  * by Peter Orbaek
  */
 
 #include "beta.h"
 #include "crun.h"
 
-HandleIndexErr(struct ValRep *rep)
+ParamThisComp(HandleIndexErr)
 {
-    struct Object *this;
-
 #ifdef hppa
+    struct Object *this;
     this = (struct Object *)getThisReg();
 #endif
 
