adaa124c23a6958eba73cb18d974b771885de508
beta <beta@daimi.au.dk>
1992-09-04 14:37:34 +0000

    Ups, forgot type...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@349 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/HandleIndexErr.c
+++ betarun/CRUN/HandleIndexErr.c
@@ -1,14 +1,14 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $RCSfile: HandleIndexErr.c,v $, rel:$R$, date: $Date: 1992-09-04 14:34:58 $,
- * SID: $Revision: 1.2 $
+ * Mod: $RCSfile: HandleIndexErr.c,v $, rel:$R$, date: $Date: 1992-09-04 14:37:34 $,
+ * SID: $Revision: 1.3 $
  * by Peter Orbaek
  */
 
 #include "beta.h"
 #include "crun.h"
 
-ParamThisComp(HandleIndexErr)
+ParamThisComp(void, HandleIndexErr)
 {
 #ifdef hppa
     struct Object *this;
