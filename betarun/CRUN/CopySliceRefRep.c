853d9b50c6376a847c3502ce8ccbf5880aa35b17
beta <beta@daimi.au.dk>
1992-09-25 22:01:00 +0000

    Extra claims
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@413 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CopySliceRefRep.c
+++ betarun/CRUN/CopySliceRefRep.c
@@ -1,6 +1,6 @@
 /*
  * BETA C RUNTIME SYSTEM, Copyright (C) 1990,91,92 Mjolner Informatics Aps.
- * Mod: $Id: CopySliceRefRep.c,v 1.16 1992-09-08 09:52:19 poe Exp $
+ * Mod: $Id: CopySliceRefRep.c,v 1.17 1992-09-25 22:01:00 beta Exp $
  * by Peter Andersen and Tommy Thorn.
  */
 
