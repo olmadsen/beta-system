52a446732ab6947ffee4915762e4231f14963adc
beta <beta@daimi.au.dk>
1992-10-30 12:34:12 +0000

    Allow to continue after qua error
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@514 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/exit.c
+++ betarun/C/exit.c
@@ -1,6 +1,6 @@
 /*
  * BETA RUNTIME SYSTEM, Copyright (C) 1990 Mjolner Informatics Aps.
- * Mod: $Id: exit.c,v 1.6 1992-09-21 17:02:00 tthorn Exp $
+ * Mod: $Id: exit.c,v 1.7 1992-10-30 12:34:12 beta Exp $
  * by Lars Bak, Peter Andersen, Peter Orbaek and Tommy Thorn.
  */
 
@@ -21,15 +21,21 @@ BetaError(errorNo, theObj)
   int errorNo;
   ref(Object) theObj;
 {
-  if( errorNo < 0 ){
+  if (QuaCont && errorNo==QuaErr){
+    fprintf(output, "\n*** OBS. ");
+    ErrorMessage(output, QuaErr);
+    fprintf(output, " (continuing)\n");
+  } else {
+    if( errorNo < 0 ){
 #ifdef sparc
-    asm("ta 3");
-    StackEnd = (long *) ((struct RegWin *)FramePointer)->fp;
+      asm("ta 3");
+      StackEnd = (long *) ((struct RegWin *)FramePointer)->fp;
 #else
-    StackEnd = (ptr(long)) &theObj; StackEnd++;
+      StackEnd = (ptr(long)) &theObj; StackEnd++;
 #endif
-    DisplayBetaStack( errorNo, theObj);  
+      DisplayBetaStack( errorNo, theObj);  
+    }
+    BetaExit(-1);
   }
-  BetaExit(-1);
 }
 
