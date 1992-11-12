9e347610f085af0ca782a3ce8df5710bf85c7714
beta <beta@daimi.au.dk>
1992-11-12 14:44:44 +0000

    Added various DEMO stuff + some macintosh initialization stuff
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@555 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/initialize.c
+++ betarun/C/initialize.c
@@ -10,6 +10,32 @@ extern CBFAAlloc();
 #include <signal.h>
 #endif
 
+#ifdef macintosh
+#include <SegLoad.h>
+extern void _DataInit();
+#endif
+
+#ifdef DEMO
+#define DEMOSTRING "\n\
+                      **********************************\n\
+                      Mjolner BETA System - DEMO Version\n\
+                      **********************************\n\
+This software is provided \"as is\", without warranties of any kind, and with no\n\
+provisions for support or future enhancements. Neither Mjolner Informatics or \n\
+any of its employees has liabilities in connection with the use of the software.\n\
+\n\
+This DEMO version is restricted to be used by one person, and only for non-\n\
+commercial research or investigation purposes. It is not legal to distribute the\n\
+software and associated documentation, or any portion or derivatives thereof, to\n\
+anybody else.\n\
+\n\
+Using the Mjolner BETA System for commercial purposes is only legal after having\n\
+purchased the commercially available Mjolner BETA System.\n\
+\n\
+                      **********************************\n\
+\n"
+#endif
+
 long AllocateHeap( base, top, limit, size)
   ptr(long) base; /* points out a cell which should refer the base. */
   ptr(long) top;
@@ -33,9 +59,14 @@ Initialize()
   long *tmpIOA, *tmpIOATop;
 #ifdef macintosh
   InitTheCursor();
+  UnloadSeg((Ptr)_DataInit); /* Unload %A5Init% segment */
 #endif
 
   GetBetaEnv();
+
+#ifdef DEMO
+  fprintf(stdout, DEMOSTRING);
+#endif
 
   INFO( fprintf( output, "#(Heap info: IOA=2*%dKb", IOASize/Kb) );
 #ifdef AO_Area
