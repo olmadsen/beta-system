761d969d39dd877e75695eaad182d7e80c0d2989
beta <beta@daimi.au.dk>
1993-03-29 08:14:03 +0000

    Added label at end of BetaError for Valhalla
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@740 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/exit.c
+++ betarun/C/exit.c
@@ -23,47 +23,50 @@ void BetaExit( number )
 }
 
 void BetaError(errorNo, theObj)
-  long errorNo;
-  ref(Object) theObj;
+     long errorNo;
+     ref(Object) theObj;
 {
-  if( errorNo < 0 ){
+  do {
+    if( errorNo < 0 ){
 #ifdef sparc
-    asm("ta 3");
-    StackEnd = (long *) ((struct RegWin *)FramePointer)->fp;
+      asm("ta 3");
+      StackEnd = (long *) ((struct RegWin *)FramePointer)->fp;
 #else
-    StackEnd = (ptr(long)) &theObj; StackEnd++;
-    /* Current object was pushed as the first thing, when
-     * the error was detected. The "thing" just below
-     * is the first real part of the Beta stack
-     */
-#endif
-    if (errorNo==QuaErr || errorNo==QuaOrigErr){
-      if (QuaCont) {
-	fprintf(output, "\n*** OBS. ");
-	ErrorMessage(output, QuaErr);
-	fprintf(output, ".\nCurrent object:\n");
-	if( theObj != 0 ){
-	  if( isObject(theObj)){
-	    DisplayObject(output, theObj, 0);
-	  }else{
-	    fprintf(output,"Current object is damaged!\n");
-	  }
-	} else {
-	  fprintf(output,"Current object is 0!\n");
-	}
-	fprintf(output, "(continuing)\n");
-	fflush(output);
-	return;
-      } 
-#ifndef sparc
-      (long *)StackEnd+=13;
-      /* We have saved a0-a4, d0-d7, and also we have a return
-       * address from jsr Qua to ignore.; see Qua.run.
+      StackEnd = (ptr(long)) &theObj; StackEnd++;
+      /* Current object was pushed as the first thing, when
+       * the error was detected. The "thing" just below
+       * is the first real part of the Beta stack
        */
 #endif
-    }
-    DisplayBetaStack( errorNo, theObj);  
-  }
-  BetaExit(1);
+      if (errorNo==QuaErr || errorNo==QuaOrigErr){
+	if (QuaCont) {
+	  fprintf(output, "\n*** OBS. ");
+	  ErrorMessage(output, QuaErr);
+	  fprintf(output, ".\nCurrent object:\n");
+	  if( theObj != 0 ){
+	    if( isObject(theObj)){
+	      DisplayObject(output, theObj, 0);
+	    }else{
+	      fprintf(output,"Current object is damaged!\n");
+	    }
+	  } else {
+	    fprintf(output,"Current object is 0!\n");
+	  }
+	  fprintf(output, "(continuing)\n");
+	  fflush(output);
+	  break; /* Don't BetaExit() */
+	} 
+#ifndef sparc
+	(long *)StackEnd+=13;
+	/* We have saved a0-a4, d0-d7, and also we have a return
+	 * address from jsr Qua to ignore.; see Qua.run.
+	 */
+#endif
+      }
+      DisplayBetaStack( errorNo, theObj);  
+    }    
+    BetaExit(1);
+  } while (FALSE);
+  asmemptylabel(BetaErrorEnd);
 }
 
