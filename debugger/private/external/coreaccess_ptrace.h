a73de59a492e31def615a3d3445b9fe49429d856
beta <beta@daimi.au.dk>
1994-04-22 17:54:09 +0000

    Files added to handle the setting of breakpoints on systems where ptrace is the only/easiest way to write in the process code area.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1097 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ debugger/private/external/coreaccess_ptrace.h
@@ -0,0 +1,25 @@
+
+#ifndef COREACCESS_PTRACE_H
+#define COREACCESS_PTRACE_H
+
+#include <sys/types.h>
+
+/* int SetBreak (pid_t pid, int address, int *oldInstruction)
+ * =========================================================
+ *
+ * Set breakpoint at address. The previous instruction at that address
+ * is returned to be used by UnsetBreak.
+ * Return 0 if success. errno otherwise. */
+
+int SetBreak (pid_t pid, int address, int *oldInstruction);
+
+
+/* int UnsetBreak (pid_t pid, int address, int oldInstruction)
+ * ===========================================================
+ *
+ * Remove breakpoint previously set by SetBreak. 
+ * Return 0 if success. errno otherwise. */
+
+int UnsetBreak (pid_t pid, int address, int oldInstruction);
+
+#endif
