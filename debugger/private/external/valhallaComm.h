6c52ccfb0b3c0559eeb7dd4a9f15a449a8429239
beta <beta@daimi.au.dk>
1994-05-03 17:51:14 +0000

    valhallaComm.c header. Contains symbolic constants for communication types.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1118 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ debugger/private/external/valhallaComm.h
@@ -0,0 +1,15 @@
+
+#ifndef VALHALLACOMM_H
+#define VALHALLACOMM_H
+ 
+/* Opcodes for communication from valhalla to debugged process.
+ */
+
+#define VOP_SCANGROUPS          1 
+#define VOP_KILL                2 
+#define VOP_GETDATAMEM          3 
+#define VOP_GETINSTRUCTIONMEM   4 
+#define VOP_SETDATAMEM          5 
+#define VOP_GETDATAWORDS        6 
+      
+#endif
