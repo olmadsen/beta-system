41a41f8dd0a344d7480ee7b83b5ac21b028aed11
beta <beta@daimi.au.dk>
1994-08-19 14:02:33 +0000

    Lots of crts changes
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1300 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Misc.c
+++ betarun/CRUN/Misc.c
@@ -17,13 +17,13 @@ long *   savedRefSP;
 #endif
 
 #ifdef hppa
-void Return() {}
+void Return() 
+{}
 #endif
      
 #ifdef crts
-     void Return() 
-{
-}
+void Return() 
+{}
 #endif
 
 #ifdef sparc
@@ -207,6 +207,7 @@ void FailureExit()
 }
 #endif
 
+
 #ifdef RTDEBUG
 #ifndef hppa
 void CCk(ref(Object) r)
@@ -221,30 +222,54 @@ void CCk(ref(Object) r)
 #ifdef crts
 
 /* Global address registers */
-/*char *a0,*a1,*a2,*a3,*a4,*a7;*/
 long a0, a1, a2, a3, a4, a7; 
 long leave;
 
+void FailureExit()
+{
+  BetaError(StopCalledErr, (struct Object *) a0);
+}
+
 /* New RT routines for crts */
-static long *refstack[1000];
-static long reftop=0;
 
 void pushAdr(a)
      long *a;
 {
-  refstack[reftop] = a;
-  reftop=reftop+1;
+  *RefSP++ = (long)a;
 }
 
 long *popAdr()
 {
-  reftop=reftop-1;
-  return (long *)refstack[reftop];
+  return (long *) *(--RefSP);
 }  
 
-int CallBackPar(off)
-     int off;
+/* oldStackPtr is provited to the callback function by HandleCB which read the
+   value of the old stack pointer when called. It is then necessary that the
+   called BETA object is called with the parameters theObj, oldStackPtr, arg1,..
+*/
+int CallBackPar(long oldStackPtr, long parNo)
 {
+  register long retValue;
+
+  switch (parNo){
+  case 1:
+    asm("mov %%o0, %0": "=r" (retValue));
+  case 2:
+    asm("mov %%o1, %0": "=r" (retValue));
+  case 3:
+    asm("mov %%o2, %0": "=r" (retValue));
+  case 4:
+    asm("mov %%o3, %0": "=r" (retValue));
+  otherwise:
+    /* Ok, this is a hard one: oldStackPtr points to the previosly stack frame.
+       This frame contains the parameters of the C-function calling back to BETA.
+       On SPARC 23 words are used before the the parameters past the sixth (if any)
+       are pushed on the stack. The first parameter on the stack is then the
+       seventh parameter and so one.
+       */
+    retValue=*(long *)(oldStackPtr+23+(parNo-5));
+  }
+  return retValue;
 }
 
 void Trap()
@@ -291,16 +316,14 @@ signed long SignExtWord(signed short a)
 
 #include <setjmp.h>
 
-/* This is a new jumpbuf implementation (pool of jmpbuffers). 
- *  PRJ
- */
+/* Implementation of a pool of jmpbuffers. */
 
 #define maxNoInPool 10
 
 /* Basis type in pool of jump buffers */
 typedef struct jmpInfoElem {
   jmp_buf *jumpBuffer;
-  long refTop;
+  long * refTop;
   struct jmpInfoElem * nextElem;
 } jmpInfoElem;
 
@@ -355,6 +378,14 @@ void reallocJmpList()
 }
 
 
+/* 1. allocate a jmp_buf to be used by longjmp;   */
+/*    this is done below                          */
+/* 2. save stacktop of ref. stack;                */
+/*    this is currently NOT done                  */
+/* 3. save ref. for above info in a0[off]         */
+/*    currently only 1 is saved                   */
+/* 4. return jmp_buf                              */
+
 jmp_buf *GetJmpBuf(int addr, int off)
 {
   jmp_buf *tmp;
@@ -362,7 +393,7 @@ jmp_buf *GetJmpBuf(int addr, int off)
   if (jumpList->nextElem==NULL)
     reallocJmpList();
   tmp = jumpList->jumpBuffer;
-  jumpList->refTop = reftop; 
+  jumpList->refTop = RefSP; 
   *(jmpInfoElem **)(addr+off) = jumpList;
   jumpList=jumpList->nextElem;
   return tmp;
@@ -378,6 +409,12 @@ void FreeJmpBuf(int addr, int off)
   jumpList->nextElem = jumpList+1; /* Next element in list */
 }
 
+/* 1. get ref. to stateinfo from a1[off]  */
+/* 2. restore ref stack top               */
+/*    NOT done                            */
+/* 3. restore a0 using a1                 */
+/* 4. return saved jmp_buf                */
+/* 5. jmp_buf should really be deallocaed */
 
 jmp_buf *UseJmpBuf(int addr, int off)
 {
@@ -386,67 +423,10 @@ jmp_buf *UseJmpBuf(int addr, int off)
   
   a0 = (long)addr;                                
   p = *(jmpInfoElem **)(addr+off); 
-  reftop = p->refTop;                               
+  RefSP = p->refTop;                               
   CRTSjbp = p->jumpBuffer;
   jbuf = p->jumpBuffer;
   FreeJmpBuf(addr, off);
   return jbuf;
 }
-
-
-
-#if 0
-/* Old jump buffer implementation. PRJ */
-typedef struct jmpInfo {
-  jmp_buf *jumpBuffer;
-  long refTop;
-} jmpInfo;
-
-jmp_buf *CRTSjbp;
-
-/* 1. allocate a jmp_buf to be used by longjmp;   */
-/*    this is done below                          */
-/* 2. save stacktop of ref. stack;                */
-/*    this is currently NOT done                  */
-/* 3. save ref. for above info in a0[off]         */
-/*    currently only 1 is saved                   */
-/* 4. return jmp_buf                              */
-jmp_buf *GetJmpBuf(int addr, int off) 
-{
-  jmpInfo *p;
-  
-  p = (struct jmpInfo *)malloc(sizeof(jmpInfo));
-  p->jumpBuffer = (jmp_buf *)malloc(sizeof(jmp_buf)); /* 1 */
-  p->refTop = reftop;                                 /* 2 */
-  *(jmpInfo **)(addr+off) = p;                        /* 3 */
-  return p->jumpBuffer;                               /* 4 */
-}
-
-/* 1. get ref. to stateinfo from a1[off]  */
-/* 2. restore ref stack top               */
-/*    NOT done                            */
-/* 3. restore a0 using a1                 */
-/* 4. return saved jmp_buf                */
-/* 5. jmp_buf should really be deallocaed */
-jmp_buf *UseJmpBuf(int addr, int off)
-{
-  struct jmpInfo *p;
-  jmp_buf *tmp;
-  
-  a0 = (char *)addr;                                 /* 3 */
-  p = *(jmpInfo **)(addr+off);                       /* 1 */
-  reftop = p->refTop;                                /* 2 */
-  tmp = p->jumpBuffer;
-  free(p);                                           /* 5 */
-  CRTSjbp = tmp;           /* To be free'd later! */ /* 5 */
-  return tmp;                                        /* 4 */
-}
-
-
-void FreeJmpBuf(int a, int b)
-{
-  printf("FreeJumpBuf NYI\n");
-}
-
-#endif
 #endif /* crts */
