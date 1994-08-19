94e34b9d6678f56357b5f9c3716684de4a4f391d
beta <beta@daimi.au.dk>
1994-08-19 13:33:40 +0000

    Removed unnecessary setthisreg (snake)
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1282 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CallBack.c
+++ betarun/CRUN/CallBack.c
@@ -229,7 +229,9 @@ long CHandleCB(long a1, long a2, long a3, long a4, long FOR)
      * (ap) = arg#4, -4(ap) = arg#5,...
      * Unfortunately this doesn't work for doubles :-(
      */
-    setThisReg(0); /* To prevent pushing of garbage in the CallBackRoutine */
+    /* setThisReg(0); To prevent pushing of garbage in the CallBackRoutine.
+     * No longer necessary since registers are now cleared in 
+     * SnakeAdditions.S (HandleCB). SBRANDT 25/7/94 */
     setCallReg(theObj);
     retval = theObj->Proto->CallBackRoutine(a1, a2, a3, a4, &FOR - 128/4);
 
