1a8365aba1955dd529391b5eb2ecf9215d7c8197
beta <beta@daimi.au.dk>
1994-10-21 10:28:14 +0000

    Added machine depended code for SGI. Two new #ifdef: One for sun4s and sun4 and one for sgi. These ifdef contains the machine depended code.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1392 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/betarun.h
+++ betarun/betarun.h
@@ -5,7 +5,7 @@
  * Copyright (C) 1992-94 Mjolner Informatics Aps.
  * Written by Ole Lehrmann Madsen, Jacob Seligmann, and Peter Andersen.
  *
- * $Id: betarun.h,v 1.5 1994-08-29 09:23:02 beta Exp $
+ * $Id: betarun.h,v 1.6 1994-10-21 10:28:14 beta Exp $
  *
  */
 
@@ -86,15 +86,126 @@ struct PartObject {
 extern long *IOA;
 extern long *IOATop;
 
-/* extern char *a0,*a1,*a2,*a3,*a4,*a7; */
 extern long a0, a1, a2, a3, a4, a7; 
 extern long leave;
 extern void *CRTSjbp;
 
-extern void pushAdr(long *);
-extern long *popAdr();
-extern long CallBackPar(long off);
-extern void Trap();
+extern long * cIntStackPtr;
+extern double * cFloatStackPtr;
+extern long * RefSP;
+extern long * oldSP;
+
+extern void Trap(void);
+
+#if defined(sun4) || defined(sun4s)
+/* oldStackPtr is provided to the callback function by HandleCB which read the
+   value of the old stack pointer when called. It is then necessary that the
+   called BETA object is called with the parameters theObj, oldStackPtr, arg1,..
+*/
+#define CallBackPar1(dReg) (dReg = arg1)
+#define CallBackPar2(dReg) (dReg = arg2)
+#define CallBackPar3(dReg) (dReg = arg3)
+#define CallBackPar4(dReg) (dReg = arg4)
+#define CallBackPar5(dReg) (dReg = arg5)
+#define CallBackPar6(dReg) (dReg = arg6)
+
+#define CallBackParN(oldStackPtr, dReg, parNo)						\
+  /* Ok, this is a hard one: oldStackPtr points to the previosly stack frame.		\
+     This frame contains the parameters of the C-function calling back to BETA.		\
+     On SPARC 23 words are used before the the parameters past the sixth (if any)	\
+     are pushed on the stack. The first parameter on the stack is then the		\
+     seventh parameter and so one.							\
+   */											\
+  dReg = *(long *)(oldStackPtr+(23+parNo-7)*4);
+
+#define SaveCompState()
+
+#define RestoreCompState()
+
+#define SaveCurrSP()
+
+#endif /* SUN4 & SUN4S */
+
+#ifdef sgi
+
+#define SaveCurrSP() \
+  { register long * SP asm("$29"); \
+    asm("":"=r" (SP)); /* fool gcc */ \
+    oldSP = SP; \
+   }
+
+#define SaveCompState()				\
+  asm(" addiu $29, $29, -108 ");		\
+  asm(" sw $2, 4($29) ");			\
+  asm(" sw $3, 8($29) ");			\
+  asm(" sw $4, 12($29) ");			\
+  asm(" sw $5, 16($29) ");			\
+  asm(" sw $6, 20($29) ");			\
+  asm(" sw $7, 24($29) ");			\
+  asm(" sw $8, 28($29) ");			\
+  asm(" sw $9, 32($29) ");			\
+  asm(" sw $10, 36($29) ");			\
+  asm(" sw $11, 40($29) ");			\
+  asm(" sw $12, 44($29) ");			\
+  asm(" sw $13, 48($29) ");			\
+  asm(" sw $14, 52($29) ");			\
+  asm(" sw $15, 56($29) ");			\
+  asm(" sw $24, 60($29) ");			\
+  asm(" sw $25, 64($29) ");			\
+  asm(" sw $16, 68($29) ");			\
+  asm(" sw $17, 72($29) ");			\
+  asm(" sw $18, 76($29) ");			\
+  asm(" sw $19, 80($29) ");			\
+  asm(" sw $20, 84($29) ");			\
+  asm(" sw $21, 88($29) ");			\
+  asm(" sw $22, 92($29) ");			\
+  asm(" sw $23, 96($29) ");			\
+  asm(" sw $28, 100($29) ");			\
+  asm(" sw $30, 104($29) ");			\
+  asm(" sw $31, 108($29) ");                    \
+  SaveCurrSP();
+
+#define RestoreCompState()			\
+  asm(" lw $2, 4($29) ");			\
+  asm(" lw $3, 8($29) ");			\
+  asm(" lw $4, 12($29) ");			\
+  asm(" lw $5, 16($29) ");			\
+  asm(" lw $6, 20($29) ");			\
+  asm(" lw $7, 24($29) ");			\
+  asm(" lw $8, 28($29) ");			\
+  asm(" lw $9, 32($29) ");			\
+  asm(" lw $10, 36($29) ");			\
+  asm(" lw $11, 40($29) ");			\
+  asm(" lw $12, 44($29) ");			\
+  asm(" lw $13, 48($29) ");			\
+  asm(" lw $14, 52($29) ");			\
+  asm(" lw $15, 56($29) ");			\
+  asm(" lw $24, 60($29) ");			\
+  asm(" lw $25, 64($29) ");			\
+  asm(" lw $16, 68($29) ");			\
+  asm(" lw $17, 72($29) ");			\
+  asm(" lw $18, 76($29) ");			\
+  asm(" lw $19, 80($29) ");			\
+  asm(" lw $20, 84($29) ");			\
+  asm(" lw $21, 88($29) ");			\
+  asm(" lw $22, 92($29) ");			\
+  asm(" lw $23, 96($29) ");			\
+  asm(" lw $28, 100($29) ");			\
+  asm(" lw $30, 104($29) ");			\
+  asm(" lw $31, 108($29) ");			\
+  asm(" addiu $29, $29, 108 ");
+
+#define CallBackPar1(dReg) (dReg = arg1)
+#define CallBackPar2(dReg) (dReg = arg2)
+#define CallBackPar3(dReg) (dReg = arg3)
+#define CallBackPar4(dReg) (dReg = arg4)
+#define CallBackPar5(dReg) (dReg = arg5)
+#define CallBackPar6(dReg) (dReg = arg6)
+
+#define CallBackParN(oldStackPtr, dReg, parNo)	\
+    dReg = *(long *)(oldStackPtr+(parNo-1)*4);
+
+#endif /* sgi */
 
 #include <setjmp.h>
 jmp_buf *UseJmpBuf(int,int);
