da2996a145fd802996a7b19b1c8969eab2b4f405
beta <beta@daimi.au.dk>
1994-10-21 11:14:00 +0000

    New code for crts. In crts components are removed from the system stack and saved in a stack object whenever a component is suspended.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1408 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Suspend.c
+++ betarun/CRUN/Suspend.c
@@ -9,9 +9,104 @@
 #include "crun.h"
 
 #ifdef crts
-ParamThis(struct Component *, Susp)
+extern long a0;
+struct Component * Susp(struct Object * this)
+/* ParamThis(struct Component *, Susp) */
 {
-  fprintf(output, "Suspend NYI\n");
+  struct Component * caller;
+  struct Component * called;
+  long sizeToMove, sizeOfRefStack;
+  struct StackObject * theStackObj;
+  long suspStackSize;
+  char * suspSaveArea;
+  long * newSP, *SP, *FP, RA;
+  
+  /* Strategy:
+   *  (1) Save Susp stack area on the heap.
+   *  (2) Pack current component block to stack.
+   *  (3) Pack current reference stack at the same stack object as above.
+   *  (4) Restore the suspend area at top of the stack at a location above the new component
+   *  (5) Move the new component block from the stack object to the stack below the Susp area.
+   *  (6) Restore the reference stack from the stack object.
+   *  (7) Update stackpointer and framepointer according to the new stack  layout and return
+   *      to the new component.
+   */
+  
+  getret_Susp(RA); /* Fetch return address */ 
+
+  GCable_Entry();
+  Ck(this);
+  push(this); /* Push this (=a0) to reference stack */
+
+  if (ActiveCallBackFrame)
+    BetaError(CompCallBackErr, this);
+  ActiveComponent->CallerLSC = RA; /* Store return value in CallerLSC */
+
+  SaveCompState_sparc(); /* Save current registers to stack */
+  /* Save the Susp part of the stack */
+  GetStackPointer(SP);
+  GetFramePointer(FP);
+  suspStackSize = ((long)FP-(long)SP)/4;
+  suspSaveArea = malloc(suspStackSize*4);
+  memcpy(suspSaveArea, SP+1, suspStackSize*4);
+
+  /* Pack current component block to stack */
+  theStackObj = ActiveComponent->StackObj;
+  sizeToMove = ((long)baseStackPtr-(long)FP)/4; /* Number of longs on run-time stack */
+  sizeOfRefStack = ((long)RefSP - (long)baseRefSP)/4; /* Number of longs on reference stack */
+  if ( ((long)theStackObj == 0) || ((long)theStackObj == -1) || 
+      ((sizeToMove+sizeOfRefStack) > theStackObj->BodySize) ){
+    push(this);
+    theStackObj = AlloSO(sizeToMove+sizeOfRefStack+1);
+    pop(this);
+    AssignReference((long *)&ActiveComponent->StackObj, (struct Item *) theStackObj);
+  }
+  theStackObj->StackSize = sizeToMove; /* Saved as longs, as GC expect this! */
+  theStackObj->Body[0] = (long) FP;
+  memcpy(theStackObj->Body+1, FP+1, sizeToMove*4);
+
+  /* Pack current reference stack to stack. */
+  memcpy(theStackObj->Body+1+sizeToMove, baseRefSP, sizeOfRefStack*4);
+    
+  caller = ActiveComponent->CallerComp;
+  if (caller == 0){
+    /* This is the case if the basic component is suspended. Leave the program in a nice way. */
+    BetaExit(0);
+  }
+  if (caller->StackObj == 0){
+    free(suspSaveArea);
+    BetaError(CompTerminatedErr, this);
+  }
+  theStackObj = caller->StackObj;
+  sizeToMove = theStackObj->StackSize; /* sizeToMove is now number of longs! */
+
+  /* Restore the Susp part of the stack */
+  newSP = baseStackPtr-sizeToMove;
+  memcpy(newSP-suspStackSize+1, suspSaveArea, suspStackSize*4);
+  SetStackPointer(newSP-suspStackSize);
+  SetFramePointer(newSP);
+
+  /* Activate new componont: caller */
+  memcpy(newSP+1, theStackObj->Body+1, sizeToMove*4);
+  
+  /* Restore reference stack */
+  sizeOfRefStack = theStackObj->BodySize-theStackObj->StackSize-1; /* In longs! */
+  memcpy(baseRefSP, theStackObj->Body+1+sizeToMove, sizeOfRefStack*4);
+  RefSP = baseRefSP + sizeOfRefStack;
+  a0=(long)popAdr();
+
+  ActiveComponent->CallerObj = (struct Object *)0;
+  ActiveComponent->CallerComp = (struct Component *)0;
+  
+  /* Change active component */
+  called = ActiveComponent;
+  ActiveComponent = caller;
+  free(suspSaveArea);
+
+  setret_Susp(ActiveComponent->CallerLSC); 
+
+  return called;  
+
 }
 #endif
 
