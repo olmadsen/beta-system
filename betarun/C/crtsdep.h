64a53abb6c0f34a28835af020fa2a9ec13570d71
beta <beta@daimi.au.dk>
1994-10-21 10:31:57 +0000

    A lot of changes. Basically introduction of #ifdefs containing machine depended macroes and functions. For now two ifdefs: sgi and sun4/sun4s.
    Also included prototype for some C-runtime functions:
    AlloRR, AlloVR1, AlloVR2, AlloVR4, AlloVR8, CopyT, AlloSO, initJmpPool
    
    
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1394 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/crtsdep.h
+++ betarun/C/crtsdep.h
@@ -1,8 +1,8 @@
 /*
  * BETA RUNTIME SYSTEM, Copyright (C) 1992-94 Mjolner Informatics Aps.
  * crtsdep.h
- * by Peter Ryberg Jensen
- * $Id: crtsdep.h,v 1.2 1994-08-24 13:25:14 beta Exp $
+ * by Ole Lehrmann Madsen, Peter Andersen and Peter Ryberg Jensen
+ * $Id: crtsdep.h,v 1.3 1994-10-21 10:31:57 beta Exp $
  */
 
 #define ParamOriginProto(retType,name)			 \
@@ -12,15 +12,12 @@
 #define ParamObjOffRange(retType, name)			 \
   retType name(struct Object *theObj,			         \
 	 unsigned offset, /* in bytes */		 \
-	 unsigned range				         \
+	 int range				         \
 	 )
 #define ParamStruc(retType, name)                              \
   retType name(struct Structure *struc)
 #define ParamThis(retType,name)                                \
   retType name(struct Object *this)
-
-#define setret(newret)
-#define getret(saved)
 
 #define RETURN(v) return v
 #define GCable_Entry()
@@ -30,10 +27,17 @@ extern long a1;
 #define CallBetaEntry(entry,item)	                \
     a1=(long)(item); (* (void (*)()) (entry))()
 
-extern void pushAdr(long *a);
-extern long *popAdr();
-#define push(v) pushAdr((long*)v)
-#define pop(v) (long*)v=popAdr()
+/* extern void pushAdr(long *a);
+   extern long *popAdr();*/
+
+#define pushAdr(a) \
+  *RefSP++ = (long)a
+
+#define popAdr() \
+  (long *) *(--RefSP)
+
+#define push(v) *RefSP++ = (long) v
+#define pop(v) v = (void *) *(--RefSP)
 
 #define Protect(var, code)				\
   push(var);						\
@@ -46,18 +50,18 @@ extern long *popAdr();
   pop(v2); pop(v1);
 
 #define PushGCRegs()				\
-  pushAdr((long *) a0);				\
-  pushAdr((long *) a1);				\
-  pushAdr((long *) a2);				\
-  pushAdr((long *) a3);				\
-  pushAdr((long *) a4); 
+  *RefSP++=a0;					\
+  *RefSP++=a1;					\
+  *RefSP++=a2;					\
+  *RefSP++=a3;					\
+  *RefSP++=a4;
 
-#define PopGCRegs()			\
-  a4 = (long) popAdr();                 \
-  a3 = (long) popAdr();			\
-  a2 = (long) popAdr();			\
-  a1 = (long) popAdr();			\
-  a0 = (long) popAdr();
+#define PopGCRegs()			        \
+  a4 = *(--RefSP);				\
+  a3 = *(--RefSP);				\
+  a2 = *(--RefSP);				\
+  a1 = *(--RefSP);				\
+  a0 = *(--RefSP);
 
 #define DeclReference1(type, name) type name
 #define DeclReference2(type, name) type name
@@ -67,15 +71,186 @@ extern long *popAdr();
 #define FetchThis()
 #define FetchThisComp()
 
-
-/* Various machine instructions used for callbacks in Callback.c */
-/* SPARC machine code: */
-#define MOV_O7_G1 0x8210000f
-#define MK_CALL(p,f) \
-  (*(long *)p = 0x40000000| (((unsigned) ((char*)f-(char*)p)) >> 2))
-#define NOP 0x1000000
-
 /* Points to top of cStack. Used for nested external calls */
 extern long *cIntStackPtr;
 extern double *cFloatStackPtr;
 
+extern struct RefRep *		AlloRR(struct Object* theObj, 
+				       unsigned offset, 
+				       int range);
+extern struct ValRep *		AlloVR1(struct Object* theObj, 
+					unsigned offset, 
+					int range);
+extern struct ValRep *		AlloVR2(struct Object* theObj, 
+					unsigned offset, 
+					int range);
+extern struct ValRep *		AlloVR4(struct Object* theObj, 
+					unsigned offset,
+					int range);
+extern struct ValRep *		AlloVR8(struct Object* theObj, 
+					unsigned offset, 
+					int range);
+extern void 			CopyT(char *asciz, 
+				      struct Item *theItem,
+				      unsigned offset);
+
+extern struct StackObject *     AlloSO(unsigned size);
+
+extern void initJmpPool(void);
+
+
+/************************************************************************************/
+/*                           Machine dependent makroes                              */
+/************************************************************************************/
+
+#if defined(SUN4) || defined(SUN4S)
+
+extern long * oldSP;
+
+/* For callbacks in Callback.c */
+
+#define MK_CALL(f) \
+  CBFATop->code[0] = 0x9de3bf90; /* save %sp, -112, %sp */ \
+  CBFATop->code[1] = 0x11000000 | (((unsigned) &oldSP) & 0xfffffc00)>>10; /* sethi %hi(oldSP), %o0 */ \
+  CBFATop->code[2] = 0xfc222000 | (((unsigned) &oldSP) & 0x3ff); /* st %fp, [%o0+lo(oldSP) */\
+  CBFATop->code[3] = 0x90100018; /* mov %i0, %o0 */ \
+  CBFATop->code[4] = 0x92100019; /* mov %i1, %o1 */ \
+  CBFATop->code[5] = 0x9410001a; /* mov %i2, %2 */ \
+  CBFATop->code[6] = 0x9610001b; /* mov %i3, %o3 */ \
+  CBFATop->code[7] = 0x9810001c; /* mov %i4, %o4 */ \
+  CBFATop->code[8] = 0x40000000 | (((unsigned) ((char*)f-(char*)&(CBFATop->code[8]))) >> 2); /* call HandleCB */\
+  CBFATop->code[9] = 0x9a10001d; /* mov %i5, %o5 */ \
+  CBFATop->code[10] = 0x81c7e008; /* ret */ \
+  CBFATop->code[11] = 0x91e80008; /* restore %go, %o0, %o0 */
+
+
+#define NUMBER_TO_STRUCT (9*4)
+
+#define FlushCache	 					\
+  __asm__("iflush %0"::"r" (&CBFATop->code[0]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[1]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[2]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[3]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[4]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[5]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[6]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[7]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[8]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[9]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[10]));		\
+  __asm__("iflush %0"::"r" (&CBFATop->code[11]));		\
+
+/* Used for components in Attach.c and Suspend.c */
+register long * StackPointer asm("%sp");
+register long * FramePointer asm("%fp");
+register long   retAddress   asm("%i7");
+
+#define setret(newret) (retAddress = (long) (newret))
+#define setret_Att(newret) (retAddress = (long) (newret))
+#define getret(saved) (saved = retAddress)
+#define getret_CB(saved) (saved = retAddress)
+ 
+#define SetStackPointer(newSP) (StackPointer = newSP)
+#define SetFramePointer(newFP) (FramePointer = newFP)
+
+#define GetStackPointer(SP) (SP=StackPointer)
+#define GetFramePointer(FP) (FP=FramePointer)
+#define GetFramePointer_Att(FP) (FP=FramePointer)
+
+#define SaveCompState_sparc()\
+  asm("ta 3");
+
+#endif /* SUN4 */
+
+
+#ifdef SGI
+
+#include <sys/cachectl.h>
+
+extern long * oldSP;
+
+/* For callbacks in Callback.c */
+
+#define MK_CALL(f)\
+  CBFATop->code[0] = 0x27bdffc8; /* addiu $sp,$sp,-56 */ \
+  CBFATop->code[1] = 0xafbc0018; /* sw $gp,24($sp) */ \
+  CBFATop->code[2] = 0xafbf0030; /* sw $ra,48($sp) */ \
+  CBFATop->code[3] = 0xafb7002c; /* sw $s7,44($sp) */ \
+  CBFATop->code[4] = 0xafbc0028; /* sw $gp,40($sp) */ \
+  CBFATop->code[5] = 0xafa40038; /* sw $ao,56($sp) */ \
+  CBFATop->code[6] = 0xafa5003c; /* sw $a1,60($sp) */ \
+  CBFATop->code[7] = 0xafa60040; /* sw $a2,64($sp) */ \
+  CBFATop->code[8] = 0xafa70044; /* sw $a3,72($sp) */ \
+  CBFATop->code[9] = 0x3c170000 | (((unsigned) &oldSP & 0xffff0000)>>16); /* lui $s7,hi(oldSP)*/\
+  CBFATop->code[10] = 0x36f70000 | ((unsigned) &oldSP & 0xffff); /* ori $s7,$s7,lo(oldSP) */ \
+  CBFATop->code[11] = 0x03a02021; /* move $a0,$sp */ \
+  CBFATop->code[12] = 0x24840038; /* addiu $a0,$a0,56 */ \
+  CBFATop->code[13] = 0xaee40000; /* sw $a0,0($s7) */ \
+  CBFATop->code[14] = 0x8fa20048; /* lw $v0,72($sp) */ \
+  CBFATop->code[15] = 0x00000000; /* nop */ \
+  CBFATop->code[16] = 0xafa20010; /* sw $v0,16($sp) */ \
+  CBFATop->code[17] = 0x8fa2004c; /* lw $v0,76($sp) */ \
+  CBFATop->code[18] = 0x00000000; /* nop */ \
+  CBFATop->code[19] = 0xafa20014; /* sw $v0,20($sp) */ \
+  CBFATop->code[20] = 0x03a0b821; /* move $s7,$sp */ \
+  CBFATop->code[21] = 0x3c190000 | (((unsigned)f & 0xffff0000)>>16); /* lui $t9,hi(HandleCB) */ \
+  CBFATop->code[22] = 0x37390000 | ((unsigned)f & 0xffff); /* ori $t9,$t9,lo(HnadleCB) */ \
+  CBFATop->code[23] = 0x8fa40038; /* lw a0, 56($sp) */ \
+  CBFATop->code[24] = 0x00000000; /* nop */ \
+  CBFATop->code[25] = 0x0320f809; /* jarl $t9 */ \
+  CBFATop->code[26] = 0x00000000; /* nop */ \
+  CBFATop->code[27] = 0x8efc0018; /* lw $gp,24($s7) */ \
+  CBFATop->code[28] = 0x00000000; /* nop */ \
+  CBFATop->code[29] = 0x02e0e821; /* move $sp,$s7 */ \
+  CBFATop->code[30] = 0x8fbf0030; /* lw $ra,48($sp) */ \
+  CBFATop->code[31] = 0x00000000; /* nop */ \
+  CBFATop->code[32] = 0x8fb7002c; /* lw $s7,44($sp) */ \
+  CBFATop->code[33] = 0x00000000; /* nop */ \
+  CBFATop->code[34] = 0x27bd0038; /* addiu $sp,$sp,56 */ \
+  CBFATop->code[35] = 0x03e00008; /* jr $ra */ \
+  CBFATop->code[36] = 0x00000000; /* nop */
+
+#define NUMBER_TO_STRUCT (28*4)
+
+/* _flush_cache(addr, number_of_bytes_to_flush,cache)
+   where cache is either DCACHE, ICACHE or BCACHE (both caches)
+*/
+#define FlushCache \
+  _flush_cache((char *)(&CBFATop->code[0]), 36*4, DCACHE)
+
+/* Used for components in Attach.c and Suspend.c */
+
+#define setret(newret) (*(oldSP-2) = newret)
+#define setret_Susp(newret) (*(oldSP-1) = newret)
+#if 0
+#define setret_Att(newret) \
+  asm("sw %0, 64($29)"::"r" (newret));
+#endif
+
+#define getret(saved) (saved = *(oldSP-2))
+#define getret_Susp(saved) (saved = *(oldSP-1))
+#if 0
+#define getret_Att(saved) \
+  asm("lw %0, 64($29)":"=r" (saved));
+#endif
+
+#define getret_CB(saved) \
+  asm("move %0, $31":"=r" (saved));
+
+#define SetStackPointer(newSP) \
+  asm("move $sp, %0"::"r" (newSP)); \
+  asm("lw $gp,16($sp)");
+
+#define GetStackPointer(SP) \
+  asm("move %0, $sp":"=r" (SP));
+
+#define SetFramePointer(newFP) (oldSP = newFP)
+#define GetFramePointer(FP) (FP = oldSP)
+
+#define GetFramePointer_Att(FP) \
+  GetStackPointer(FP); \
+  FP=FP+72/4;
+
+#define SaveCompState_sparc()
+
+#endif /* SGI */
