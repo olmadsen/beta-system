742fe60ea67303f579d5c4cf3dd47cdf59b38fcf
beta <beta@daimi.au.dk>
1995-01-13 11:45:05 +0000

    No need for extra wrapper around doGC anymore
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1606 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/PerformGC.c
+++ betarun/CRUN/PerformGC.c
@@ -49,12 +49,30 @@ void doGC() /* The one called from IOA(c)alloc */
     asmemptylabel(EndGC);
 }
 
+#ifdef sparc
+/* IOA(c)alloc is now in-lined.
+ * That is, ProcessStack does not skip the regwin of IOA(c)alloc,
+ * and thus there is no need for the extra regwin around doGC anymore.
+ */
+#ifdef sun4s
+asmlabel(DoGC,
+	 "sethi  %hi(ReqObjectSize), %g1\n"
+	 "ba     doGC\n"
+         "clr    [ %g1 + %lo(ReqObjectSize) ]\n");
+#else
+asmlabel(_DoGC,
+	 "sethi  %hi(_ReqObjectSize), %g1\n"
+	 "ba     _doGC\n"
+         "clr    [ %g1 + %lo(_ReqObjectSize) ]\n");
+#endif
+#else
 void DoGC() /* The one called directly from betaenv */
 { 
   /* This wrapper adds an activation record around doGC, which skips two AR's */
   ReqObjectSize = 0;
   doGC();
 }
+#endif
 
 #ifdef RTDEBUG
 void DoGC_UseCk()
