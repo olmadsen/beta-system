08ad4f7c19e33909d3267f0406933ed1cbc85699
beta <beta@daimi.au.dk>
1995-02-03 11:03:54 +0000

    Initial revision
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1705 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/CRUN/IOAAlloc.h
@@ -0,0 +1,93 @@
+
+/* Allocation in IOA heap */
+/* GC/PerformGC.c: Not declared in function.h, doGC should only be 
+ * called from IOA(c)lloc or DoGC.
+ */
+extern void doGC();
+
+
+static 
+#ifdef __GNUC__
+inline 
+#endif
+char *IOAalloc(unsigned size)
+{
+  register char *p;
+  
+  /*GCable_Entry();*/
+  
+  /*fprintf(output, "IOAalloc: IOATop=0x%x, size=0x%x\n", IOATop, size);*/
+  
+  DEBUG_CODE(Claim(size>0, "IOAalloc: size>0"));
+#if (defined(sparc) || defined(hppa) || defined(crts))
+  DEBUG_CODE(Claim( ((long)size&7)==0 , "IOAalloc: (size&7)==0"));
+  DEBUG_CODE(Claim( ((long)IOATop&7)==0 , "IOAalloc: (IOATop&7)==0"));
+#endif
+  
+  while ((char *)IOATop+size > (char *)IOALimit) {
+    ReqObjectSize = size / 4;
+    doGC();
+  }
+  
+  p = (char *)IOATop;
+#ifdef hppa
+  /* setIOATopoffReg(getIOATopoffReg() + size); */
+  IOATop = (long*)((long)IOATop+size);
+#endif
+#ifdef sparc
+  IOATopoff += size;
+#endif
+#ifdef crts
+  IOATop = (long*)((long)IOATop+size);
+#endif
+  
+  return p;
+}
+
+static
+#ifdef __GNUC__
+inline 
+#endif
+char *IOAcalloc(unsigned size)
+{
+  register char *p;
+  
+  /*GCable_Entry();*/
+  
+  /*fprintf(output, "IOACalloc: IOATop=0x%x, size=0x%x\n", IOATop, size);*/
+  
+  DEBUG_CODE(Claim(size>0, "IOACalloc: size>0"));
+#if (defined(sparc) || defined(hppa) || defined(crts))
+  DEBUG_CODE(Claim( ((long)size&7)==0 , "IOAcalloc: (size&7)==0"));
+  DEBUG_CODE(Claim( ((long)IOATop&7)==0 , "IOAcalloc: (IOATop&7)==0"));
+#endif
+  
+  while ((char *) IOATop+size > (char *)IOALimit) {
+    ReqObjectSize = size / 4;
+    doGC();
+  }
+  
+  p = (char *)IOATop;
+#ifdef hppa
+  /*setIOATopoffReg(getIOATopoffReg() + size);*/
+  IOATop = (long*)((long)IOATop+size);
+#endif
+#ifdef sparc
+  IOATopoff += size;
+#endif
+#ifdef crts
+  IOATop = (long*)((long)IOATop+size);
+#endif
+  
+  /* Not needed anymore since IOA is cleared after IOAGc.
+   * YES still needed. The memset solution turned out to
+   * be slower or at best marginally faster than using 
+   * long_clear.
+   */
+  long_clear(p, size);
+#ifdef RTDEBUG
+  zero_check(p, size);
+#endif
+  
+  return p;
+}
