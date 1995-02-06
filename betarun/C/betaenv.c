0f91301948e5054dfd2857379c0c9d0dbf584acb
beta <beta@daimi.au.dk>
1995-02-06 21:30:06 +0000

    Changed realloc to REALLOC.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1722 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/betaenv.c
+++ betarun/C/betaenv.c
@@ -82,7 +82,7 @@ void SetupArgValues(int ret, int inst, int prev, char *cmd, int show)
     /* At start of argument or EOT */
     if (*p) {
       /* Make room for argument */
-      ArgVector = (char**)realloc(ArgVector, (ArgCount+1)*sizeof(char*));
+      ArgVector = (char**)REALLOC(ArgVector, (ArgCount+1)*sizeof(char*));
       ArgVector[ArgCount++] = p;
       /* Skip argument */
       while (*p && *p != ' ' && *p != '\t')
