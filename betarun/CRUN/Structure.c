bae8e229f8f20ae5d7fa3db6829d348215d67d91
beta <beta@daimi.au.dk>
1995-02-15 08:26:35 +0000

    moved sparc_allosi to misc.c
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@1772 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/Structure.c
+++ betarun/CRUN/Structure.c
@@ -115,22 +115,6 @@ ref(Structure) ObjS(ref(Object) theObj)
   return newStruct;
 }
 
-#ifdef sparc
-/* Functions used to call RT routines directly from C.
- * Needed because %i1 in calling regwin is destroyed by (C)AlloSI
- * Must be here before AlloSI to prevent gcc from inlining it.
- */
-
-struct Item *SPARC_AlloSI(struct Structure *s, int i1, int i2, int i3, int i4)
-{
-  struct Item *CAlloSI(struct Structure *s, int i1, int i2, int i3, int i4);
-  GCable_Entry();
-  return CAlloSI(s, i1, i2 ,i3, i4);
-  GCable_Exit(1);
-}
-
-#endif
-
 ParamStruc(struct Item *, AlloSI)
 {
   struct Item *ss;
