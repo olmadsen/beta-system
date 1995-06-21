253734026116032b5857c122b04cc4187259f986
beta <beta@daimi.au.dk>
1995-06-21 13:07:38 +0000

    Initial revision
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@2144 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/CRUN/AllocateObjectRep.c
@@ -0,0 +1,208 @@
+/*
+ * BETA C RUNTIME SYSTEM, Copyright (C) 1992-94 Mjolner Informatics Aps.
+ * by Peter Andersen.
+ */
+
+#define GCable_Module
+
+#include "beta.h"
+#include "crun.h"
+
+ParamObjOriginProtoOffRange(ref(ObjectRep), AlloORG)
+{
+  DeclReference1(struct ObjectRep *, theRep);
+  struct Item *item;
+  GCable_Entry();
+#ifdef hppa
+  range  = getD1reg();
+  origin = getOriginReg();
+  proto  = getCallReg();
+  theObj = getThisReg();
+  offset = getD0reg();
+#endif
+
+  Ck(theObj);
+  Protect(theObj, 
+	  theRep = cast(ObjectRep)IOAcalloc(StatObjectRepSize(range,proto)));
+  Ck(theObj);
+
+  theRep->Proto = ObjectRepPTValue;
+  theRep->GCAttr = 1;
+  theRep->LowBorder = 1;
+  theRep->HighBorder = range;
+  theRep->iProto = proto;
+  theRep->iOrigin = origin;
+  /* theRep->isComp = 0; not needed because of calloc */
+  /* theRep->isDynamic = 0; not needed because of calloc */
+
+  SaveVar(theObj);
+
+  while(--range>=0){
+    item=(struct Item *)((long)(&theRep->Body) + range*ItemSize(proto));
+    setup_item(item, proto, origin); 
+    Protect(theRep, CallBetaEntry(proto->GenPart,item));
+  }
+
+  RestoreVar(theObj);
+
+  AssignReference((long *)((char *)theObj + offset), cast(Item) theRep);
+
+  RETURN(theRep);
+}
+
+ParamObjOriginProtoOffRange(ref(ObjectRep), AlloORGC)
+{
+  DeclReference1(struct ObjectRep *, theRep);
+  struct Component *comp;
+  GCable_Entry();
+#ifdef hppa
+  range  = getD1reg();
+  origin = getOriginReg();
+  proto  = getCallReg();
+  theObj = getThisReg();
+  offset = getD0reg();
+#endif
+
+  Ck(theObj);
+  Protect(theObj, 
+	  theRep = cast(ObjectRep)IOAcalloc(StatCompRepSize(range,proto)));
+  Ck(theObj);
+
+  theRep->Proto = ObjectRepPTValue;
+  theRep->GCAttr = 1;
+  theRep->LowBorder = 1;
+  theRep->HighBorder = range;
+  theRep->iProto = proto;
+  theRep->iOrigin = origin;
+  theRep->isComp = 1;
+  /* theRep->isDynamic = 0; not needed because of calloc */
+
+  SaveVar(theObj);
+
+  while(--range>=0){
+    comp=(struct Component *)((long)(&theRep->Body) + range*ComponentSize(proto));
+
+    comp->Proto = ComponentPTValue;
+    comp->GCAttr = 1;
+    comp->StackObj = cast(StackObject) 0;
+    comp->CallerObj = cast(Object) 0;
+    comp->CallerComp = cast(Component) 0;
+    comp->CallerLSC = 0;
+
+    setup_item(cast(Item) &comp->Body, proto, origin);
+    (cast(Item) &comp->Body)->GCAttr = -((headsize(Component))/4);
+
+    Protect(theRep, CallBetaEntry(proto->GenPart,&comp->Body));
+  }
+
+  RestoreVar(theObj);
+
+  AssignReference((long *)((char *)theObj + offset), cast(Item) theRep);
+
+  RETURN(theRep);
+}
+
+ParamObjOriginProtoOffRange(ref(ObjectRep), AlloORR)
+{
+  DeclReference1(struct ObjectRep *, theRep);
+  struct Item *item;
+  GCable_Entry();
+#ifdef hppa
+  range  = getD1reg();
+  origin = getOriginReg();
+  proto  = getCallReg();
+  theObj = getThisReg();
+  offset = getD0reg();
+#endif
+
+  Ck(theObj);
+  Protect(theObj, 
+	  theRep = cast(ObjectRep)IOAcalloc(DynObjectRepSize(range)));
+  Ck(theObj);
+
+  theRep->Proto = ObjectRepPTValue;
+  theRep->GCAttr = 1;
+  theRep->LowBorder = 1;
+  theRep->HighBorder = range;
+  theRep->iProto = proto;
+  theRep->iOrigin = origin;
+  /* theRep->isComp = 0; not needed because of calloc */
+  theRep->isDynamic = 1;
+
+  SaveVar(theObj);
+
+  while(--range>=0){
+#ifdef sparc
+    Protect(theRep, 
+	    item = SPARC_AlloI(cast(Object) theRep->iOrigin, 0, theRep->iProto, 0, 0));
+#endif
+#ifdef hppa
+    Protect(theRep, 
+	    item = CAlloI(cast(Object) theRep->iOrigin, theRep->iProto));
+#endif
+#ifdef crts
+    Protect(theRep, 
+	    item = AlloI(cast(Object) theRep->iOrigin, theRep->iProto));
+#endif
+    AssignReference((long *)((long)&theRep->Body + range*4), item);
+  }
+
+  RestoreVar(theObj);
+
+  AssignReference((long *)((char *)theObj + offset), cast(Item) theRep);
+
+  RETURN(theRep);
+}
+
+ParamObjOriginProtoOffRange(ref(ObjectRep), AlloORRC)
+{
+  DeclReference1(struct ObjectRep *, theRep);
+  struct Component *comp;
+  GCable_Entry();
+#ifdef hppa
+  range  = getD1reg();
+  origin = getOriginReg();
+  proto  = getCallReg();
+  theObj = getThisReg();
+  offset = getD0reg();
+#endif
+
+  Ck(theObj);
+  Protect(theObj, 
+	  theRep = cast(ObjectRep)IOAcalloc(DynObjectRepSize(range)));
+  Ck(theObj);
+
+  theRep->Proto = ObjectRepPTValue;
+  theRep->GCAttr = 1;
+  theRep->LowBorder = 1;
+  theRep->HighBorder = range;
+  theRep->iProto = proto;
+  theRep->iOrigin = origin;
+  theRep->isComp = 0; 
+  theRep->isDynamic = 1;
+
+  SaveVar(theObj);
+
+  while(--range>=0){
+#ifdef sparc
+    Protect(theRep, 
+	    comp = SPARC_AlloC(cast(Object) theRep->iOrigin, 0, theRep->iProto, 0, 0));
+#endif
+#ifdef hppa
+    Protect(theRep, 
+	    comp = CAlloC(cast(Object) theRep->iOrigin, theRep->iProto));
+#endif
+#ifdef crts
+    Protect(theRep, 
+	    comp = AlloC(cast(Object) theRep->iOrigin, theRep->iProto));
+#endif
+    AssignReference((long *)((long)&theRep->Body + range*4), 
+		    (struct Item *)comp);
+  }
+
+  RestoreVar(theObj);
+
+  AssignReference((long *)((char *)theObj + offset), cast(Item) theRep);
+
+  RETURN(theRep);
+}
