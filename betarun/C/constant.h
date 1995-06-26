a65e069cdb0b47a4fbff49113b79e2373d635508
beta <beta@daimi.au.dk>
1995-06-26 13:43:25 +0000

    Defined constants for 4 different objectrepetitions. Removed unused XXXType constants
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@2180 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/constant.h
+++ betarun/C/constant.h
@@ -10,28 +10,18 @@
 #define WordRepPTValue         ((struct ProtoType *) -7)
 #define DoubleRepPTValue       ((struct ProtoType *) -8)
 #define DopartObjectPTValue    ((struct ProtoType *) -9)
-#define ObjectRepPTValue       ((struct ProtoType *) -10)
+#define StatItemRepPTValue     ((struct ProtoType *) -10)
+#define StatCompRepPTValue     ((struct ProtoType *) -11)
+#define DynItemRepPTValue      ((struct ProtoType *) -12)
+#define DynCompRepPTValue      ((struct ProtoType *) -13)
 
 #define MaxPTValue             -1
-#define MinPTValue             -10
+#define MinPTValue             -13
 
 #ifdef UseRefStack
 /* Used to mark external activation part on reference stack */
 #define ExternalMarker	       -10
 #endif
-
-
-#define ItemType                1
-#define ComponentType           2
-#define StackObjectType         3
-#define ValRepType              4
-#define RefRepType              5
-#define StructureType           6
-#define ByteRepType             7
-#define WordRepType             8
-#define DoubleRepType           9
-#define DopartType             10
-#define ObjectRepType          11
 
 #define IOAMaxAge             128
 
