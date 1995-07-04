12a5949bed541bd39739747d33a0a52d2e04377f
beta <beta@daimi.au.dk>
1995-07-04 12:29:38 +0000

    USE of NEWREP macro
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@2225 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/CopySliceValRep.c
+++ betarun/CRUN/CopySliceValRep.c
@@ -8,6 +8,7 @@
  */
 
 #define REP ((struct ObjectRep *)theRep)
+#define NEWREP ((struct ObjectRep *)newRep)
 
 #ifdef hppa
 register long _dummy8 asm("%r15"); /* really tmp data 1 */
@@ -99,15 +100,15 @@ void CCopySVR(ref(ValRep) theRep,
     newRep->HighBorder = range;
 
     if (isObjectRep(theRep)){
-      ((struct ObjectRep *)newRep)->iOrigin = REP->iOrigin;
-      ((struct ObjectRep *)newRep)->iProto = REP->iProto;
+      NEWREP->iOrigin = REP->iOrigin;
+      NEWREP->iProto = REP->iProto;
     }
 
     /* Copy the body part of the repetition. */
     switch ( (long) theRep->Proto){
       case (long) ByteRepPTValue:
 	{ /* Since the slice may start on any byte we copy it byte by byte */
-	    unsigned char *newBody= (unsigned char *)newRep->Body;
+	    unsigned char *newBody= (unsigned char *)NEWREP->Body;
 	    unsigned char *oldBody= (unsigned char *)((unsigned)theRep->Body+(low-theRep->LowBorder));
 	    for (i = 0;  i < range; ++i)
 	      *(unsigned char *)((unsigned)newBody+i) = *(unsigned char *)((unsigned)oldBody+i);
@@ -117,7 +118,7 @@ void CCopySVR(ref(ValRep) theRep,
 	break;
       case (long) WordRepPTValue:
 	{ /* Since the slice may start on any word we copy it word by word */
-	    short *newBody= (short *)newRep->Body;
+	    short *newBody= (short *)NEWREP->Body;
 	    short *oldBody= (short *)((unsigned)theRep->Body+2*(low-theRep->LowBorder));
 	    for (i = 0;  i < range; ++i)
 	      *(short *)((unsigned)newBody+2*i) = *(short *)((unsigned)oldBody+2*i);
@@ -125,11 +126,11 @@ void CCopySVR(ref(ValRep) theRep,
 	break;
       case (long) ValRepPTValue:
 	for (i = 0; i < range; ++i){
-	  newRep->Body[i] = theRep->Body[i+low-theRep->LowBorder];
+	  NEWREP->Body[i] = theRep->Body[i+low-theRep->LowBorder];
 	}
 	break;
       case (long) DoubleRepPTValue:
-	{   double *newBody= (double *)newRep->Body;
+	{   double *newBody= (double *)NEWREP->Body;
 	    double *oldBody= (double *)((unsigned)theRep->Body+8*(low-theRep->LowBorder));
 	    for (i = 0;  i < range; ++i)
 	      *(double *)((unsigned)newBody+8*i) = *(double *)((unsigned)oldBody+8*i);
@@ -138,18 +139,18 @@ void CCopySVR(ref(ValRep) theRep,
       case (long) DynItemRepPTValue:
       case (long) DynCompRepPTValue:
 	for (i = 0; i < range; ++i)
-	  newRep->Body[i] = theRep->Body[i+low-theRep->LowBorder] /* AssignReference? */;
+	  NEWREP->Body[i] = theRep->Body[i+low-theRep->LowBorder] /* AssignReference? */;
 	break;
       case (long) StatItemRepPTValue:
 	/* copy as longs */
 	for (i = 0; i < range*ItemSize(REP->iProto)/4; ++i)
-	  newRep->Body[i] 
+	  NEWREP->Body[i] 
 	    = theRep->Body[i+(low-theRep->LowBorder)*ItemSize(REP->iProto)/4];
 	break;
       case (long) StatCompRepPTValue:
 	/* copy as longs */
 	for (i = 0; i < range*ComponentSize(REP->iProto)/4; ++i)
-	  newRep->Body[i] 
+	  NEWREP->Body[i] 
 	    = theRep->Body[i+(low-theRep->LowBorder)*ComponentSize(REP->iProto)/4];
 	break;
 
