a110c271a78aeb7328673ce11a033955e2fb313b
beta <beta@daimi.au.dk>
1996-03-27 10:27:15 +0000

    fixed comment style
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@2984 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- objectserver/private/external/runAlloc_linux.s
+++ objectserver/private/external/runAlloc_linux.s
@@ -10,57 +10,57 @@
 .text
 .globl _DoAlloVR1
 _DoAlloVR1:
-      movl 4(%esp),%edx  /* obj to RegObj */
-      movl 8(%esp),%eax  /* offset to DataReg1 */
-      movl 12(%esp),%ebx /* range to DataReg2 */
+      movl 4(%esp),%edx  # obj to RegObj 
+      movl 8(%esp),%eax  # offset to DataReg1 
+      movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR1
       ret
 
 .globl _DoAlloVR2
 _DoAlloVR2:
-      movl 4(%esp),%edx  /* obj to RegObj */
-      movl 8(%esp),%eax  /* offset to DataReg1 */
-      movl 12(%esp),%ebx /* range to DataReg2 */
+      movl 4(%esp),%edx  # obj to RegObj 
+      movl 8(%esp),%eax  # offset to DataReg1 
+      movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR2
       ret
 
 .globl _DoAlloVR4
 _DoAlloVR4:
-      movl 4(%esp),%edx  /* obj to RegObj */
-      movl 8(%esp),%eax  /* offset to DataReg1 */
-      movl 12(%esp),%ebx /* range to DataReg2 */
+      movl 4(%esp),%edx  # obj to RegObj 
+      movl 8(%esp),%eax  # offset to DataReg1 
+      movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR4
       ret
 
 .globl _DoAlloVR8
 _DoAlloVR8:
-      movl 4(%esp),%edx  /* obj to RegObj */
-      movl 8(%esp),%eax  /* offset to DataReg1 */
-      movl 12(%esp),%ebx /* range to DataReg2 */
+      movl 4(%esp),%edx  # obj to RegObj 
+      movl 8(%esp),%eax  # offset to DataReg1 
+      movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR8
       ret
 
 .globl _DoAlloRR
 _DoAlloRR:
-      movl 4(%esp),%edx  /* obj to RegObj */
-      movl 8(%esp),%eax  /* offset to DataReg1 */
-      movl 12(%esp),%ebx /* range to DataReg2 */
+      movl 4(%esp),%edx  # obj to RegObj 
+      movl 8(%esp),%eax  # offset to DataReg1 
+      movl 12(%esp),%ebx # range to DataReg2 
       call AlloRR
       ret
 
 .globl _DoAlloS
 _DoAlloS:
       movl 4(%esp),%edi
-      movl %edi,_pReg1    /* origin to pReg1 */
-      movl 8(%esp),%edi  /* proto to RegArg */
+      movl %edi,_pReg1   # origin to pReg1 
+      movl 8(%esp),%edi  # proto to RegArg 
       call AlloS
-      movl %edi,%eax     /* result in RegArg */
+      movl %edi,%eax     # result in RegArg 
       ret
 
 .globl _DoAlloH
 _DoAlloH:
-      movl $0,_pReg1     /* origin to pReg1 (unused) */
-      movl 4(%esp),%edi /* proto to RegArg */
+      movl $0,_pReg1     # origin to pReg1 (unused) 
+      movl 4(%esp),%edi  # proto to RegArg 
       call AlloH
-      movl %edi,%eax    /* Result in RegArg */
+      movl %edi,%eax     # Result in RegArg 
       ret
