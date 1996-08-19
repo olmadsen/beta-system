a5bef9f2d0b15747cdba1ab0a50647f0a1c39496
beta <beta@daimi.au.dk>
1996-08-19 08:50:35 +0000

    changes for linux ELF (no underscore
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@3555 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- objectserver/private/external/runAlloc_linux.s
+++ objectserver/private/external/runAlloc_linux.s
@@ -1,4 +1,4 @@
-## runAlloc_linux.s
+## runAlloclinux.s
 ## ================
 ## 
 ## Created by Soren Brandt and Jacob Seligmann.
@@ -6,60 +6,60 @@
 ## Linux interface to BETA RTS allocation routines used by ObjectSerializer.
 ##
 .data
-.globl	_pReg1
+.globl	pReg1
 .text
-.globl _DoAlloVR1
-_DoAlloVR1:
+.globl DoAlloVR1
+DoAlloVR1:
       movl 4(%esp),%edx  # obj to RegObj 
       movl 8(%esp),%eax  # offset to DataReg1 
       movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR1
       ret
 
-.globl _DoAlloVR2
-_DoAlloVR2:
+.globl DoAlloVR2
+DoAlloVR2:
       movl 4(%esp),%edx  # obj to RegObj 
       movl 8(%esp),%eax  # offset to DataReg1 
       movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR2
       ret
 
-.globl _DoAlloVR4
-_DoAlloVR4:
+.globl DoAlloVR4
+DoAlloVR4:
       movl 4(%esp),%edx  # obj to RegObj 
       movl 8(%esp),%eax  # offset to DataReg1 
       movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR4
       ret
 
-.globl _DoAlloVR8
-_DoAlloVR8:
+.globl DoAlloVR8
+DoAlloVR8:
       movl 4(%esp),%edx  # obj to RegObj 
       movl 8(%esp),%eax  # offset to DataReg1 
       movl 12(%esp),%ebx # range to DataReg2 
       call AlloVR8
       ret
 
-.globl _DoAlloRR
-_DoAlloRR:
+.globl DoAlloRR
+DoAlloRR:
       movl 4(%esp),%edx  # obj to RegObj 
       movl 8(%esp),%eax  # offset to DataReg1 
       movl 12(%esp),%ebx # range to DataReg2 
       call AlloRR
       ret
 
-.globl _DoAlloS
-_DoAlloS:
+.globl DoAlloS
+DoAlloS:
       movl 4(%esp),%edi
-      movl %edi,_pReg1   # origin to pReg1 
+      movl %edi,pReg1   # origin to pReg1 
       movl 8(%esp),%edi  # proto to RegArg 
       call AlloS
       movl %edi,%eax     # result in RegArg 
       ret
 
-.globl _DoAlloH
-_DoAlloH:
-      movl $0,_pReg1     # origin to pReg1 (unused) 
+.globl DoAlloH
+DoAlloH:
+      movl $0,pReg1     # origin to pReg1 (unused) 
       movl 4(%esp),%edi  # proto to RegArg 
       call AlloH
       movl %edi,%eax     # Result in RegArg 
