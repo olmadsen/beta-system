34e54f69401822ecaa4277fac3d2e67cf107fb1e
beta <beta@daimi.au.dk>
1997-06-18 09:41:04 +0000

    fixed bootdogram to handle boor of metagrammar correctly
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@4860 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- bin/bootdogram.bat
+++ bin/bootdogram.bat
@@ -5,11 +5,11 @@ rem grammar tools of the previous release.
 
 rem --- configuration---
 set OLD_BETALIB=e:\beta\r4.0.3
+set META_VERSION=v5.1
+set BOBSGEN_VERSION=v1.5
+set PRETTYGEN_VERSION=v5.1
 
 rem --- don't change below ---
-
-set NEW_BETALIB=%BETALIB%
-set BETALIB=%OLD_BETALIB%
 
 rem Try to guess which SDK is being used
 if not exist "%BETALIB%"\system\nti\ms\beta.exe set sdk=bor
@@ -35,17 +35,42 @@ goto getopts
 :execute
 rem Start the grammar tools
 set _myopts_=%_opts_%
-call %BETALIB%\bin\generator %_myopts_%
-call %BETALIB%\bin\bobsit %_myopts_%
-call %BETALIB%\bin\makepretty %_myopts_%
-call %BETALIB%\bin\morepretty %_myopts_%
+
+rem call %OLD_BETALIB%\bin\generator %_myopts_%
+echo generator version %META_VERSION% 
+"%OLD_BETALIB%"\meta\%META_VERSION%\NTIgen %_opts_%
+
+rem call %OLD_BETALIB%\bin\bobsit %_myopts_%
+echo bobsit version %BOBSGEN_VERSION%
+%OLD_BETALIB%\bobsgen\%BOBSGEN_VERSION%\NTIexbobs < %_opts_%-parser.bobs
+if not exist bobslist goto KEEPGOING
+if exist %_opts_%-parser.lst del %_opts_%-parser.lst
+rename bobslist  %_opts_%-parser.lst
+:KEEPGOING
+echo "End-of-BOBS"
+if not exist tables goto gramerrors
+echo Creating tables (tabc version 1.5)
+%OLD_BETALIB%\bobsgen\%BOBSGEN_VERSION%\NTItabc tables %_opts_%-parser
+:gramerrors
+echo ERRORS IN GRAMMAR
+echo Look at %_opts_%-parser.lst
+
+rem call %OLD_BETALIB%\bin\makepretty %_myopts_%
+echo makepretty version %PRETTYGEN_VERSION% 
+"%OLD_BETALIB%"\prettygen\%PRETTYGEN_VERSION%\NTImakepretty %_opts_%
+
+rem call %OLD_BETALIB%\bin\morepretty %_myopts_%
+echo morepretty version %PRETTYGEN_VERSION% 
+"%OLD_BETALIB%"\prettygen\%PRETTYGEN_VERSION%\NTImorepretty %_opts_%
+
 
 
 rem Clean-up
-set BETALIB=%NEW_BETALIB%
 set _opts_=
 set OLD_BETALIB=
-set NEW_BETALIB=
+set META_VERSION=
+set BOBSGEN_VERSION=
+set PRETTYGEN_VERSION=
 
 goto done
 
