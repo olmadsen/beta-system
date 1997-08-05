fb05a7502ed1e4c918eb7f174cd6a6a3f0b25cb4
Lennert Sloth <les@hypergenic.com>
1997-08-05 10:58:20 +0000

    Initial valhalla script.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@5166 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ bin/valhalla.bat
@@ -0,0 +1,46 @@
+@echo off
+
+if "%BETALIB%"=="" goto install
+
+rem Try to guess which SDK is being used
+if not exist "%BETALIB%"\compiler\nti\ms\beta.exe set sdk=bor
+if not exist "%BETALIB%"\compiler\nti\bor\beta.exe set sdk=ms
+
+rem Set SDK to either "bor" or "ms" for either Borland or Microsoft SDK.
+if "%sdk%"=="" goto usage
+
+rem Collect arguments and expand those expandable
+set _opts_=
+:getopts
+if "%1"=="" goto execute
+if exist %1 goto name
+set _opts_=%_opts_% %1
+goto next
+:name
+for %%f in (%1) do set _opts_=%_opts_% %%f
+:next
+shift
+goto getopts
+
+:execute
+rem Start the executable
+"%BETALIB%"\debugger\v2.3\nti\%sdk%\valhalla %_opts_%
+
+rem Clean-up
+set _opts_=
+goto done
+
+:usage
+echo Please set the environment variable SDK to either ms or bor using:
+echo   set SDK=ms
+echo or
+echo   set SDK=bor
+goto done
+
+:install
+echo Please read the installation notes before trying to Valhalla.
+echo Thank you.
+goto done
+
+:done
+rem Done
