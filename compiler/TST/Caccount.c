de464a7722e6996f44aafa20f46212931686b00b
Ole Lehrmann Madsen <olm@cs.au.dk>
1997-10-07 14:20:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@5540 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/TST/Caccount.c
@@ -0,0 +1,69 @@
+/**********************************************************
+ * 
+ *   This is a C program that builds a simplified
+ *   Microsoft COM-like class as described in "Inside OLE"
+ *
+ *   The class is an account with operations 
+ *   init, deposit and withdraw
+ */
+
+struct Account; /* forward declaration */
+ 
+/* vtbl is the virtual dispatch table for Account objects 
+ * The vtbl has entries in the form of function pointers
+ */
+struct vtbl
+{ long (*init)(struct Account *this, char *name,long initialAmount);
+  long (*deposit)(struct Account *this, long amount);
+  long (*withdraw)(struct Account *this,long amount);
+};
+
+/* Account is the class */
+struct Account
+{ struct vtbl * proto; /* pointer to virtual dispatch table */
+  long balance;       
+  char * name;
+};
+
+/* Operations on Account-objects;
+ * the first argument is always the this-pointer 
+ * to the Account object;
+ */
+
+long init(struct Account *this, char* name, long initialAmount)
+{ printf("Initializing account for %s\n",name);
+  this->name = name;
+  this->balance = initialAmount;
+  return this->balance;
+}
+
+long deposit(struct Account *this, long amount)
+{ printf("Deposit %i\n",amount);
+  this->balance = this->balance + amount;
+  return this->balance;
+}
+
+long withdraw(struct Account *this, long amount)
+{ printf("Withdraw %i\n",amount);
+  this->balance = this->balance - amount;
+  return this->balance;
+}
+
+/* This is a virtual dispatch table instance, 
+ * that may be shared by all Account objects 
+ */
+static struct vtbl theVTBL;
+
+/* GetAccount creates a new Account and returns a pointer to it */
+struct Account * GetAccount()
+{ struct Account * R;
+  /* initialize dispatch table */
+  theVTBL.init = &init;
+  theVTBL.deposit = &deposit;
+  theVTBL.withdraw = &withdraw;
+  /* Allocate Account object */
+  R = (struct Account *)malloc(sizeof(struct Account));
+  R->proto = &theVTBL;
+  R->balance = 0;
+  return R; 
+}
