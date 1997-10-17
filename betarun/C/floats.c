1a6c33238b761f5c6d849efe0407a1bed29bfff8
Manmathan Muthukumarapillai <mannan@daimi.au.dk>
1997-10-17 11:15:51 +0000

    GNU -mannan
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@5603 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/C/floats.c
+++ betarun/C/floats.c
@@ -25,7 +25,7 @@
 #define FPU_UNDERFLOW     _EM_UNDERFLOW
 #define FPU_PRECISIONLOST _EM_INEXACT
 #define EnableFPUexceptions(mask) _controlfp((unsigned int)~(mask), _MCW_EM)
-#endif /* nti_gnu */
+#endif /* nti_gnu || nti_ms */
 
 #ifdef nti_bor
 /* The following is necessary in order to get printf to
