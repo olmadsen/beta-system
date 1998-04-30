c0d22771788dd68537c3b9ea249c72bf4c73543c
beta <beta@daimi.au.dk>
1998-04-30 14:47:55 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@6468 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- comlib/idl2beta/do_all_sdk_files.sh
+++ comlib/idl2beta/do_all_sdk_files.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 >log
-for f in [ test/SDK_IDL/*.idl test/SDK_IDL/*.Idl ]
+for f in test/SDK_IDL/*.idl test/SDK_IDL/*.Idl
 do
   echo $f 
-  echo $f >> log
-  bidl -f -q -e $f | tee -a log
+  echo $f >>log
+  bidl -f -q -p $f | tee -a log
 done
 
   
