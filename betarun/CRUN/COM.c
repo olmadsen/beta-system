a2dac10c6c11e9d2f35289c8880c087738ad08c0
beta <beta@daimi.au.dk>
1998-06-03 12:42:59 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@6641 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/CRUN/COM.c
@@ -0,0 +1,65 @@
+/*
+ * BETA C RUNTIME SYSTEM, Copyright (C) 1992-94 Mjolner Informatics Aps.
+ * by Peter Andersen and Tommy Thorn.
+ */
+
+
+#define GCable_Module
+
+#include "beta.h"
+#include "crun.h"
+
+#ifndef MT
+
+ParamOriginProto(struct Item *,AlloCOM)
+/* = struct Item * AlloCOM(struct Object *origin, struct ProtoType *proto) */
+{
+    DeclReference1(struct Item *, item); /*= struct Item * item; */
+    GCable_Entry();
+    MCHECK();
+    FetchOriginProto();
+
+    DEBUG_CODE(NumAlloCOM++);
+
+    Ck(origin);
+
+#if (defined(hppa) && defined(RTDEBUG))
+    if((unsigned)/*getRefSP()*/RefSP > (unsigned)ReferenceStack + 990*4) {
+      Notify("ReferenceStack overflow!!!");
+    }
+#endif
+
+    DEBUG_CODE( Claim(proto->Size > 0, "AlloCOM: proto->Size > 0") );
+
+    Protect(origin, item = (struct Item *) AOAcalloc(ItemSize(proto)));
+
+    /* The new Object is now allocated, but not initialized yet! */
+
+    setup_item(item, proto, origin); 
+
+    /* COM objects have their prototype point AFTER the static part of 
+     * the proto type struct 
+     */
+    item->Proto++;
+
+    if (proto->GenPart){
+#ifdef RTDEBUG
+      Protect2(origin, item, CallBetaEntry(proto->GenPart,item));
+#else
+      Protect(item, CallBetaEntry(proto->GenPart,item));
+#endif
+    }
+
+    Ck(origin); Ck(item);
+
+    MCHECK();
+
+    GCable_Exit(1);    
+#ifdef sparc
+    return_in_i1(item);
+#else
+    RETURN(item);
+#endif
+}
+
+#endif /* MT */
