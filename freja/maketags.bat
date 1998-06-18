e76f533d95b8adbe6beac1fd827fc6ae1499d51c
Michael Christensen <toby@daimi.au.dk>
1998-06-18 07:55:14 +0000

    See CHANGES
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@6698 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- freja/maketags.bat
+++ freja/maketags.bat
@@ -1,4 +1,4 @@
-dir /s /b *.bet private\*.bet ..\gpp\*.bet ..\gpp\private\*.bet \*.bet | find ".bet" | find /v ".bet~" | etags -o .\TAGS -
+dir /s /b *.bet private\*.bet ..\gpp\*.bet ..\gpp\private\*.bet | find ".bet" | find /v ".bet~" | etags -o .\TAGS -
 echo "(Ignore above error!)"
 echo "Done!"
 
