ef74765580d0615b40d7a1ab0227bc3155b12e01
beta <beta@daimi.au.dk>
1998-08-07 11:36:50 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@6901 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- bin/admin/rebuildall.sh
+++ bin/admin/rebuildall.sh
@@ -18,6 +18,7 @@ if ($# != 2) then
     exit 1
 endif
 
+source .login
 setenv BETALIB    $1
 setenv REMOVEASTS $2
 source ${BETALIB}/configuration/env.csh
@@ -25,14 +26,21 @@ source ${BETALIB}/configuration/env.csh
 setenv LOG ${BETALIB}/log/rebuildall.$MACHINETYPE
 
 rm -f $LOG
-echo "rebuildall.sh: Starting." >>& $LOG
+echo "rebuildall.sh: Starting on BETALIB $BETALIB." >>& $LOG
 date >>& $LOG
 if ( $REMOVEASTS == "yes" ) then
     echo "rebuildall.sh: Removing all asts and codefiles." >>& $LOG
     mbs_rmast -u >>& $LOG
     mbs_rmcode -u sun4s linux sgi hpux9pa nti_ms nti_gnu ms gnu bor >>& $LOG
 endif
+# Fragbody makes the compiler crash for the time being.  
+# Make sure there is a compiler by just retrying.
+setenv BETAOPTS --nocode
 mbs_compiletools compiler >>& $LOG
+unsetenv BETAOPTS
+mbs_compiletools compiler >>& $LOG
+mbs_compiletools compiler >>& $LOG
+mbs_compile --nocode >>& LOG
 mbs_compile >>& LOG
 mbs_compiletools -u >>& $LOG
 date >>& $LOG
