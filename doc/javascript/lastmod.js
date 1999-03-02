136927759c14bb421729221d4365d68712909e4f
Peter Andersen <datpete@gmail.com>
1999-03-02 23:47:04 +0000

    created remaining interfaces
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@8324 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- doc/javascript/lastmod.js
+++ doc/javascript/lastmod.js
@@ -2,5 +2,5 @@
 // html file
 var mod=document.lastModified;
 
-document.writeln(" <TD align=center><FONT size=-1>&COPY; <A HREF=\"http://www.mjolner.com\">Mjølner Informatics</A></FONT></TD>");
+document.writeln(" <TD align=center><FONT size=-1>&COPY; <A HREF=\"http://www.mjolner.com\">Mj&oslash;lner Informatics</A></FONT></TD>");
 document.writeln(" <TD align=right><FONT size=-1>[Last modified: " + mod + "]</FONT></TD>");
