433ef73334c31d68f811605e0b86b46e1cd8ef21
Peter Andersen <datpete@gmail.com>
1999-03-02 11:54:06 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@8309 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- doc/javascript/lastmod.js
+++ doc/javascript/lastmod.js
@@ -1,5 +1,6 @@
 // Generate table data with right justified last modification date of
 // html file
 var mod=document.lastModified;
+
+document.writeln(" <TD align=center><FONT size=-1>&COPY; <A HREF=\"http://www.mjolner.com\">Mjølner Informatics</A></FONT></TD>");
 document.writeln(" <TD align=right><FONT size=-1>[Last modified: " + mod + "]</FONT></TD>");
-  
