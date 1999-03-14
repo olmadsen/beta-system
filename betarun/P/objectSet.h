9632ff3c92de2ca83e42e32a92fb65f03630f360
Stephan Erbs Korsholm <void@daimi.au.dk>
1999-03-14 09:30:20 +0000

    Removed unused files --void
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@8444 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/P/objectSet.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OBJECTSET_H_
-#define _OBJECTSET_H_
-#include "beta.h"
-
-void OSinsert(Object *theObj);
-void OSscan(void (*foreach)(Object *theObj));
-void OSclear(void);
-
-#endif /* _OBJECTSET_H_ */
