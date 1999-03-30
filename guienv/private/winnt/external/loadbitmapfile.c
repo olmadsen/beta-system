37f2b38830122b8354244000e42d0663e4cd7d55
beta <beta@daimi.au.dk>
1999-03-30 08:17:19 +0000

    flyttede transparentpixmap fra r4.2.1bcorrentionbranch til r5.0 -gram
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@8531 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- guienv/private/winnt/external/loadbitmapfile.c
+++ guienv/private/winnt/external/loadbitmapfile.c
@@ -639,27 +639,167 @@ HANDLE ReadDibBitmapInfo (HFILE fh)
  
 
  
-
-
-void DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
-                           short yStart, int xDestPos, int yDestPos, COLORREF cTransparentColor)
+ 
+BOOL DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
+                           short yStart, int xDestPos, int yDestPos, int width,int height,COLORREF cTransparentColor)
 {
-   BITMAP     bm;
+	HDC hdc;
+	BYTE f;
+	
+	hdc = GetDC( hwnd );
+	f = DrawTransparentBitmapHDC(hdc,hBitmap,xStart,yStart,xDestPos,yDestPos,width,height,cTransparentColor);
+	ReleaseDC(hwnd,hdc);
+	return f;
+}
+
+BOOL DrawTransparentBitmapHDC(HDC hdc, HBITMAP hBitmap, int xStart,
+                           int yStart, int xDestPos, int yDestPos, int width,int height,COLORREF cTransparentColor)
+{
    COLORREF   cColor;
    HBITMAP    bmAndBack, bmAndObject, bmAndMem, bmSave;
    HBITMAP    bmBackOld, bmObjectOld, bmMemOld, bmSaveOld;
-   HDC        hdc, hdcMem, hdcBack, hdcObject, hdcTemp, hdcSave;
+   HDC        hdcMem, hdcBack, hdcObject, hdcTemp, hdcSave;
    POINT      ptSize;
-
-   hdc = GetDC ( hwnd );
+   BOOL       res;
 
    hdcTemp = CreateCompatibleDC(hdc);
    SelectObject(hdcTemp, hBitmap);   // Select the bitmap
 
-   GetObject(hBitmap, sizeof(BITMAP), (LPSTR)&bm);
 
-   ptSize.x = bm.bmWidth;            // Get width of bitmap
-   ptSize.y = bm.bmHeight;           // Get height of bitmap
+   ptSize.x = width;            // Get width of bitmap
+   ptSize.y = height;           // Get height of bitmap
+   DPtoLP(hdcTemp, &ptSize, 1);      // Convert from device
+                                     // to logical points
+
+   // Create some DCs to hold temporary data.
+   hdcBack   = CreateCompatibleDC(hdc);
+   hdcObject = CreateCompatibleDC(hdc);
+   hdcMem    = CreateCompatibleDC(hdc);
+   hdcSave   = CreateCompatibleDC(hdc);
+
+   // Create a bitmap for each DC. DCs are required for a number of
+   // GDI functions.
+
+   // Monochrome DC
+
+   bmAndBack   = CreateBitmap(ptSize.x, ptSize.y, 1, 1, NULL);
+
+   // Monochrome DC
+   bmAndObject = CreateBitmap(ptSize.x, ptSize.y, 1, 1, NULL);
+
+   bmAndMem    = CreateCompatibleBitmap(hdc, ptSize.x, ptSize.y);
+   bmSave      = CreateCompatibleBitmap(hdc, ptSize.x, ptSize.y);
+
+   // Each DC must select a bitmap object to store pixel data.
+   bmBackOld   = SelectObject(hdcBack, bmAndBack);
+   bmObjectOld = SelectObject(hdcObject, bmAndObject);
+   bmMemOld    = SelectObject(hdcMem, bmAndMem);
+   bmSaveOld   = SelectObject(hdcSave, bmSave);
+
+   // Set proper mapping mode.
+   SetMapMode(hdcTemp, GetMapMode(hdc));
+
+
+   // Save the bitmap sent here, because it will be overwritten.
+   BitBlt(hdcSave, 0, 0, width, height, hdcTemp, 0, 0, SRCCOPY);
+
+   // Set the background color of the source DC to the color.
+   // contained in the parts of the bitmap that should be transparent
+   cColor = SetBkColor(hdcTemp, cTransparentColor);
+
+   // Create the object mask for the bitmap by performing a BitBlt()
+   // from the source bitmap to a monochrome bitmap.
+   BitBlt(hdcObject, 0, 0, ptSize.x, ptSize.y, hdcTemp, 0, 0,
+          SRCCOPY);
+
+   // Set the background color of the source DC back to the original
+   // color.
+   SetBkColor(hdcTemp, cColor);
+
+
+   // Create the inverse of the object mask.
+   BitBlt(hdcBack, 0, 0, ptSize.x, ptSize.y, hdcObject, 0, 0,
+          NOTSRCCOPY);
+
+   // Copy the background of the main DC to the destination.
+   BitBlt(hdcMem, 0, 0, ptSize.x, ptSize.y, hdc, xDestPos, yDestPos,
+          SRCCOPY);
+
+   // Mask out the places where the bitmap will be placed.
+   BitBlt(hdcMem, 0, 0, ptSize.x, ptSize.y, hdcObject, 0, 0, SRCAND);
+
+   // Mask out the transparent colored pixels on the bitmap.
+   BitBlt(hdcTemp, 0, 0, ptSize.x, ptSize.y, hdcBack, 0, 0, SRCAND);
+
+   // XOR the bitmap with the background on the destination DC.
+   BitBlt(hdcMem, 0, 0, ptSize.x, ptSize.y, hdcTemp, 0, 0, SRCPAINT);
+
+
+   // Copy the destination to the screen.
+   res = BitBlt(hdc, xDestPos, yDestPos, ptSize.x, ptSize.y, hdcMem, xStart, yStart,
+          SRCCOPY);
+
+   // Place the original bitmap back into the bitmap sent here.
+   BitBlt(hdcTemp, 0, 0, ptSize.x, ptSize.y, hdcSave, 0, 0, SRCCOPY);
+
+   // Delete the memory bitmaps.
+   DeleteObject(SelectObject(hdcBack, bmBackOld));
+   DeleteObject(SelectObject(hdcObject, bmObjectOld));
+   DeleteObject(SelectObject(hdcMem, bmMemOld));
+   DeleteObject(SelectObject(hdcSave, bmSaveOld));
+
+   // Delete the memory DCs.
+   DeleteDC(hdcMem);
+   DeleteDC(hdcBack);
+
+   DeleteDC(hdcObject);
+   DeleteDC(hdcSave);
+   DeleteDC(hdcTemp);
+
+   return res;
+ }
+
+BOOL DrawTransparentBitmapFromMask
+  (HWND hwnd, HBITMAP hBitmap, int xStart, int yStart, int xDestPos, 
+   int yDestPos, int width, int height, HBITMAP hBitmapMask)
+{
+  HDC hdc;
+  BOOL f;
+  
+  hdc = GetDC ( hwnd );
+ 
+  f = DrawTransparentBitmapFromMaskHDC(hdc,hBitmap,xStart,yStart,xDestPos,yDestPos,width,height,hBitmapMask);
+  
+  ReleaseDC(hwnd,hdc);
+  
+  return f;
+}
+
+
+BOOL DrawTransparentBitmapFromMaskHDC
+  (HDC hdc, HBITMAP hBitmap, int xStart, int yStart, int xDestPos, 
+   int yDestPos, int width, int height, HBITMAP hBitmapMask)
+{
+   COLORREF   cColor,cTransparentColor;
+   HBITMAP    bmAndBack, bmAndObject, bmAndMem, bmSave;
+   HBITMAP    bmBackOld, bmObjectOld, bmMemOld, bmSaveOld;
+   HDC        hdcMem, hdcBack, hdcObject, hdcTemp, hdcSave;
+   POINT      ptSize;
+   BOOL       res;
+   BITMAP     bm;
+   INT        bmwidth,bmheight;
+   
+   cTransparentColor=0;
+
+   hdcTemp = CreateCompatibleDC(hdc);
+   SelectObject(hdcTemp, hBitmap);   // Select the bitmap
+
+   GetObject(hBitmap, sizeof(BITMAP), (LPSTR)&bm); 
+   if (width>(bm.bmWidth-xDestPos)) width=bm.bmWidth-xStart;
+   if (height>(bm.bmHeight-yDestPos)) height=bm.bmHeight-yStart;
+
+   ptSize.x = width;            // Get width of bitmap
+   ptSize.y = height;           // Get height of bitmap
    DPtoLP(hdcTemp, &ptSize, 1);      // Convert from device
                                      // to logical points
 
@@ -701,8 +841,9 @@ void DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
 
    // Create the object mask for the bitmap by performing a BitBlt()
    // from the source bitmap to a monochrome bitmap.
-   BitBlt(hdcObject, 0, 0, ptSize.x, ptSize.y, hdcTemp, 0, 0,
-          SRCCOPY);
+   // BitBlt(hdcObject, 0, 0, ptSize.x, ptSize.y, hdcTemp, 0, 0, SRCCOPY);
+   //FGC
+   SelectObject(hdcObject,hBitmapMask);
 
    // Set the background color of the source DC back to the original
    // color.
@@ -728,7 +869,7 @@ void DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
 
 
    // Copy the destination to the screen.
-   BitBlt(hdc, xDestPos, yDestPos, ptSize.x, ptSize.y, hdcMem, xStart, yStart,
+   res = BitBlt(hdc, xDestPos, yDestPos, ptSize.x, ptSize.y, hdcMem, xStart, yStart,
           SRCCOPY);
 
    // Place the original bitmap back into the bitmap sent here.
@@ -736,7 +877,7 @@ void DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
 
    // Delete the memory bitmaps.
    DeleteObject(SelectObject(hdcBack, bmBackOld));
-   DeleteObject(SelectObject(hdcObject, bmObjectOld));
+//   DeleteObject(SelectObject(hdcObject, bmObjectOld));
    DeleteObject(SelectObject(hdcMem, bmMemOld));
    DeleteObject(SelectObject(hdcSave, bmSaveOld));
 
@@ -748,7 +889,8 @@ void DrawTransparentBitmap(HWND hwnd, HBITMAP hBitmap, short xStart,
    DeleteDC(hdcSave);
    DeleteDC(hdcTemp);
 
-   ReleaseDC(hwnd,hdc);
+
+   return res;
  }
 
 
@@ -790,6 +932,53 @@ BOOL drawBitmap (HDC hdc, HBITMAP hbm, INT x, INT y,
     DeleteDC(hdcBits);
 
     return f;
+}
+BOOL drawBitmapOnBitmap(HBITMAP hdst,INT dstx, INT dsty,HBITMAP hsrc,INT srcx,INT srcy,
+			INT width,INT height,INT tc,HBITMAP hmsk)
+{
+	BOOL f;
+	HDC hdcdst,hdcsrc;
+	COLORREF tcolor;
+	
+	if (!hdst || !hsrc ) return FALSE;
+	
+	switch(tc) {
+	case -1:    /* draw as not transparent  */
+		hdcdst = CreateCompatibleDC((HDC)0);
+		hdcsrc = CreateCompatibleDC((HDC)0);
+		if (!hdcdst || !hdcsrc) return FALSE;
+		
+		SelectObject(hdcsrc,hsrc);
+		SelectObject(hdcdst,hdst);
+
+		f = BitBlt(hdcdst,dstx,dsty,width,height,hdcsrc,srcx,srcy,SRCCOPY);
+
+		DeleteDC(hdcdst);
+		DeleteDC(hdcsrc);
+		break;
+	case -2:    /* draw transparent with mask */
+	        if (!hmsk) return FALSE;
+	        hdcdst = CreateCompatibleDC((HDC)0);
+
+		if (!hdcdst || !hmsk ) return FALSE;
+		SelectObject(hdcdst,hdst);
+
+		f = DrawTransparentBitmapFromMaskHDC(hdcdst,hsrc,dstx,dsty,srcx,srcy,width,height,hmsk);
+
+		DeleteDC(hdcdst);
+
+		break;
+	default:    /* use tc as transparent color */
+		tcolor = (COLORREF) tc;
+		hdcdst = CreateCompatibleDC((HDC)0);
+		SelectObject(hdcdst,hdst);
+		
+		f = DrawTransparentBitmapHDC(hdcdst,hsrc,0,0,dstx,dsty,width,height,tcolor);
+
+		DeleteDC(hdcdst);
+		break;
+	};
+	return f;
 }
 
 
