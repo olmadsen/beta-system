f603e9ec01737e2ff3b0410d84193949a74221c0
beta <beta@daimi.au.dk>
1999-06-23 13:01:23 +0000

    Changed implementation and interface of trie to do more clever memory allocation
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@8806 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/P/trie.c
+++ betarun/P/trie.c
@@ -1,11 +1,17 @@
-#include "beta.h"
 #include "trie.h"
+#include <stdio.h>
 
 #ifdef sparc
 #define OURINLINE __inline__
 #else
 #define OURINLINE
 #endif
+
+#ifdef nti
+#include <memory.h>
+#include <stdlib.h>
+#include <tchar.h>
+#endif /* nti */
 
 /* Local types */
 #define INITIALOFFSET 4
@@ -26,25 +32,28 @@ static void TRealloc(Trie **trie);
 static unsigned long allocNode(Trie *trie);
 static unsigned long allocContentsBox(Trie *trie);
 static OURINLINE void insertDown(unsigned long key, 
-				 void *contents, 
+				 unsigned long contents, 
 				 Node *current, 
 				 Trie *trie,
 				 unsigned long insertKey);
 static OURINLINE void insertRight(unsigned long key, 
-				  void *contents, 
+				  unsigned long contents, 
 				  Node *current, 
 				  Trie *trie,
 				  unsigned long insertKey);
 static void _TInsert(unsigned long key, 
-		     void *contents, 
+		     unsigned long contents, 
 		     Node *current, 
 		     Trie *trie,
 		     unsigned long insertKey);
-static OURINLINE void *downLookup(unsigned long key, Node *current, Trie *trie);
-static OURINLINE void *rightLookup(unsigned long key, Node *current, Trie *trie);
-static void *_TILookup(unsigned long key, Node *current, Trie *trie);
+static OURINLINE unsigned long downLookup(unsigned long key, Node *current, Trie *trie);
+static OURINLINE unsigned long rightLookup(unsigned long key, Node *current, Trie *trie);
+static unsigned long _TILookup(unsigned long key, Node *current, Trie *trie);
 static void _TIVisit(Node *current, Trie *trie, void (*visitFunc)(contentsBox *));
-static void _TIFree(Node *current, Trie *trie, void (*freeFunc)(void *));
+static void _TIFree(Node *current, Trie *trie, void (*freeFunc)(unsigned long));
+/* TEXT TRIES */
+static unsigned long _TTLookup(char *key, Node *current, Trie *trie);
+static void _TTInsert(char *key, unsigned long contents, Node *current, Trie *trie);
 
 /* LOCAL VARIABLES */
 static Node *new;
@@ -61,15 +70,6 @@ Trie *TInit(void)
   ((Node *)(&(new -> buffer[0]) + (unsigned long)INITIALOFFSET)) -> d = -1;
   new -> top += sizeof(struct Node);
   return new;
-}
-
-void TInitp(Trie *new)
-{
-  new -> size = INITIALTRIESIZE;
-  new -> top = INITIALOFFSET;
-  /* allocate root node */
-  ((Node *)(&(new -> buffer[0]) + (unsigned long)INITIALOFFSET)) -> d = -1;
-  new -> top += sizeof(struct Node);
 }
 
 static void TRealloc(Trie **trie)
@@ -91,7 +91,6 @@ static unsigned long allocNode(Trie *trie)
     trie -> top += sizeof(struct Node);
     return trie -> top - sizeof(struct Node);
   } else {
-    Claim(FALSE, "allocNode: Unexpected reallocation");
     return 0;
   }
 }
@@ -102,13 +101,12 @@ static unsigned long allocContentsBox(Trie *trie)
     trie -> top += sizeof(struct contentsBox);
     return trie -> top - sizeof(struct contentsBox);
   } else {
-    Claim(FALSE, "allocContentsBox: Unexpected reallocation");
     return 0;
   }
 }
 
 static OURINLINE void insertDown(unsigned long key, 
-				 void *contents, 
+				 unsigned long contents, 
 				 Node *current, 
 				 Trie *trie,
 				 unsigned long insertKey)
@@ -125,7 +123,7 @@ static OURINLINE void insertDown(unsigned long key,
 }
 
 static OURINLINE void insertRight(unsigned long key, 
-				  void *contents, 
+				  unsigned long contents, 
 				  Node *current, 
 				  Trie *trie,
 				  unsigned long insertKey)
@@ -142,7 +140,7 @@ static OURINLINE void insertRight(unsigned long key,
 }
 
 void TInsert(unsigned long key, 
-	     void *contents, 
+	     unsigned long contents, 
 	     Trie **trie, 
 	     unsigned long insertKey)
 {
@@ -160,30 +158,30 @@ void TInsert(unsigned long key,
 }
 
 static void _TInsert(unsigned long key, 
-		     void *contents, 
+		     unsigned long contents, 
 		     Node *current, 
 		     Trie *trie,
 		     unsigned long insertKey)
 {
   if (key > 10) {
-    if (current -> d == -1) {
-      current -> d = key % 10;
+    if (current -> d == (char)-1) {
+      current -> d = (char)(key % 10);
       insertDown(key / 10, contents, current, trie, insertKey);
     } else {
-      if (key % 10 == current -> d) {
+      if ((char)(key % 10) == current -> d) {
 	insertDown(key / 10, contents, current, trie, insertKey);
       } else {
 	insertRight(key, contents, current, trie, insertKey);
       }
     }
   } else {
-    if (current -> d == -1) {
-      current -> d = key;
+    if (current -> d == (char)-1) {
+      current -> d = (char)key;
       current -> contents = allocContentsBox(trie);
       CONTENTSBOX(current -> contents) -> contents = contents;
       CONTENTSBOX(current -> contents) -> key = insertKey;
     } else {
-      if (key == current -> d) {
+      if ((char)key == current -> d) {
 	if (current -> contents == 0) {
 	  current -> contents = allocContentsBox(trie);
 	} 
@@ -196,43 +194,43 @@ static void _TInsert(unsigned long key,
   }
 }
 
-static OURINLINE void *downLookup(unsigned long key, Node *current, Trie *trie)
+static OURINLINE unsigned long downLookup(unsigned long key, Node *current, Trie *trie)
 {
   if (current -> down != 0) {
     return _TILookup(key, NODE(current -> down), trie);
   } else {
-    return NULL;
+    return 0;
   }
 }
 
-static OURINLINE void *rightLookup(unsigned long key, Node *current, Trie *trie)
+static OURINLINE unsigned long rightLookup(unsigned long key, Node *current, Trie *trie)
 {
   if (current -> right != 0) {
     return _TILookup(key, NODE(current -> right), trie);
   } else {
-    return NULL;
+    return 0;
   }
 }
 
-void *TILookup(unsigned long key, Trie *trie)
+unsigned long TILookup(unsigned long key, Trie *trie)
 {
   return _TILookup(key, HEAD(trie), trie);
 }
 
-static void *_TILookup(unsigned long key, Node *current, Trie *trie)
+static unsigned long _TILookup(unsigned long key, Node *current, Trie *trie)
 {
   if (key > 10) {
-    if (key % 10 == current -> d) {
+    if ((char)(key % 10) == current -> d) {
       return downLookup(key / 10, current, trie);
     } else {
       return rightLookup(key, current, trie);
     }
   } else {
-    if (key == current -> d) {
+    if ((char)key == current -> d) {
       if (current -> contents != 0) {
 	return CONTENTSBOX(current -> contents) -> contents;
       } else {
-	return NULL;
+	return 0;
       }
     } else {
       return rightLookup(key, current, trie);
@@ -258,7 +256,7 @@ static void _TIVisit(Node *current, Trie *trie, void (*visitFunc)(contentsBox *)
   }
 }
 
-void TIFree(Trie *trie, void (*freeFunc)(void *))
+void TIFree(Trie *trie, void (*freeFunc)(unsigned long))
 {
   if (freeFunc) {
     _TIFree(HEAD(trie), trie, freeFunc);
@@ -266,7 +264,7 @@ void TIFree(Trie *trie, void (*freeFunc)(void *))
   free(trie);
 }
 
-static void _TIFree(Node *current, Trie *trie, void (*freeFunc)(void *))
+static void _TIFree(Node *current, Trie *trie, void (*freeFunc)(unsigned long))
 {
   if (current -> contents != 0) {
     freeFunc(CONTENTSBOX(current -> contents) -> contents);
@@ -278,9 +276,113 @@ static void _TIFree(Node *current, Trie *trie, void (*freeFunc)(void *))
     _TIFree(NODE(current -> right), trie, freeFunc);
   }
 }
+
+/* Below we include functions very similar to the above, only the key
+   used to insert objects in the trie comes in the form of character
+   strings. */
+
+/* LOOKUP */
+
+unsigned long TTLookup(char *key, Trie *trie)
+{
+  if (key) {
+    if (key[0] != '\0') {
+      return _TTLookup(key, HEAD(trie), trie);
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+
+static unsigned long _TTLookup(char *key, Node *current, Trie *trie)
+{
+  if (/* key > 10 */ key[1] != '\0') {
+    if (/* key % 10 == current -> d */ key[0] == current -> d) {
+      return downLookup(/* key / 10 */ (unsigned long)(key + 1), current, trie);
+    } else {
+      return rightLookup((unsigned long)key, current, trie);
+    }
+  } else {
+    if (/* key == current -> d*/ key[0] == current -> d) {
+      if (current -> contents != 0) {
+	return CONTENTSBOX(current -> contents) -> contents;
+      } else {
+	return 0;
+      }
+    } else {
+      return rightLookup((unsigned long)key, current, trie);
+    }
+  }
+}
+
+/* INSERT */
+
+void TTInsert(char *key, 
+	      unsigned long contents, 
+	      Trie **trie)
+{
+  /* Make sure that there is enough room to hold the new value */
+  /* A max of strlen(key) new nodes + one contentsbox might be generated */
+  
+  if ((*trie) -> top + 
+      (sizeof(struct Node) * strlen(key)) + 
+      sizeof(struct contentsBox) < (*trie) -> size) {
+    _TTInsert(key, contents, HEAD((*trie)), *trie);
+  } else {
+    TRealloc(trie);
+    TTInsert(key, contents, trie);
+  }
+}
+
+static void _TTInsert(char *key, 
+		      unsigned long contents, 
+		      Node *current, 
+		      Trie *trie)
+{
+  if (/* key > 10 */ key[1] != 0) {
+    if (current -> d == -1) {
+      current -> d = /* key % 10 */ key[0];
+      insertDown(/* key / 10 */ (unsigned long)(key + 1), 
+		 contents, 
+		 current, 
+		 trie,
+		 0);
+    } else {
+      if (/* key % 10 == current -> d */ key[0] == current -> d) {
+	insertDown(/* key / 10 */ (unsigned long)(key + 1), 
+		   contents, 
+		   current, 
+		   trie,
+		   0);
+      } else {
+	insertRight((unsigned long)key, contents, current, trie, 0);
+      }
+    }
+  } else {
+    if (current -> d == -1) {
+      current -> d = /* key */ key[0];
+      current -> contents = allocContentsBox(trie);
+      CONTENTSBOX(current -> contents) -> contents = contents;
+      CONTENTSBOX(current -> contents) -> key = 0;
+    } else {
+      if (/* key == current -> d */ key[0] == current -> d) {
+	if (current -> contents == 0) {
+	  current -> contents = allocContentsBox(trie);
+	} 
+	CONTENTSBOX(current -> contents) -> contents = contents;
+	CONTENTSBOX(current -> contents) -> key = 0;
+      } else {
+	insertRight((unsigned long)key, contents, current, trie, 0);
+      }
+    }
+  }
+}
   
 /* TESTING */
 #ifdef RTDEBUG
+#define Assert(cond, message) if (!(cond)) { fprintf(stderr, message); } 
 void visitFunc(contentsBox *cb)
 {
   printf("visitFunc: %s\n", (char *)(cb -> contents));
@@ -292,8 +394,9 @@ void testTrie(void)
   
   trie = TInit();
 
+  /* test integer trie */
 #define TINSERT(x,y,z,v) \
-  fprintf(output, "TInsert(%s)\n", (char*)y); fflush(output); \
+  fprintf(stderr, "TInsert(%s)\n", (char*)y); fflush(stderr); \
   TInsert(x,y,z,v)
   
   TINSERT(10101, "10101 inserted", &trie, 0);
@@ -303,15 +406,43 @@ void testTrie(void)
   TINSERT(10101, "10101 inserted again", &trie, 0);
   TINSERT(1010111, "1010111 inserted", &trie, 0);
   
-  Claim(TILookup(10101, trie) != NULL, "");
-  Claim(TILookup(0, trie) != NULL, "");
-  Claim(TILookup(1, trie) != NULL, "");
-  Claim(TILookup(1010111, trie) != NULL, "");
+  Assert(TILookup(10101, trie) != 0, "");
+  Assert(TILookup(0, trie) != 0, "");
+  Assert(TILookup(1, trie) != 0, "");
+  Assert(TILookup(1010111, trie) != 0, "");
 
-  Claim(TILookup(100, trie) == NULL, "");
-  Claim(TILookup(101, trie) == NULL, "");
+  Assert(TILookup(100, trie) == 0, "");
+  Assert(TILookup(101, trie) == 0, "");
   
   TIVisit(trie, visitFunc);
+
+  /* test text trie */
+#define TTINSERT(x,y,z) \
+  fprintf(stderr, "TTInsert(%s)\n", (char*)y); fflush(stderr); \
+  TTInsert(x,y,z)
+
+  {
+    Trie *textTrie;
+    
+    textTrie = TInit();
+    
+    TTINSERT("10101", "10101 inserted", &textTrie);
+    TTINSERT("0", "0 inserted", &textTrie);
+    TTINSERT("1", "1 inserted", &textTrie);
+    TTINSERT("1", "1 inserted", &textTrie);
+    TTINSERT("10101", "10101 inserted again", &textTrie);
+    TTINSERT("1010111", "1010111 inserted", &textTrie);
+    
+    Assert(TTLookup("10101", textTrie) != 0, "");
+    Assert(TTLookup("0", textTrie) != 0, "");
+    Assert(TTLookup("1", textTrie) != 0, "");
+    Assert(TTLookup("1010111", textTrie) != 0, "");
+    
+    Assert(TTLookup("100", textTrie) == 0, "");
+    Assert(TTLookup("101", textTrie) == 0, "");
+
+    TIVisit(textTrie, visitFunc);
+  }
 }
 
 #endif /* RTDEBUG */
