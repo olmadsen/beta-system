dab7b9e4ab28119b28ad191f39aead553666e1d3
Elmer Sandvad <elmer@sandvad.com>
1999-08-18 17:51:22 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@9130 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- doc/internal/navigation.js
+++ doc/internal/navigation.js
@@ -61,6 +61,7 @@ foldersTree = gFld("<B>Contents</B>", "");
 			    insFld(aux3, gLnk(0, "990808", "../mjolner/internal/eval/990808.txt"));
 			    insFld(aux3, gLnk(0, "990816", "../mjolner/internal/eval/990816.txt"));
 			    insFld(aux3, gLnk(0, "990817", "../mjolner/internal/eval/990817.txt"));
+			    insFld(aux3, gLnk(0, "990818", "../mjolner/internal/eval/990818.txt"));
 
 		     aux3 = insFld(aux2, gFld("Architecture", "../mjolner/internal/architecture"));
 			    insFld(aux3, gLnk(0, "990319", "../mjolner/internal/architecture/990319/index.html"));
