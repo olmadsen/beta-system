944bbb3236df688c2393f9436a4d708d1da7ab0f
Peter Andersen <datpete@gmail.com>
1999-08-23 07:52:20 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@9170 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- sysutils/private/external/int64.c
+++ sysutils/private/external/int64.c
@@ -18,10 +18,16 @@ INT64 c64(long a, long b){ return ((INT64)a << 32) + b;}
 INT64 a64(INT64 a, INT64 b){ return a + b;}
 INT64 s64(INT64 a, INT64 b){ return a - b;}
 
+/* The following requires
+ *   /usr/local/lib/gcc-lib/sparc-sun-solaris2.5.1/2.8.1/libgcc.a
+ * on sparc.
+ */
+#if 0
 INT64 m64(INT64 a, INT64 b){ return a * b;}
 INT64 d64(INT64 a, INT64 b){ return a / b;}
-
 INT64 r64(INT64 a, INT64 b){ return a % b;}
+#endif
+
 int eq64(INT64 a, INT64 b){return a == b;}
 int ne64(INT64 a, INT64 b){return a != b;}
 int lt64(INT64 a, INT64 b){return a < b;}
@@ -29,3 +35,13 @@ int le64(INT64 a, INT64 b){return a <= b;}
 int gt64(INT64 a, INT64 b){return a > b;}
 int ge64(INT64 a, INT64 b){return a >= b;}
 
+#define TEST 1
+#undef TEST
+#ifdef TEST
+#include <stdio.h>
+int main()
+{
+  fprintf(stdout, "%d\n", (int)m64(2,3));
+  fflush(stdout);
+}
+#endif
