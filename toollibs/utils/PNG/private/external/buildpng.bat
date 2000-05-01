0ca3153cd49d82b881b6b956b80c683a035dcc95
Peter Andersen <datpete@gmail.com>
2000-05-01 13:22:59 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@10435 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- toollibs/utils/PNG/private/external/buildpng.bat
+++ toollibs/utils/PNG/private/external/buildpng.bat
@@ -2,12 +2,12 @@ mkdir /s ..\..\..\..\..\..\lib\nti
 
 chdir zlib
 nmake /f msdos\makefile.w32
-move zlib.lib ..\..\..\..\..\..\lib\nti\zlib.lib
+move zlib.lib ..\..\..\..\..\..\lib\nti\
 
 
 chdir ..\libpng
 nmake /f scripts\makefile.w32
-move libpng.lib ..\..\..\..\..\..\lib\nti\zlib.lib
+move libpng.lib ..\..\..\..\..\..\lib\nti\
 
 chdir ..
 
