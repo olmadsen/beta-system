d550e191c85ec5633e97720b85636587ca4a3e7a
Peter Andersen <datpete@gmail.com>
2000-05-30 14:41:02 +0000

    eliminated need to *generate* data.c and data.h /pa+hml
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@10565 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ betarun/C/data.c
@@ -0,0 +1,9 @@
+#include "define.h"
+#include <stdio.h>
+#include "heap.h"
+#include "macro.h"
+#ifdef MT
+#include <thread.h>
+#endif
+#include "declare.h" 
+#include "data.gen"
---
+++ betarun/C/data.h
@@ -0,0 +1,9 @@
+#include "export.h"
+#include "data.gen" 
+
+#undef Cell  
+#undef Var   
+#undef UVar  
+#undef Func  
+#undef Array 
+#undef Struct
