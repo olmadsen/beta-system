265f8d49a901cb491120d2dd5d7a8999e3b01c04
Peter Andersen <datpete@gmail.com>
2000-06-26 09:03:24 +0000

    fixed Illegal Leave sparc dump
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@10632 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/CRUN/ExitObjects.c
+++ betarun/CRUN/ExitObjects.c
@@ -22,6 +22,11 @@ void CExitO(long exitAddr, Object * exitObj, Object * theObj)
   Component * theComp;
   RegWin * rw;		/* Callers Register Window */
 
+  /* Save global vars in case of error */
+  Component *OrigActiveComponent          = ActiveComponent;
+  CallBackFrame *OrigActiveCallBackFrame  = ActiveCallBackFrame;
+  ComponentBlock *OrigLastCompBlock       = lastCompBlock;
+
   DEBUG_CODE(NumExO++);
 
   Ck(exitObj); 
@@ -29,8 +34,6 @@ void CExitO(long exitAddr, Object * exitObj, Object * theObj)
   /* FIXME: Compiler currently (v384) generates offset in thoObj parameter
    * Ck(theObj);
    */
-  /* We return to exitAddr (the -8 is the SPARC convention) */
-  setret(exitAddr-8);
 
   /* We need to read the stack, thus this trap to flush regwins */
   __asm__("ta 3");
@@ -40,14 +43,12 @@ void CExitO(long exitAddr, Object * exitObj, Object * theObj)
     return;			/* to exitAddr */
     
   while ((theObj = (Object *) rw->i0) != exitObj) {
-#ifdef FISKEMAND
+#ifdef LEAVE_ACROSS_CALLBACK
     if ((CallBackFrame *)rw == ActiveCallBackFrame){
       /* This is AR of HandleCB. Update ActiveCallBackFrame.   */
       ActiveCallBackFrame = (CallBackFrame *)  rw->l5;
       rw = (RegWin *)rw->l6 /* skip to betaTop */;
-#ifdef RTDEBUG
-      fprintf(output, "RTS: Leaving callback.\n");
-#endif
+      DEBUG_CODE(fprintf(output, "RTS: Leaving callback.\n"));
     } else {
 #endif
       /* Ordinary BETA activation record */
@@ -57,7 +58,14 @@ void CExitO(long exitAddr, Object * exitObj, Object * theObj)
 	DEBUG_CODE(NumTermComp++);
 	theComp = ActiveComponent;
 	if (theComp->CallerComp == 0){
-	  /* attempt to leave basic component! */
+	  /* Attempt to leave basic component! */
+	  /* Restore global variables to ensure correct dump.
+	   * FramePointer and StackPointer are still unchanged 
+	   * at this point.
+	   */
+	  ActiveComponent     = OrigActiveComponent;
+	  ActiveCallBackFrame = OrigActiveCallBackFrame;
+	  lastCompBlock       = OrigLastCompBlock;
 	  BetaError(LeaveBasicCompErr,theObj);
 	}
 	ActiveComponent = theComp->CallerComp; theComp->CallerComp = 0;
@@ -71,10 +79,18 @@ void CExitO(long exitAddr, Object * exitObj, Object * theObj)
       } 
       /* go one step back */
       rw = (RegWin *) rw->fp;
-#ifdef FISKEMAND
+#ifdef LEAVE_ACROSS_CALLBACK
     }
 #endif
   }
+
+  /* ActiveCallbackFrame, lastCompBlock, ActiveComponent have all
+   * been incrementally updated above.
+   */
+
+  /* We return to exitAddr (the -8 is the SPARC convention) */
+  setret(exitAddr-8);
+  /* Unwind stack */
   FramePointer = (long *) rw;
 }
 
