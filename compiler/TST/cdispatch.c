7d0ad19fd207acb7d61b97717cb5313ae07f04ea
Ole Lehrmann Madsen <olm@cs.au.dk>
2000-07-26 11:06:45 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@10763 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/TST/cdispatch.c
+++ compiler/TST/cdispatch.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define test 0
+#ifdef nti
+# define STDCALL _stdcall
+#else
+# define STDCALL
+#endif
 
-struct arg { long argType, a;} arg;
 
-struct T
-{ long rgvarg;
-  struct arg *b;
-  long c,d; 
-} T;
+#define test 1
 
-struct T*  InitDispatch(long rgvarg) 
-{ struct T *S;
-  if (test) printf("InitDispatch:%i\n",rgvarg);
-  S = (struct T*)malloc(sizeof(T));
-  S->rgvarg = 0;
-  S->b = (struct arg*)malloc(rgvarg*2*4);
+
+
+struct VARIANT { long argType, argVal;} VARIANT;
+
+struct struct_tagDISPPARAMS
+{ struct VARIANT *rgvarg;
+  struct VARIANT *rgdispidNamedArgs;
+  long cArgs, cNamedArgs;
+} struct_tagDISPPARAMS;
+
+struct struct_tagDISPPARAMS *InitDispatch(long cArgs) 
+{ struct struct_tagDISPPARAMS *S;
+  if (test) printf("InitDispatch:%i\n",cArgs);
+  S = (struct struct_tagDISPPARAMS*)malloc(sizeof(struct_tagDISPPARAMS));
+  S->cArgs = 0;
+  S->rgvarg = (struct VARIANT*)malloc(cArgs*sizeof(VARIANT));
   return S;
 }
+
 char *MkNameList(char *name) 
 { if (test) printf("MkNameList: %s\n",name);
   return name;
 }
+
 long textLen(char *name)
 { long i,len=0;
   for (i=0; name[i]>0; i++) len= len +1;
@@ -36,43 +47,41 @@ void copyText(char *name, char *dest, long len)
   for (i=0; i<len; i++) dest[i] = name[i];
 }
 
-int  CallInvoke() {}
-int  AddDispatchInt8() {}
-int  AddDispatchInt16() {}
-
-struct T * AddDispatchInt32(struct T *S,long arg, long argType) 
+struct struct_tagDISPPARAMS *AddDispatchInt32(struct struct_tagDISPPARAMS *S,long arg, long argType) 
 { long next;
-  next = S->rgvarg;
+  next = S->cArgs;
   if (test) 
      printf("AddDispatchInt32, next=%i, arg=%i, type=%i\n",next,arg,argType);
 
-  S->b[next].argType = argType;
-  S->b[next].a = arg;
+  S->rgvarg[next].argType = argType;
+  S->rgvarg[next].argVal = arg;
   next = next + 1;
-  S->rgvarg = next;
+  S->cArgs = next;
   return S;
 }
-int  AddDispatchTxtCst() {}
-int  AddDispatchRef() {}
 
-int getType(struct arg *S,long N) 
+
+int getType(struct VARIANT *S,long N) 
 { if (test) 
-     printf("getType: N=%i type=%i arg=%i\n",N,S[N-1].argType,S[N-1].a);
+     printf("getType: N=%i type=%i arg=%i\n",N,S[N-1].argType,S[N-1].argVal);
   return S[N-1].argType;
 }
-int getArg(struct arg *S,long N) 
+
+int getArg(struct VARIANT *S,long N) 
 { if (test)  
-     printf("getArg: N=%i type=%i arg=%i\n",N,S[N-1].argType,S[N-1].a);
-  return S[N-1].a;
+     printf("getArg: N=%i type=%i arg=%i\n",N,S[N-1].argType,S[N-1].argVal);
+  return S[N-1].argVal;
 }
-char *getTextArg(struct arg *S,long N) 
+
+char *getTextArg(struct VARIANT *S,long N) 
 { if (test)  
-     printf("getArg: N=%i type=%i arg=%S\n",N,S[N-1].argType,S[N-1].a);
-  return (char *)S[N-1].a;
+     printf("getArg: N=%i type=%i arg=%S\n",N,S[N-1].argType,S[N-1].argVal);
+  return (char *)S[N-1].argVal;
 }
-long MkResList()
+
+long *MkResList()
 { if (test) printf("MkResList\n");
-  return malloc(8);
+  return (long *)malloc(8);
 }
 
 long GetDispId(long *dispList)
@@ -84,8 +93,8 @@ void PutDispId(long dispId, long *dispList)
   dispList[0] = dispId;
 }
 
-struct T *MkArgList(long noOfArgs,...)
-{ struct T *S;
+struct struct_tagDISPPARAMS *MkArgList(long noOfArgs,...)
+{ struct struct_tagDISPPARAMS *S;
 
   long types[noOfArgs];
 
@@ -120,3 +129,71 @@ struct T *MkArgList(long noOfArgs,...)
   va_end(ap);
   return S;
 }
+
+struct idispatch; // forward
+struct vtbl
+{ long (STDCALL *QI)(struct idispatch *this);
+  long(STDCALL *AddRef)(struct idispatch *this);
+  long (STDCALL *DeleteRef)(struct idispatch *this);
+  long (STDCALL *GetIdsOfNames)(struct idispatch *this
+				, char *names
+				, long *resList);
+  char (STDCALL *Invoke)(struct idispatch *this
+			 ,long dispIdMember
+			 ,struct struct_tagDISPPARAMS *pDispParams );
+};
+
+struct idispatch
+{ struct vtbl * proto; /* pointer to virtual dispatch table */
+};
+
+long BETA_Invoke(struct idispatch *pdisp
+		 ,char *pszName
+		 ,long noOfArgs
+		 ,...)
+{ char * nameList;
+  long * resList;
+  long HR,dispid;
+  struct struct_tagDISPPARAMS *argList;
+  long types[noOfArgs];
+  va_list ap;
+  long i;
+
+  if (test) printf("BETA_Invoke: %s noOfArgs=%i\n",pszName,noOfArgs);
+
+  argList = InitDispatch(noOfArgs);
+  va_start(ap,noOfArgs);
+
+  for (i=0; i<noOfArgs; i++) types[i] = (long) va_arg(ap,long);
+
+  for (i=0; i<noOfArgs; i++)
+    { long arg=0;
+      long type =types[i];
+      if (test) printf("type= %i arg=",type);
+      switch (types[i]) {
+      case 3: // long
+	arg = va_arg(ap,long);
+	if (test) printf("%i\n",arg);    
+	argList = AddDispatchInt32(argList,arg,type);
+	break;
+      case 4: // text
+	arg = (long) va_arg(ap,char *);
+	if (test) printf("%s\n",arg);    
+	argList = AddDispatchInt32(argList,arg,type);
+	break;
+      default:
+	printf("MkArgList: unknown argument type: %i\n",types[i]);
+      };
+    };
+  va_end(ap);
+  nameList = MkNameList(pszName);
+
+  resList = MkResList();
+
+
+  HR = pdisp->proto->GetIdsOfNames(pdisp,nameList,resList);
+  dispid = resList[0];
+
+  pdisp->proto->Invoke(pdisp,dispid,argList);
+}
+
