a272fc88e5d52870aa98e6ba852e83226e057e59
Erik Corry <corry@mjolner.dk>
2000-07-27 14:58:20 +0000

    Added script and link directives to let libpng and libz work on nti_gnu Note that the .lib files were moved for nti_ms from %betalib%\lib\nti to %betalib%\lib\nti_ms.  You may have to remove the nti directory by hand if you have an MS delvelopment environment.
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@10780 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- toollibs/utils/PNG/private/external/buildpng.bat
+++ toollibs/utils/PNG/private/external/buildpng.bat
@@ -1,13 +1,13 @@
-mkdir /s %betalib%\lib\nti
+mkdir /s %betalib%\lib\nti_ms
 
 chdir zlib
 nmake /f msdos\makefile.w32
-move zlib.lib %betalib%\lib\nti\
+move zlib.lib %betalib%\lib\nti_ms\
 
 
 chdir ..\libpng
 nmake /f scripts\makefile.w32
-move libpng.lib %betalib%\lib\nti\
+move libpng.lib %betalib%\lib\nti_ms\
 
 chdir ..
 
