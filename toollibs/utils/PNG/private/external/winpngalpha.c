fae1d7d0cfe928b6b8bb0e1a700cd0a81d6a5138
Michael Lassen <henryml@mjolner.dk>
2001-02-07 16:26:45 +0000

    ...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@12022 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- toollibs/utils/PNG/private/external/winpngalpha.c
+++ toollibs/utils/PNG/private/external/winpngalpha.c
@@ -16,6 +16,96 @@ int ReadPNGalpha(char *name, HBITMAP *phbmp, int *width, int *height)
   BetaImage image;
   
   void *pBits = NULL;
+  unsigned char *src;
+  unsigned char *dst;
+  unsigned char  r, g, b, a;
+  int i;
+  
+  HBITMAP hBmp;
+  
+  result = BetaReadPNG(name, &image, 2);
+  if(result != 0) {
+    return result;
+  }
+  
+  *width = image.width;
+  *height = image.height;
+  
+  {
+    BITMAPINFO bmp = { { sizeof(BITMAPINFOHEADER), image.width, image.height, 1, 32 } };
+    hBmp = CreateDIBSection(NULL, & bmp, DIB_RGB_COLORS, & pBits, NULL, 0);
+
+    src = (char *) image.data;
+    dst = (char *) pBits;
+    for(i = 0; i < image.width * image.height; i++) {
+      r = src[0];
+      g = src[1];
+      b = src[2];
+      a = src[3];
+      dst[0] = b * a / 0xFF;
+      dst[1] = g * a / 0xFF;
+      dst[2] = r * a / 0xFF;
+      dst[3] = a;
+      src += 4;
+      dst += 4;
+    }
+  }
+  /* Return the bitmap */
+  *phbmp = hBmp;
+  return 0;
+}
+
+int ReadPNGalphaImage(char *name, HBITMAP *phbmp, int *width, int *height)
+{
+  int result;
+  BetaImage image;
+  
+  void *pBits = NULL;
+  char *src;
+  char *dst;
+  char r, g, b, a;
+  int i;
+  
+  HBITMAP hBmp;
+  
+  result = BetaReadPNG(name, &image, 2);
+  if(result != 0) {
+    return result;
+  }
+  
+  *width = image.width;
+  *height = image.height;
+  
+  {
+    BITMAPINFO bmp = { { sizeof(BITMAPINFOHEADER), image.width, image.height, 1, 32 } };
+    hBmp = CreateDIBSection(NULL, & bmp, DIB_RGB_COLORS, & pBits, NULL, 0);
+
+    src = (char *) image.data;
+    dst = (char *) pBits;
+    for(i = 0; i < image.width * image.height; i++) {
+      r = src[0];
+      g = src[1];
+      b = src[2];
+      a = src[3];
+      dst[0] = 255 - a;
+      dst[1] = 255 - a;
+      dst[2] = 255 - a;
+      dst[3] = 255;
+      src += 4;
+      dst += 4;
+    }
+  }
+  /* Return the bitmap */
+  *phbmp = hBmp;
+  return 0;
+}
+
+int ReadPNGrgbImage(char *name, HBITMAP *phbmp, int *width, int *height)
+{
+  int result;
+  BetaImage image;
+  
+  void *pBits = NULL;
   char *src;
   char *dst;
   char r, g, b, a;
@@ -45,7 +135,7 @@ int ReadPNGalpha(char *name, HBITMAP *phbmp, int *width, int *height)
       dst[0] = b;
       dst[1] = g;
       dst[2] = r;
-      dst[3] = a;
+      dst[3] = 255;
       src += 4;
       dst += 4;
     }
@@ -55,12 +145,14 @@ int ReadPNGalpha(char *name, HBITMAP *phbmp, int *width, int *height)
   return 0;
 }
 
+
+
 void AlphaDraw(HDC hDC, int x, int y, int width, int height, HBITMAP hBmp)
 {
   HDC     hMemDC = CreateCompatibleDC(hDC);
   HGDIOBJ hOld   = SelectObject(hMemDC, hBmp);
   BLENDFUNCTION pixelblend = { AC_SRC_OVER, 0, 255, 0x1 };
-  AlphaBlend(hDC, x, y+height, width, height, hMemDC, 0, 0, width, height, pixelblend); // blend with per-pixel alpha
+  AlphaBlend(hDC, x, y, width, height, hMemDC, 0, 0, width, height, pixelblend); // blend with per-pixel alpha
   SelectObject(hMemDC, hOld);
   DeleteObject(hMemDC);
 }
