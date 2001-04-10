aafd39e01b523937e8a8abe84e118e93a8d816d8
Erik Corry <corry@mjolner.dk>
2001-04-10 15:36:09 +0000

    Just testing Betarun version number: 4
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@12192 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/version.c
+++ betarun/version.c
@@ -1,7 +1,11 @@
-char betarun_version[] = "3";
+char betarun_version[] = "4";
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2001/04/10 15:16:59  beta
+ * Betarun version number: 3
+ * Added (c) message
+ *
  * Revision 1.2  2001/04/10 14:55:50  corry
  *
  * Betarun version number: 2
