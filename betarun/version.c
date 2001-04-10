0cb4f9dc8f95d2873d8c58a1a82dd41f6c15fbad
beta <beta@daimi.au.dk>
2001-04-10 15:16:59 +0000

    Betarun version number: 3 Added (c) message
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@12190 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/version.c
+++ betarun/version.c
@@ -1,7 +1,13 @@
-char betarun_version[] = "2";
+char betarun_version[] = "3";
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2001/04/10 14:55:50  corry
+ *
+ * Betarun version number: 2
+ * Developer:
+ * Added (C) message to Makefile-toplev
+ *
  * Revision 1.1  2001/04/10 13:49:53  corry
  * Added version.c
  *
