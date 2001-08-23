d224c424c00516b06785db9d75357e83e7e62747
Michael Lassen <henryml@mjolner.dk>
2001-08-23 14:59:20 +0000

    ...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@12545 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- guienv/private/winnt/external/ovalfill.c
+++ guienv/private/winnt/external/ovalfill.c
@@ -5,14 +5,18 @@ typedef int Sint32;
 
 static unsigned char bits[255];
 
-static HBITMAP bitmap;
+static HBITMAP bitmap = NULL;
+static HBITMAP prev_bitmap;
 static HDC hdc;
 static unsigned char *data;
 static int width;
 static int height;
 static int rowbytes;
 
-static unsigned char *stencil;
+static unsigned char *stencil = 0;
+
+static int limit;
+
 
 
 static int number_of_bits_set( Uint32 a )
@@ -59,7 +63,7 @@ void fill_oval(int x, int y, int owidth, int oheight)
   unsigned char *stencil_ptr;
    
 
-  if(owidth >= 128 || oheight >= 96) {
+  if(owidth >= limit || oheight >= limit) {
     return;
   }
 
@@ -77,9 +81,9 @@ void fill_oval(int x, int y, int owidth, int oheight)
         data_ptr[i] = 0;
         stencil_ptr[i] += bits[value];
       }
-      data_ptr += 128;
+      data_ptr += limit;
     }
-    stencil_ptr += 128;
+    stencil_ptr += limit;
   }
   
   stencil_ptr = stencil;
@@ -98,61 +102,65 @@ void fill_oval(int x, int y, int owidth, int oheight)
         }
       }
     }
-    stencil_ptr+= 128;
+    stencil_ptr+= limit;
   }
   
 }
 
-
-
-void init_ovals(void)
+void oval_limit(int size)
 {
-  unsigned char *data_ptr;
-  
-  int value;
-  int gray;
-
-  int i, j;
-  int minor;
-
-  int red, green, blue;
   
   unsigned char *stencil_ptr;
-   
-
-  HDC screen;
+  int i, j;
   
+  limit = size;
+
+
+  if(stencil) {
+    free(stencil);
+    stencil = NULL;
+  }
+  if(bitmap) {
+    SelectObject(hdc, prev_bitmap);
+    DeleteObject(bitmap);
+    bitmap = NULL;
+  }
   
-
-  init_bits();
-
-  stencil = (unsigned char *) malloc(96 * 128);
+  stencil = (unsigned char *) malloc(limit * limit);
   stencil_ptr = stencil;
+  
    
-  for (j = 0; j < 96; j++) {
-    for(i = 0; i < 128; i++) {
+  for (j = 0; j < limit; j++) {
+    for(i = 0; i < limit; i++) {
       *stencil_ptr = 0;
       stencil_ptr++;
     }
   }
-  
-  
-  width = 1024;
-  height = 768;
-  rowbytes = 128;
-  
-  
+
   data = NULL;
   
   {
-    BITMAPINFO bmp = { { sizeof(BITMAPINFOHEADER), width, -height, 1, 1 } };
+    BITMAPINFO bmp = { { sizeof(BITMAPINFOHEADER), limit*8, -limit*8, 1, 1 } };
     bitmap = CreateDIBSection(NULL, & bmp, DIB_RGB_COLORS, &data, NULL, 0);
-  }
 
+    prev_bitmap = SelectObject(hdc, bitmap);
+  }
   
+  
+}
+
+
+void init_ovals(void)
+{
+  HDC screen;
+ 
+
+  init_bits();
+
   screen = GetDC(NULL);
   hdc = CreateCompatibleDC(screen);
-  SelectObject(hdc, bitmap);
+
+  oval_limit(96);
   return;
 }
 
