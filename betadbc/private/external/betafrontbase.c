ae6a04a239c1163380acca58bf0b0ec487650821
Karsten Strandgaard Jorgensen <karsten@mjolner.dk>
2001-09-06 12:59:03 +0000

    06.09.2001 by KSJ - Changed interface names from betadbc-* to betadbc_* and made sure that recent additions to r5.2.1 are in r5.3  as well
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@12628 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betadbc/private/external/betafrontbase.c
+++ betadbc/private/external/betafrontbase.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Frontbase_getInteger(void *r[],int inx)
-{
-  return (*(int*)(r[inx-1]));
+int Frontbase_getInteger(void *r[],int inx) {
+  if (r[inx-1])
+    return (*(int*)(r[inx-1]));
+  return 0;
 }
 
-char* Frontbase_getText(void *r[],int inx)
-{
-  return ((char*) r[inx-1]);
+char* Frontbase_getText(void *r[],int inx) {
+  if (r[inx-1])
+    return ((char*) r[inx-1]);
+  return "";
 }
 
-double Frontbase_getReal(void *r[],int inx)
-{
-  return (*(double*) r[inx-1]);
+double Frontbase_getReal(void *r[],int inx) {
+  if (r[inx-1])
+    return (*(double*) r[inx-1]);
+  return 0.0;
 }
 
-unsigned char Frontbase_getBoolean(void *r[], int inx)
-{
-  return (*(unsigned char*) r[inx-1]);
+unsigned char Frontbase_getBoolean(void *r[], int inx) {
+  if (r[inx-1])
+    return (*(unsigned char*) r[inx-1]);
+  return '\0';
 }
 
