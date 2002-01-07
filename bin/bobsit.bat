d979bf7b1e8314b2c4e1bc7afc429d73c2e5529c
Erik Corry <corry@mjolner.dk>
2002-01-07 15:47:02 +0000

    Add -b option for booting new spor
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13009 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- bin/bobsit.bat
+++ bin/bobsit.bat
@@ -1,22 +1,47 @@
 @echo off
 
+echo bobsit version 1.6
+
+if "%1"=="-b" goto isboot
+set boot=0
 if "%1"=="" goto missing
-if "%2"=="" goto start
-goto missing
+if not "%2"=="" goto missing
+set grammar=%1
+:normal
+echo normal exbobs
+exbobs < %grammar%-parser.bobs
+goto start
+
+:isboot
+set boot=1
+if "%2"=="" goto missing
+echo 3
+if not "%3"=="" goto missing
+echo 4
+set grammar=%2
+echo 5
+if not "%grammar%"=="metagrammar" goto normal
+echo booting metagrammar with bobsit
+%BETALIB%\boot\bin\nti_%MIASDK%\exbobs < %BETALIB%\boot\grammars\metagrammar\%grammar%-parser.bobs
+echo 6
 
 :start
-echo bobsit version 1.6
-NTIexbobs < %1-parser.bobs
 if not exist bobslist goto KEEPGOING
-if exist %1-parser.lst del %1-parser.lst
-rename bobslist %1-parser.lst
+if exist %grammar%-parser.lst del %grammar%-parser.lst
+rename bobslist %grammar%-parser.lst
 :KEEPGOING
 
 echo "End-of-BOBS"
 if not exist tables goto gramerrors
 
 echo Creating tables (tabc version 1.6)
-tabc tables %1-parser
+if "%boot%"=="1" goto boottabc
+	tabc tables %grammar%-parser %grammar%
+	goto pasttabc
+:boottabc
+	echo ..boot...tabc tables %grammar%-parser
+	%BETALIB%\boot\bin\nti_%MIASDK%\tabc tables %grammar%-parser %grammar%
+:pasttabc
 
 goto done
 
--- bin/dogram.bat
+++ bin/dogram.bat
@@ -1,13 +1,11 @@
 @echo off
 
-if "%1"=="" goto missing
-if "%2"=="" goto start
-goto missing
+if "%1"=="-b" goto boot
 
-:start
+if "%1"=="" goto missing
+if not "%2"=="" goto missing
 echo generator
 call gen %1
-
 echo bobsit version 1.6
 call exbobs < %1-parser.bobs
 if not exist bobslist goto KEEPGOING
@@ -26,6 +24,28 @@ call makepretty %1
 
 echo morepretty
 call morepretty %1
+
+goto done
+
+:boot
+
+echo Booting %grammar% with dogram
+if "%2"=="" goto missing
+if not "%3"=="" goto missing
+echo generator
+call %BETALIB%\boot\bin\nti_%MIASDK%\gen %2
+echo bobsit version 1.6
+call %BETALIB%\boot\bin\nti_%MIASDK%\exbobs < %2-parser.bobs
+if not exist bobslist goto KEEPGOING
+if exist %2-parser.lst del %2-parser.lst
+rename bobslist %2-parser.lst
+:KEEPGOING
+
+echo "End-of-BOBS"
+if not exist tables goto gramerrors
+
+echo Creating tables (tabc version 1.6)
+call %BETALIB%\boot\bin\nti_%MIASDK%\tabc tables %2-parser %2
 
 goto done
 
