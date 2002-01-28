eb2b9a52da2cd6f5de5a0d506dcbd1464c7a7c14
Michael Lassen <henryml@mjolner.dk>
2002-01-28 11:33:04 +0000

    ...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13096 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ guienv/private/winnt/external/readjpgfile.c
@@ -0,0 +1,149 @@
+#include <windows.h>
+#include "ijl.h"
+
+
+int ReadJPGFileToDib(char *path, BITMAPINFOHEADER **dib)
+{
+  IJLERR jerr;
+  long width;
+  long height;
+  long nchannels;
+  long dib_line_width;
+  long dib_pad_bytes;
+  long wholeimagesize;
+
+  char *buffer;
+  BITMAPINFOHEADER *bmih;
+
+  JPEG_CORE_PROPERTIES jcprops;
+
+  buffer = NULL;
+  bmih = NULL;
+  
+  jerr = ijlInit(&jcprops);
+  
+  if (jerr != IJL_OK) {
+    return 1;
+  }
+
+  jcprops.JPGFile = path;
+
+  jerr = ijlRead(&jcprops, IJL_JFILE_READPARAMS);
+
+  if (jerr != IJL_OK) {
+    ijlFree(&jcprops);
+    return 1;
+  }
+
+  width = jcprops.JPGWidth;
+  height = jcprops.JPGHeight;
+  nchannels = 3;
+
+  dib_line_width = width * nchannels;
+  dib_pad_bytes = IJL_DIB_PAD_BYTES(width, nchannels);
+
+  wholeimagesize = (dib_line_width + dib_pad_bytes) * height;
+
+  buffer = (char *) malloc(sizeof(BITMAPINFOHEADER) + wholeimagesize);
+
+  if(buffer == NULL) {
+    ijlFree(&jcprops);
+    return 1;
+  }
+
+  bmih = (BITMAPINFOHEADER *) buffer;
+
+  bmih->biSize = sizeof(BITMAPINFOHEADER);
+  bmih->biWidth = width;
+  bmih->biHeight = height;
+  bmih->biPlanes = 1;
+  bmih->biBitCount = 24;
+  bmih->biCompression = BI_RGB;
+  bmih->biSizeImage = 0;
+  bmih->biXPelsPerMeter = 0;
+  bmih->biYPelsPerMeter = 0;
+  bmih->biClrUsed = 0;
+  bmih->biClrImportant = 0;
+
+  jcprops.DIBWidth = width;
+  jcprops.DIBHeight = -height;
+  jcprops.DIBChannels = nchannels;
+  jcprops.DIBColor = IJL_BGR;
+  jcprops.DIBPadBytes = dib_pad_bytes;
+  jcprops.DIBBytes = buffer + sizeof(BITMAPINFOHEADER);
+
+  switch(jcprops.JPGChannels) {
+  case 1:
+    jcprops.JPGColor = IJL_G;
+    break;
+  case 3:
+    jcprops.JPGColor = IJL_YCBCR;
+    break;
+  default:
+    jcprops.DIBColor = (IJL_COLOR)IJL_OTHER;
+    jcprops.JPGColor = (IJL_COLOR)IJL_OTHER;
+    break;
+  }
+
+  jerr = ijlRead(&jcprops, IJL_JFILE_READWHOLEIMAGE);
+  
+  if (jerr != IJL_OK) {
+    free(buffer);
+    ijlFree(&jcprops);
+    return 1;
+  }
+
+  *dib = bmih;
+  return 0;
+
+}
+
+
+
+int ReadJPGtoDibSection(char *name, HBITMAP *hbmp, char **pixels, long *width, long *height)
+{
+  BITMAPINFOHEADER *bmih;
+
+  int err;
+  void *pBits;
+  int i;
+  unsigned char *src;
+  unsigned char *dst;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+
+  
+  
+  err = ReadJPGFileToDib(name, &bmih);
+
+  if (err != 0) {
+    return 1;
+  }
+
+  *hbmp = CreateDIBSection(NULL, (BITMAPINFO *) bmih, DIB_RGB_COLORS, &pBits, NULL, 0);
+
+  src = ((unsigned char *) bmih) + sizeof(BITMAPINFOHEADER);
+  dst = (unsigned char *) pBits;
+  
+  for(i = 0; i < (bmih->biWidth) * (bmih->biHeight); i++) {
+
+    r = src[0];
+    g = src[1];
+    b = src[2];
+
+    dst[0] = r;
+    dst[1] = g;
+    dst[2] = b;
+    src += 3;
+    dst += 3;
+  }
+
+  *width = bmih->biWidth;
+  *height = bmih->biHeight;
+  
+  free(bmih);
+
+  
+  return 0;
+}
