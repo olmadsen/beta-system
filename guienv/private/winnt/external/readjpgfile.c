a0710feb1b00ffa9bddc6a125c3a936d82265ba1
Michael Lassen <henryml@mjolner.dk>
2002-01-30 15:27:19 +0000

    ...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13103 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- guienv/private/winnt/external/readjpgfile.c
+++ guienv/private/winnt/external/readjpgfile.c
@@ -121,10 +121,12 @@ int ReadJPGtoDibSection(char *name, HBITMAP *hbmp, char **pixels, long *width, l
     return 1;
   }
 
+  bmih->biBitCount = 32;
   *hbmp = CreateDIBSection(NULL, (BITMAPINFO *) bmih, DIB_RGB_COLORS, &pBits, NULL, 0);
 
   src = ((unsigned char *) bmih) + sizeof(BITMAPINFOHEADER);
   dst = (unsigned char *) pBits;
+  *pixels = pBits;
   
   for(i = 0; i < (bmih->biWidth) * (bmih->biHeight); i++) {
 
@@ -136,7 +138,7 @@ int ReadJPGtoDibSection(char *name, HBITMAP *hbmp, char **pixels, long *width, l
     dst[1] = g;
     dst[2] = b;
     src += 3;
-    dst += 3;
+    dst += 4;
   }
 
   *width = bmih->biWidth;
@@ -147,3 +149,120 @@ int ReadJPGtoDibSection(char *name, HBITMAP *hbmp, char **pixels, long *width, l
   
   return 0;
 }
+
+int WriteDIBtoJPGFile(char *path, BITMAPINFOHEADER *bmih, long quality)
+{
+  IJLERR jerr;
+  long dib_pad_bytes;
+
+  JPEG_CORE_PROPERTIES jcprops;
+
+  jerr = ijlInit(&jcprops);
+  if(jerr != IJL_OK) {
+    return 1;
+  }
+
+  dib_pad_bytes = IJL_DIB_PAD_BYTES(bmih->biWidth, 3);
+
+  jcprops.DIBWidth = bmih->biWidth;
+  jcprops.DIBHeight = -bmih->biHeight;
+  jcprops.DIBBytes = ((unsigned char *) bmih + sizeof(BITMAPINFOHEADER));
+  jcprops.DIBChannels = 3;
+  jcprops.DIBColor = IJL_BGR;
+
+  jcprops.JPGFile = path;
+
+  jcprops.JPGWidth = bmih->biWidth;
+  jcprops.JPGHeight = bmih->biHeight;
+
+  jcprops.JPGChannels = 3;
+  jcprops.JPGColor = IJL_YCBCR;
+  jcprops.JPGSubsampling = IJL_411;
+  jcprops.jquality = quality;
+
+  jerr = ijlWrite(&jcprops, IJL_JFILE_WRITEWHOLEIMAGE);
+
+  ijlFree(&jcprops);
+
+  if (jerr != IJL_OK) {
+    return 1;
+  }
+
+  return 0;
+  
+  
+}
+
+int WriteDIBSectionToJPGFile(char *name,
+                             unsigned char *pixels, long width, long height,
+                             long left, long top, long right, long bottom,
+                             long chan, long quality)
+{
+  long dib_line_width;
+  long nchannels;
+  long dib_pad_bytes;
+  long wholeimagesize;
+  char *buffer;
+  BITMAPINFOHEADER *bmih;
+  int i;
+  int j;
+  
+  unsigned char *src;
+  unsigned char *dst;
+  unsigned char *row;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+  
+ 
+  buffer = NULL;
+  bmih = NULL;
+  
+  nchannels = 3;
+  dib_line_width = (right - left) * nchannels;
+  dib_pad_bytes = IJL_DIB_PAD_BYTES((right - left), nchannels);
+
+  wholeimagesize = (dib_line_width + dib_pad_bytes) * (bottom - top);
+
+  buffer = (char *) malloc(sizeof(BITMAPINFOHEADER) + wholeimagesize);
+  if(buffer == NULL) {
+    return 1;
+  }
+
+  bmih = (BITMAPINFOHEADER *) buffer;
+  
+  bmih->biSize = sizeof(BITMAPINFOHEADER);
+  bmih->biWidth = (right - left);
+  bmih->biHeight = (bottom - top);
+  bmih->biPlanes = 1;
+  bmih->biBitCount = 24;
+  bmih->biCompression = BI_RGB;
+  bmih->biSizeImage = 0;
+  bmih->biXPelsPerMeter = 0;
+  bmih->biYPelsPerMeter = 0;
+  bmih->biClrUsed = 0;
+  bmih->biClrImportant = 0;
+
+  row = pixels;
+  dst = ((unsigned char *) bmih) + sizeof(BITMAPINFOHEADER);
+
+  for (j = 0; j < bmih->biHeight; j++) {
+    src = row + left * chan;
+    for(i = 0; i < bmih->biWidth; i++) {
+      r = src[0];
+      g = src[1];
+      b = src[2];
+      dst[0] = r;
+      dst[1] = g;
+      dst[2] = b;
+      src += chan;
+      dst += 3;
+    }
+    row += width * chan;
+  }
+  WriteDIBtoJPGFile(name, bmih, quality);
+  
+  free(buffer);
+  return 0;
+}
+
