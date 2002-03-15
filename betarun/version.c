1d3a14ffcf9a2093ee99754dd6b1b5aefc16ed6b
Erik Corry <corry@mjolner.dk>
2002-03-15 14:09:07 +0000

    Testing testing Betarun version number: 76++++ -------------------------------------------------------------- -----------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13234 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/version.c
+++ betarun/version.c
@@ -1,7 +1,13 @@
-char betarun_version[] = "BETARUN VERSION 5.3(76+++)";
+char betarun_version[] = "BETARUN VERSION 5.3(76++++)";
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.87  2002/03/15 14:03:48  corry
+ *
+ * Betarun version number: 76+++
+ * --------------------------------------------------------------
+ * -----------
+ *
  * Revision 1.86  2002/03/15 14:02:52  corry
  *
  * Betarun version number: 76++
