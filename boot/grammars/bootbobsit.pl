2493aa28652ff710ba3062e199c901d3d7c373e5
beta <beta@daimi.au.dk>
2002-03-17 22:18:20 +0000

    scripts that can be used from bin/admin/mbs_boottrack (but not yet used)
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13251 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ boot/grammars/bootbobsit.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+sub usage()
+{
+    print "Usage: bootdogram.pl <grammar-name>\n";
+    exit (1);
+}
+
+push(@INC, $ENV{"BETALIB"} . "/bin/admin");
+require "env.perl";
+
+usage if ($#ARGV != 0);
+$grammarname = $ARGV[0];
+
+
+print "boot-bobsit $grammarname-parser.bobs\n";
+if ("metagrammar" eq $grammarname){
+    $bobsfile = "$betalib/boot/grammars/metagrammar/metagrammar-parser.bobs";
+} else {
+    $bobsfile = "$grammarname-parser.bobs";
+}
+system "$betalib/boot/bin/$objdir/exbobs < $bobsfile" || die "exbobs failed\n";
+rename("bobslist", "$grammarname-parser.lst");
+if ( ! -f "tables"){   
+    print "ERRORS IN GRAMMAR\n";
+    print "Look at $grammarname-parser.lst\n";
+} else {
+    # tabc
+    print "boot-tabc\n";
+    system "$betalib/boot/bin/$objdir/tabc tables $grammarname-parser $grammarname" || die "tabc failed\n";
+}
---
+++ boot/grammars/bootdogram.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+sub usage()
+{
+    print "Usage: bootdogram.pl <grammar-name>\n";
+    exit (1);
+}
+
+push(@INC, $ENV{"BETALIB"} . "/bin/admin");
+require "env.perl";
+
+usage if ($#ARGV != 0);
+$grammarname = $ARGV[0];
+   
+
+# generator
+print "boot-generator\n";
+system "$betalib/boot/bin/$objdir/gen $grammarname" || die "gen failed\n";
+
+# bobsit
+do "$betalib/boot/grammars/bootbobsit.pl";
+
