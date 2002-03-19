ef0ff6c54299cdcb4956cd7dbd57a64d0fe7a9e8
Morten Grouleff <grouleff@daimi.au.dk>
2002-03-19 13:39:54 +0000

    underscore for gnu as well. Betarun version number: 83 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13284 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- betarun/version.c
+++ betarun/version.c
@@ -1,7 +1,12 @@
-char betarun_version[] = "BETARUN VERSION 5.3(82++)";
+char betarun_version[] = "BETARUN VERSION 5.3(83)";
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.97  2002/03/19 13:17:03  corry
+ * get rid of ^M breakage from Cygwin
+ * Betarun version number: 82++
+ * -------------------------------------------------------------------------
+ *
  * Revision 1.96  2002/03/19 12:59:15  datpete
  * Removed obsolete Makefiles and GEN backends
  * Betarun version number: 82
