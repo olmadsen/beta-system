30d239bd6e0635e71d68e16cdd9d64d52cf2bc38
Peter Andersen <datpete@gmail.com>
2002-06-11 14:10:09 +0000

    First very preliminary stuff to write a Java binary class file. Compiler version number: 925 Changed files:     compilerrcmversion.bet Added   files:     GENERATOR/JAVA/classfile.bet     GENERATOR/JAVA/classfilebody.bet     GENERATOR/JAVA/classfiledefs.bet     GENERATOR/JAVA/java_opcodes.bet     GENERATOR/JAVA/TEST/helloworld.java     GENERATOR/JAVA/TEST/writehello.bet -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13462 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/JAVA/TEST/helloworld.java
@@ -0,0 +1,19 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply displays "Hello World!" to the standard output.
+ * 
+ * compile with (Sun)
+ *   javac helloworld.java
+ * or (GNU gcc3.1)
+ *   gcj helloworld.java
+ * examine with (Sun)
+ *   javap -c  -private -s -verbose HelloWorld
+ * or (GNU gcc3.1)
+ *   jcf-dump -c HelloWorld
+ * 
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("Hello World!"); //Display the string.
+    }
+}
