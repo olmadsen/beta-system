3b3327e46dbeebd89cf20208c5f3ab90c3017983
Peter Andersen <datpete@gmail.com>
2002-06-14 12:10:55 +0000

    Implemented BUILD for .NET   Added google demo for .NET Compiler version number: 928 Changed files:     compilerrcmversion.bet     GENERATOR/BYTECODE/BCasmlink.bet     GENERATOR/BYTECODE/BCdotnetBody.bet     GENERATOR/BYTECODE/TEST/TODO     GENERATOR/BYTECODE/TEST/dotnettstenvbody.bet     GENERATOR/BYTECODE/TEST/javabctstenvbody.bet     GENERATOR/BYTECODE/TEST/tstenv.bet Added   files:     GENERATOR/BYTECODE/TEST/google.bet     GENERATOR/BYTECODE/TEST/google.cs     GENERATOR/BYTECODE/TEST/google.exe.config -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13468 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/BYTECODE/TEST/google.cs
@@ -0,0 +1,51 @@
+/*
+  Compilation statements: 
+    csc -target:library -out: googleapi/dotnet/GoogleSearchService.dll googleapi/dotnet/GoogleSearchService.cs
+    csc /r:System.dll /r:googleapi/dotnet/GoogleSearchService.dll google.cs 
+*/ 
+using System;
+using System.Text.RegularExpressions;
+
+class Google
+{
+  public static void Main(String[] arguments) 
+    { 
+      String licence = "hweCKhQvbXY6kvKmOpDS2P3lTqQ5ggBu";
+      String query = "";
+      int i;
+      foreach (String a in arguments) query += a + " ";
+      if (query == "") {
+	Console.Write("Search Google for: ");
+	query = Console.ReadLine();
+      }
+      if (query == "") {
+	query = "Ole Lehrmann Madsen BETA.NET";
+      }
+      Console.WriteLine("\nSearching Google for: " + query);
+      GoogleSearchService s = new GoogleSearchService();
+      GoogleSearchResult r = s.doGoogleSearch(licence, query, 0, 10, false, "", false, "", "latin1", "latin1");
+      Console.WriteLine("\n" + r.resultElements.Length + " Results:");
+      for (i=0; i<r.resultElements.Length; i++){
+	// Print result stripped for HTML using Regular Expressions
+	Regex paras  = new Regex("<p>");     // match HTML paragraph break tags
+	Regex breaks = new Regex("<br>");    // match HTML line break tags
+	Regex tags   = new Regex("<[^>]+>"); // match HTML tags
+	String title = r.resultElements[i].title;
+	String snippet = r.resultElements[i].snippet;
+	title = tags.Replace(title, "");
+	Console.WriteLine("\n" + (i+1) + ". " + title);
+	snippet = paras.Replace(snippet, "\n");
+	snippet = breaks.Replace(snippet, "\n");
+	snippet = tags.Replace(snippet, "");
+	Console.WriteLine(snippet);
+	Console.WriteLine("URL: " + r.resultElements[i].URL);
+      }
+      Console.WriteLine();
+    }
+}
+
+
+
+
+
+
