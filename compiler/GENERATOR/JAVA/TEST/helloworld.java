1285daa2db2a0d0af0c978d3e065c9c000f7d6de
Peter Andersen <datpete@gmail.com>
2002-06-17 13:07:02 +0000

    Added line number in semantic errors.   Added some more tracing capabilities to JAVA class file generation Compiler version number: 930 Changed files:     compilerrcmversion.bet     CHECKER/warningsAndErrorsBody.bet     GENERATOR/JAVA/classfilebody.bet     GENERATOR/JAVA/classfiledefs.bet     GENERATOR/JAVA/TEST/Makefile     GENERATOR/JAVA/TEST/helloworld.java Added   files:     GENERATOR/JAVA/TEST/writehello2withtrace.bet -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13470 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/JAVA/TEST/helloworld.java
+++ compiler/GENERATOR/JAVA/TEST/helloworld.java
@@ -18,6 +18,6 @@
  */
 class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("Hello World!"); //Display the string.
+      System.out.println("Hello World!"); 
     }
 }
