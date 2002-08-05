ea3e7165a59ddd7d3d13945fb7ff6b72bb87c443
Ole Lehrmann Madsen <olm@cs.au.dk>
2002-08-05 08:27:05 +0000

    jstring Compiler version number: 974+++++++++++++++++ -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13611 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/BYTECODE/TEST/jstringuser.java
@@ -0,0 +1,9 @@
+class jstringuser extends Object 
+/*  Use pattern List.bet in this Java program */
+{  public void display(String S)
+    { System.out.println(S);
+    }
+    public String get()
+    { return "Greetings from Java";
+    }
+}
