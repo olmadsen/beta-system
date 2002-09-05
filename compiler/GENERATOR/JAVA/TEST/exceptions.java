eb08ea75dd767dc6fcdce4b30fe74a5b45e1d99d
Peter Andersen <datpete@gmail.com>
2002-09-05 08:18:10 +0000

    JavaImage demo program. Java exception demo Compiler version number: 1005++ -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13703 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/JAVA/TEST/exceptions.java
@@ -0,0 +1,28 @@
+// javac exceptions.java
+// java exceptions
+
+public class exceptions
+{
+    public void do_throw() throws ArithmeticException
+    {
+        throw new ArithmeticException();
+    }
+
+    public void try_catch()
+    {
+	try
+	    {
+		do_throw();
+	    } 
+	catch (ArithmeticException e)
+	    {
+		System.out.println("Caught ArithmeticException");
+	    }
+    }
+
+    public static void main(String[] args) 
+    {
+	exceptions e = new exceptions();
+	e.try_catch();
+    }
+}
