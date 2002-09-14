e66b17574b6d3e8358c757b4b17085c43de36331
Peter Andersen <datpete@gmail.com>
2002-09-14 20:24:39 +0000

    BclassUser.java Compiler version number: 1016 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13728 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/BYTECODE/TEST/BclassUser.java
@@ -0,0 +1,40 @@
+/*
+  Compilation statements: 
+    (make clean; jbeta -x tstenv; \rm -f javabc/String.class)
+    jbeta -x Bclass.bet
+    javac -d javabc -classpath javabc BclassUser.java
+    java -classpath javabc BclassUser
+*/ 
+
+class Jclass extends Bclass
+{
+  // Java constructor must transfer origin object to BETA constructor
+  Jclass(tstenv origin)
+    {
+	super(origin);
+    }
+
+  public void compute(int arg)
+    {
+      System.out.println("Java: compute(" + arg + ") called. Calling BETA");
+      super.compute(arg); 
+    }
+}
+
+
+class BclassUser
+{
+    public static void main(String args[])
+    {
+	Bclass b = new Jclass(new tstenv(null));
+	b.set(9);
+	System.out.println(b.get());
+	b.compute(10);
+	System.out.println(b.get());
+	b.compute(-6);
+	System.out.println(b.get());
+	b.compute(40);
+	System.out.println(b.get());
+    } 
+    
+}
