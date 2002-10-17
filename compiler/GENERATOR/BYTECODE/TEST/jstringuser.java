2b83c9fecc326cdb88c4f048169b616ff70f9893
Peter Andersen <datpete@gmail.com>
2002-10-17 11:30:35 +0000

    jstringuser.java with static display method Compiler version number: 1045+ -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13821 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/jstringuser.java
+++ compiler/GENERATOR/BYTECODE/TEST/jstringuser.java
@@ -1,9 +1,17 @@
 class jstringuser extends Object 
 /*  Use pattern List.bet in this Java program */
-{  public void display(String S)
-    { System.out.println(S);
+{  
+    public void display(String S)
+    { 
+	System.out.println(S);
+    }
+    public static void staticdisplay2(String S1, String S2)
+    { 
+	System.out.print  (S1);
+	System.out.println(S2);
     }
     public String get()
-    { return "Greetings from Java";
+    { 
+	return "Greetings from Java";
     }
 }
