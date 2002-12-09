cb7d17082b49c30869b5afa0a11897d1b59381ce
Peter Andersen <datpete@gmail.com>
2002-12-09 14:39:09 +0000

    dotnet bytecode: adapted Structure code. Implemented class Structure except compare operations. Implemented byteswap Compiler version number: 1070 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13906 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/primitives.cs
+++ compiler/GENERATOR/BYTECODE/TEST/primitives.cs
@@ -1,26 +1,39 @@
 
 
-// csc /t:library /o primitives.cs
+// csc /t:library /optimize+ primitives.cs
+// ildasm /text /out=primitives.il primitives.dll 
 
 
 class Primitives {
   public bool not (bool b)
-  {
-    return !b;
-  }
-
+    {
+      return !b;
+    }
+  
   public int bitnot (int i){
     return ~i;
   }
-
+  
   public int ror (int i, int r)
-  {
-    return (i >> r) | ((i & ((1<<r)-1)) << (32-r));
-  }
+    {
+      return (i >> r) | ((i & ((1<<r)-1)) << (32-r));
+    }
   
   public int rol (int i, int r)
-  {
-    return (i << r) | ((i & ((1<<r)-1)) >> (32-r));
-  }
- 
+    {
+      return (i << r) | ((i & ((1<<r)-1)) >> (32-r));
+    }
+  
+  public ushort byteswapshort (ushort x)
+    {
+      return (ushort)((x>>8) | ((x<<8) & 0xff00));
+    }
+
+  public uint byteswapuint (uint x)
+    {
+      uint h = (x>>16);
+      uint l = (x & 0xffff);
+      return ((l>>8) | ((l<<8) & 0xff00)) << 16 | ((h>>8) | ((h<<8) & 0xff00));
+    }
+    
 }
