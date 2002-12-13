a8c151d5206a71c1b887c528e54136b358665680
Peter Andersen <datpete@gmail.com>
2002-12-13 12:38:48 +0000

    bytecode: java structure variables now invokes normal constructor. No need for default constructors Compiler version number: 1075 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13917 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/TST/Structure.java
+++ compiler/TST/Structure.java
@@ -1,125 +1,135 @@
+import java.lang.reflect.*;
 
 public class Structure {
 
-  public Object iOrigin;
-  public Class iProto;
+    public Object iOrigin;
+    public Class iProto;
+    private Constructor iConstructor;
   
-  Structure(Object o, Class p){
-    iOrigin = o;
-    iProto = p;
-  }
-
-  public static Structure AlloS(Object o, Class p) {
-    return new Structure(o, p);
-  }
-
-  public static Structure ObjS(Object o)
-    throws Exception
-  {
-    Class proto = o.getClass();
-    java.lang.reflect.Field origin_field;
-    Object origin;
-    origin_field = proto.getDeclaredField("origin");
-    origin = origin_field.get(o);
-    return new Structure(origin, proto);
-  }
-
-  public Object AlloSI() throws Exception
-  {
-    Object item;
-    java.lang.reflect.Field origin_field;
-    Object origin;
-    item = iProto.newInstance();
-    origin_field = iProto.getDeclaredField("origin");
-    origin_field.set(item, iOrigin);
-    return item;
-  }
-
-  public static boolean eqS(Structure arg1, Structure arg2)
-  {
-    if (arg1==null) {
-      if (arg2==null)
-	return true;
-      return false;
+    Structure(Object o, Class p){
+	iOrigin = o;
+	iProto = p;
+	iConstructor = null;
     }
-    if (arg2==null)
-      return false;
-    if (arg1.iProto != arg2.iProto)
-      return false;
-    if (arg1.iOrigin != arg2.iOrigin)
-      return false;
-    return true;
-  }
 
-  public static boolean neS(Structure arg1, Structure arg2)
-  {
-    return !eqS(arg1, arg2);
-  }
+    public static Structure AlloS(Object o, Class p) {
+	return new Structure(o, p);
+    }
 
-  public static boolean leS(Structure arg1, Structure arg2)
-  { 
-    return (eqS(arg1, arg2) || ltS(arg1, arg2));
-  }
+    public static Structure ObjS(Object o)
+	throws Exception
+    {
+	Class proto = o.getClass();
+	Field origin_field;
+	Object origin;
+	origin_field = proto.getDeclaredField("origin");
+	origin = origin_field.get(o);
+	return new Structure(origin, proto);
+    }
 
-  public static boolean geS(Structure arg1, Structure arg2)
-  { 
-    return (eqS(arg1, arg2) || gtS(arg1, arg2));
-  }
-
-  public static boolean gtS(Structure arg1, Structure arg2)
-  {
-    return ltS(arg2, arg1);
-  }
-
-  public static boolean ltS(Structure arg1, Structure arg2)
-  {
-    Class proto1;
-    Class proto2;
-    Object newObject;
-
-    if (arg1==null) return false;
-    if (arg2==null) return false;
-  
-    proto1 = arg1.iProto;
-    proto2 = arg2.iProto;
-  
-    if (proto1 == proto2)
-      return false;
-
-   /* Prefix of proto1 is the first try */
-  
-    for (proto1 = proto1.getSuperclass();
-	 proto1 != null; /* proto1 != Object## */
-	 proto1 = proto1.getSuperclass()) {
-      if (proto1 == proto2) {
-	/* There is some hope, now we need to check if origins are equal. */
-	if (true /*FIXME: proto2->OriginOff == arg1->iProto->OriginOff*/){
-	  /* The original prototypes have same origin offset 
-	   * i.e. (same prefix level),
-	   * so the result is (arg1->iOrigin == arg2->iOrigin) 
-	   */
-	  return arg1.iOrigin == arg2.iOrigin;
+    public Object AlloSI() throws Exception
+    {
+	Constructor[] constructors = iProto.getDeclaredConstructors();
+	if (iConstructor == null){
+	    search:
+	    for (int i = 0; i < constructors.length; i++) {
+		Class[] parameterTypes = constructors[i].getParameterTypes();
+		if (parameterTypes.length == 1){
+		    if (parameterTypes[0].isAssignableFrom(iOrigin.getClass())){
+			iConstructor = constructors[i];
+			break search;
+		    }
+		}
+	    }
 	}
-	
-	/* If proto1 and proto2 has different Origin Prefix
-	 * we need to generate an object from arg1 and then
-	 * test the resulting origin from the new object.
-	 *   
-	 * We need to generate a new item, as this is currently the only
-	 * way we can get the origin.
-	 * The problem is that there are several origins 
-	 * (one per prefixlevel).
-	 * So we generate an object corresponding to arg1 and find the 
-	 * origin at the offset determined *by the prefix* (proto2). 
-	 * This should be the same as origin of arg2.
-	 */
-	
-	//newObject = arg1.AlloSI();
-	//return true; // FIXME: (Object *)((long*)newObject)[proto2->OriginOff] == (arg2->iOrigin);
-      }
+	return iConstructor.newInstance(new Object[]{iOrigin});
     }
+
+    public static boolean eqS(Structure arg1, Structure arg2)
+    {
+	if (arg1==null) {
+	    if (arg2==null)
+		return true;
+	    return false;
+	}
+	if (arg2==null)
+	    return false;
+	if (arg1.iProto != arg2.iProto)
+	    return false;
+	if (arg1.iOrigin != arg2.iOrigin)
+	    return false;
+	return true;
+    }
+
+    public static boolean neS(Structure arg1, Structure arg2)
+    {
+	return !eqS(arg1, arg2);
+    }
+
+    public static boolean leS(Structure arg1, Structure arg2)
+    { 
+	return (eqS(arg1, arg2) || ltS(arg1, arg2));
+    }
+
+    public static boolean geS(Structure arg1, Structure arg2)
+    { 
+	return (eqS(arg1, arg2) || gtS(arg1, arg2));
+    }
+
+    public static boolean gtS(Structure arg1, Structure arg2)
+    {
+	return ltS(arg2, arg1);
+    }
+
+    public static boolean ltS(Structure arg1, Structure arg2)
+    {
+	Class proto1;
+	Class proto2;
+	Object newObject;
+
+	if (arg1==null) return false;
+	if (arg2==null) return false;
+  
+	proto1 = arg1.iProto;
+	proto2 = arg2.iProto;
+  
+	if (proto1 == proto2)
+	    return false;
+
+	/* Prefix of proto1 is the first try */
+  
+	for (proto1 = proto1.getSuperclass();
+	     proto1 != null; /* proto1 != Object## */
+	     proto1 = proto1.getSuperclass()) {
+	    if (proto1 == proto2) {
+		/* There is some hope, now we need to check if origins are equal. */
+		if (true /*FIXME: proto2->OriginOff == arg1->iProto->OriginOff*/){
+		    /* The original prototypes have same origin offset 
+		     * i.e. (same prefix level),
+		     * so the result is (arg1->iOrigin == arg2->iOrigin) 
+		     */
+		    return arg1.iOrigin == arg2.iOrigin;
+		}
+	
+		/* If proto1 and proto2 has different Origin Prefix
+		 * we need to generate an object from arg1 and then
+		 * test the resulting origin from the new object.
+		 *   
+		 * We need to generate a new item, as this is currently the only
+		 * way we can get the origin.
+		 * The problem is that there are several origins 
+		 * (one per prefixlevel).
+		 * So we generate an object corresponding to arg1 and find the 
+		 * origin at the offset determined *by the prefix* (proto2). 
+		 * This should be the same as origin of arg2.
+		 */
+	
+		//newObject = arg1.AlloSI();
+		//return true; // FIXME: (Object *)((long*)newObject)[proto2->OriginOff] == (arg2->iOrigin);
+	    }
+	}
     
-    return false; 
-  }
+	return false; 
+    }
 
 }
