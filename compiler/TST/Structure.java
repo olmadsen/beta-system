e11a368ee5598dc987dd9510c098dff4d737f109
Peter Andersen <datpete@gmail.com>
2002-12-16 12:00:16 +0000

    bytecode: fixed Structure.ltS for java, so that it handles different origin levels Compiler version number: 1079 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13923 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/TST/Structure.java
+++ compiler/TST/Structure.java
@@ -62,73 +62,50 @@ public class Structure {
     }
 
     public static boolean neS(Structure arg1, Structure arg2)
+    throws Exception
     {
 	return !eqS(arg1, arg2);
     }
 
     public static boolean leS(Structure arg1, Structure arg2)
+    throws Exception
     { 
 	return (eqS(arg1, arg2) || ltS(arg1, arg2));
     }
 
     public static boolean geS(Structure arg1, Structure arg2)
+    throws Exception
     { 
 	return (eqS(arg1, arg2) || gtS(arg1, arg2));
     }
 
     public static boolean gtS(Structure arg1, Structure arg2)
+    throws Exception
     {
 	return ltS(arg2, arg1);
     }
 
     public static boolean ltS(Structure arg1, Structure arg2)
+    throws Exception
     {
-	Class proto1;
-	Class proto2;
-	Object newObject;
-
 	if (arg1==null) return false;
 	if (arg2==null) return false;
-  
-	proto1 = arg1.iProto;
-	proto2 = arg2.iProto;
-  
-	if (proto1 == proto2)
-	    return false;
-
+	Class proto1 = arg1.iProto;
+	Class proto2 = arg2.iProto;
+	if (proto1 == proto2) return false;
 	/* Prefix of proto1 is the first try */
-  
 	for (proto1 = proto1.getSuperclass();
 	     proto1 != null; /* proto1 != Object## */
 	     proto1 = proto1.getSuperclass()) {
 	    if (proto1 == proto2) {
-		/* There is some hope, now we need to check if origins are equal. */
-		if (true /*FIXME: proto2->OriginOff == arg1->iProto->OriginOff*/){
-		    /* The original prototypes have same origin offset 
-		     * i.e. (same prefix level),
-		     * so the result is (arg1->iOrigin == arg2->iOrigin) 
-		     */
+		if (false /*identical originoff's*/){
 		    return arg1.iOrigin == arg2.iOrigin;
 		}
-	
-		/* If proto1 and proto2 has different Origin Prefix
-		 * we need to generate an object from arg1 and then
-		 * test the resulting origin from the new object.
-		 *   
-		 * We need to generate a new item, as this is currently the only
-		 * way we can get the origin.
-		 * The problem is that there are several origins 
-		 * (one per prefixlevel).
-		 * So we generate an object corresponding to arg1 and find the 
-		 * origin at the offset determined *by the prefix* (proto2). 
-		 * This should be the same as origin of arg2.
-		 */
-	
-		//newObject = arg1.AlloSI();
-		//return true; // FIXME: (Object *)((long*)newObject)[proto2->OriginOff] == (arg2->iOrigin);
+		Object newObject = arg1.AlloSI();
+		Field origin_field = proto1.getDeclaredField("origin");
+		return origin_field.get(newObject) == arg2.iOrigin;
 	    }
 	}
-    
 	return false; 
     }
 
