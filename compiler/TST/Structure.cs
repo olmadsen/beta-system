9e7b7b4ef793fed761b3fcc99f8f998a732c6070
Peter Andersen <datpete@gmail.com>
2002-12-17 16:20:51 +0000

    bytecode: Structure class for dotnet now generated by compiler. tstvarptn included in btst Compiler version number: 1083 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13930 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/TST/Structure.cs
+++ compiler/TST/Structure.cs
@@ -1,120 +1,106 @@
 public class Structure {
 
-  public BetaObject iOrigin;
-  public System.Type iProto;
-  
-  Structure(BetaObject o, System.Type p){
-    iOrigin = o;
-    iProto = p;
-  }
-  
-  public static Structure AlloS(BetaObject o, System.Type p) {
-    return new Structure(o, p);
-  }
-  
-  public static Structure objS(BetaObject o){
-    Structure s = new Structure(null, o.GetType());
-    // Cannot be done since BetaObject has no origin field.
-    // Must be done by compiler after objS call
-    // s.iOrigin = o.origin
-    return s;
-  }
+    private System.Object iOrigin;
+    private System.Type iProto;
+    private System.Reflection.ConstructorInfo iConstructor;
 
-  public BetaObject AlloSI() 
-  {
-    BetaObject item = null;
-    item = (BetaObject)(iProto.Assembly.CreateInstance(iProto.AssemblyQualifiedName));
-    // Cannot be done since BetaObject has no origin field.
-    // Must be done by compiler after AlloSI call
-    // item.origin = iOrigin;
-    return item;
-  }
-  
-  public static bool eqS(Structure arg1, Structure arg2)
-  {
-    if (arg1==null) {
-      if (arg2==null)
-	return true;
-      return false;
+    static Structure(){
+	System.Console.WriteLine("\nUSING Structure.cs!\n");
     }
-    if (arg2==null)
-      return false;
-    if (arg1.iProto != arg2.iProto)
-      return false;
-    if (arg1.iOrigin != arg2.iOrigin)
-      return false;
-    return true;
-  }
 
-  public static bool neS(Structure arg1, Structure arg2)
-  {
-    return !eqS(arg1, arg2);
-  }
-
-  public static bool leS(Structure arg1, Structure arg2)
-  { 
-    return (eqS(arg1, arg2) || ltS(arg1, arg2));
-  }
-
-  public static bool geS(Structure arg1, Structure arg2)
-  { 
-    return (eqS(arg1, arg2) || gtS(arg1, arg2));
-  }
-
-  public static bool gtS(Structure arg1, Structure arg2)
-  {
-    return ltS(arg2, arg1);
-  }
-
-  public static bool ltS(Structure arg1, Structure arg2)
-  {
-    System.Type proto1;
-    System.Type proto2;
-    //BetaObject newObject;
-
-    if (arg1==null) return false;
-    if (arg2==null) return false;
+    public Structure(System.Object o, System.Type p){
+	iOrigin = o;
+	iProto = p;
+	iConstructor = null;
+    }
   
-    proto1 = arg1.iProto;
-    proto2 = arg2.iProto;
+    public static Structure AlloS(System.Object o, System.Type p) {
+	return new Structure(o, p);
+    }
   
-    if (proto1 == proto2)
-      return false;
+    public static Structure ObjS(System.Object o){
+	System.Type proto = o.GetType();
+	System.Reflection.FieldInfo origin_field = proto.GetField("origin");
+	System.Object origin = origin_field.GetValue(o);
+	return new Structure(origin, proto);
+    }
 
-   /* Prefix of proto1 is the first try */
-  
-    for (proto1 = proto1.BaseType;
-	 proto1 != null; /* proto1 != Object## */
-	 proto1 = proto1.BaseType) {
-      if (proto1 == proto2) {
-	/* There is some hope, now we need to check if origins are equal. */
-	if (true /*FIXME: proto2->OriginOff == arg1->iProto->OriginOff*/){
-	  /* The original prototypes have same origin offset 
-	   * i.e. (same prefix level),
-	   * so the result is (arg1->iOrigin == arg2->iOrigin) 
-	   */
-	  return arg1.iOrigin == arg2.iOrigin;
+    public System.Object AlloSI(){
+	System.Reflection.ConstructorInfo[] constructors = iProto.GetConstructors();
+	if (iConstructor == null){
+	  for (int i = 0; i < constructors.Length; i++) {
+	    //System.Console.WriteLine("AlloSI: constructor: " + constructors[i].Name);
+	    System.Reflection.ParameterInfo[] parameters = constructors[i].GetParameters();
+	    if (parameters.Length == 1){
+	      //System.Console.WriteLine("Has one parameter of type: " + parameters[0].ParameterType.FullName);
+	      if (parameters[0].ParameterType.IsAssignableFrom(iOrigin.GetType())){
+		iConstructor = constructors[i];
+		break;
+	      }
+	    }
+	  }
 	}
-	
-	/* If proto1 and proto2 has different Origin Prefix
-	 * we need to generate an object from arg1 and then
-	 * test the resulting origin from the new object.
-	 *   
-	 * We need to generate a new item, as this is currently the only
-	 * way we can get the origin.
-	 * The problem is that there are several origins 
-	 * (one per prefixlevel).
-	 * So we generate an object corresponding to arg1 and find the 
-	 * origin at the offset determined *by the prefix* (proto2). 
-	 * This should be the same as origin of arg2.
-	 */
-	
-	//newObject = arg1.AlloSI();
-	//return true; // FIXME: (Object *)((long*)newObject)[proto2->OriginOff] == (arg2->iOrigin);
-      }
+#if false
+	if (iConstructor==null){
+	    System.Console.WriteLine("AlloSI:\tNo constructor with parameter type (origin)\n\t'" + iOrigin.GetType().FullName + "' found in class '" + iProto.FullName + "'!");
+	}
+#endif
+	return iConstructor.Invoke(new System.Object[]{iOrigin});
     }
-    
-    return false; 
-  }
+  
+    public static bool eqS(Structure arg1, Structure arg2) {
+	if (arg1==null) {
+	    if (arg2==null)
+		return true;
+	    return false;
+	}
+	if (arg2==null)
+	    return false;
+	if (arg1.iProto != arg2.iProto)
+	    return false;
+	if (arg1.iOrigin != arg2.iOrigin)
+	    return false;
+	return true;
+    }
 
+    public static bool neS(Structure arg1, Structure arg2) {
+	return !eqS(arg1, arg2);
+    }
+
+    public static bool leS(Structure arg1, Structure arg2) { 
+	return (eqS(arg1, arg2) || ltS(arg1, arg2));
+    }
+
+    public static bool geS(Structure arg1, Structure arg2) { 
+	return (eqS(arg1, arg2) || gtS(arg1, arg2));
+    }
+
+    public static bool gtS(Structure arg1, Structure arg2){
+	return ltS(arg2, arg1);
+    }
+  
+    public static bool ltS(Structure arg1, Structure arg2){
+	System.Type proto1;
+	System.Type proto2;
+	if (arg1==null) return false;
+	if (arg2==null) return false;
+	proto1 = arg1.iProto;
+	proto2 = arg2.iProto;
+	if (proto1 == proto2) return false;
+	  
+	for (proto1 = proto1.BaseType;
+	     proto1 != null;
+	     proto1 = proto1.BaseType) {
+	    if (proto1 == proto2) {
+		if (false){
+		  //return arg1.iOrigin == arg2.iOrigin;
+		} else {
+		    System.Object newObject = arg1.AlloSI();
+		    System.Reflection.FieldInfo origin_field = proto1.GetField("origin");
+		    return origin_field.GetValue(newObject) == arg2.iOrigin;
+		}
+	    }
+	}
+	return false; 
+    }
 }
