b3680d22475227297cdfa8992ded7e4e4f50d742
Peter Andersen <datpete@gmail.com>
2002-12-20 11:09:32 +0000

    bytecode: java repetition operations (not complete) Compiler version number: 1087 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@13935 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/TST/BetaArray.java
+++ compiler/TST/BetaArray.java
@@ -1,17 +1,45 @@
 
-public class BetaArray {
+public abstract class BetaArray {
 
-    static{
-	System.out.println("\nUSING BetaArray.java!\n");
-    }
+  static{
+    System.out.println("\nUSING BetaArray.java!\n");
+  }
   
-    public static char[] ExtVR1(char[] array, int add) {
-	int copysize = array.length;
-	int newsize  = copysize + add;
-	if (newsize<0) newsize = 0;
-	if (copysize>newsize) copysize = newsize;
-	char[] newArray = new char[newsize];
-	System.arraycopy(array, 0, newArray, 0, copysize);
-	return newArray;
-    }
+  public static char[] ExtVR1(char[] array, int add) {
+    int copysize = array.length;
+    int newsize  = copysize + add;
+    if (newsize<0) newsize = 0;
+    if (copysize>newsize) copysize = newsize;
+    char[] newArray = new char[newsize];
+    System.arraycopy(array, 0, newArray, 0, copysize);
+    return newArray;
+  }
+
+  public static char[] NewVR1(char[] array) {
+    int size = array.length;
+    if (size<0) size = 0;
+    return new char[size];
+  }
+
+  public static char[] CopyVR1(char[] array) {
+    int size = array.length;
+    if (size<0) size = 0;
+    char[] newArray = new char[size];
+    System.arraycopy(array, 0, newArray, 0, size);
+    return newArray;
+  }
+
+  public static char[] CopySVR1(char[] array, int low, int high) 
+    throws ArrayIndexOutOfBoundsException
+  {
+
+    // Check that low and high are usable. 
+    if ((low<1) || (array.length < high)) throw new ArrayIndexOutOfBoundsException();
+      
+    int newsize = high - low + 1;
+    if (newsize<0) newsize = 0;
+    char[] newArray = new char[newsize];
+    System.arraycopy(array, low-1, newArray, 0, newsize);
+    return newArray;
+  }
 }
