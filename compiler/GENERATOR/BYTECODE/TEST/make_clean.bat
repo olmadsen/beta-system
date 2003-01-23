1d039c052f1b8151560adc564198645f2610085c
Peter Andersen <datpete@gmail.com>
2003-01-23 21:54:09 +0000

    Changed names of targets javabc and dotnet to jvm and clr Compiler version number: 1106 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14001 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/make_clean.bat
+++ compiler/GENERATOR/BYTECODE/TEST/make_clean.bat
@@ -1,6 +1,6 @@
 del /f /q *~ *.dll *.pdb *.exe *.out *.lst *.class *.jar *.astL *.ast *.a2s *.dump *.diff >NUL 2>1
 del /f /q *.exe.config.new *.suo *.sln >NUL 2>1
-del /s /f /q dotnet javabc sun4s linux nti_ms nti_gnu >NUL 2>1
+del /s /f /q clr jvm sun4s linux nti_ms nti_gnu >NUL 2>1
 del /f /q *.manifest >NUL 2>1
 del /f /q googleapi\dotnet\GoogleSearchService.dll >NUL 2>1
 del /f /q googleapi\dotnet\GoogleSearchService.pdb >NUL 2>1
@@ -10,6 +10,6 @@ jbeta -w tstenv >NUL
 jbeta -w betaenv >NUL
 nbeta -w tstenv >NUL
 nbeta -w betaenv >NUL 
-del /f /q javabc\String.class >NUL 2>1
+del /f /q jvm\String.class >NUL 2>1
 cls
 
