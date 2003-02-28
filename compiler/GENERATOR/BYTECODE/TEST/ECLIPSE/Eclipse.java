065f537d1fee184eaed956ee3cc705da9541c801
Peter Andersen <datpete@gmail.com>
2003-02-28 13:25:18 +0000

    Eclipse test Compiler version number: 1142 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14122 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/ECLIPSE/Eclipse.java
+++ compiler/GENERATOR/BYTECODE/TEST/ECLIPSE/Eclipse.java
@@ -4,8 +4,13 @@ public class Eclipse
 {
     public static void main(String args[])
     {
-	BetaAutoIndentStrategy bais = new BetaAutoIndentStrategy();
+	BetaAutoIndentStrategy bais;
+
+	System.out.println("Eclipse: creating BetaAutoIndentStrategy.");
+	bais = new BetaAutoIndentStrategy();
+	System.out.println("Eclipse: calling customizeDocumentCommand.");
 	bais.customizeDocumentCommand(null, null);
+	System.out.println("Eclipse: done.");
     }
 
 }
