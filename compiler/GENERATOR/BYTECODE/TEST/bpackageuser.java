079839a40f706b3207d599fa41106dfd026ca8cb
Ole Lehrmann Madsen <olm@cs.au.dk>
2003-03-03 22:42:09 +0000

    bytecode:bpackage.bet Compiler version number: 1143+ -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14132 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/BYTECODE/TEST/bpackageuser.java
@@ -0,0 +1,10 @@
+// see bpackage.bet
+import foo.bar.*;
+public class bpackageuser
+{
+    public static void main(String args[])
+    { bpackage BP = new bpackage(new tstenv(null));
+      BP.go();
+    }
+}
+ 
