9453f327ffb4963aab70e60b222f91b7175764a4
Peter Andersen <datpete@gmail.com>
2003-03-13 11:55:38 +0000

    JAVA: class Component is now generated by compiler Compiler version number: 1165 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14160 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/Component.java
+++ compiler/GENERATOR/BYTECODE/TEST/Component.java
@@ -2,58 +2,67 @@ package beta;
 //package org.eclipse.ui.betaeditor.beta;
 
 class Component extends Thread
-{ static Component current;
-    Component caller;
-    BetaObject body;
+{ 
+    static Component current;
+    private Component caller;
+    private BetaObject body;
 
-    Component(){};
-    Component(BetaObject b) { body = b; setDaemon(true); }
-
-    static public void AlloC(BetaObject b)
-    { 
-	Component C = new Component(b);
-	b.comp$ = C;
-	C.caller = C;
+    Component(BetaObject b) { 
+	body = b; 
+	setDaemon(true); 
+	b.comp$ = this; 
+	caller = this; 
     }
+
+    //static public void AlloC(BetaObject b)
+    //{ 
+    //    Component C = new Component(b);
+    //}
+
     synchronized public void swap()
     { 
-	trace("Component:swap");
-	Component X;
-	X = current;
-	current = caller;
-	caller = X;
-	if (!isAlive()) start(); // only relevant if attach
-	else notify();
-	try{ wait(); } catch (InterruptedException e){}
+        // trace("Component:swap");
+        Component X;
+        X = current;
+        current = caller;
+        caller = X;
+        if (!isAlive()) {
+	    start(); // only relevant if attach
+	} else {
+	    notify();
+	}
+        try{ wait(); } catch (InterruptedException e){}
     }
-    synchronized void att()
-    { 
-	trace("Component:att");
-	caller = current;
-	current = this;
-	if (!isAlive()) start();
-	else notify();
-	try{ wait(); } catch (InterruptedException e){}
-    }
-    
-    synchronized void susp()
-    { 
-	trace("Component:susp");
-	current = caller;
-	notify();
-	try{ wait(); } catch (InterruptedException e){}
-    } 
 
     public void run() 
     { 
-	trace("Component:run");
-	body.xdo();
-	trace("Component:terminated");
+        // trace("Component:run");
+        body.xdo();
+        // trace("Component:terminated");
 	synchronized(this) { notify();};
     }
-    public void trace(String T)
-    { 
-	if (false) System.out.println(T);
-    }
+
+    //synchronized void att()
+    //{ 
+    //    trace("Component:att");
+    //    caller = current;
+    //    current = this;
+    //    if (!isAlive()) start();
+    //    else notify();
+    //    try{ wait(); } catch (InterruptedException e){}
+    //}
+    
+    //synchronized void susp()
+    //{ 
+    //    trace("Component:susp");
+    //    current = caller;
+    //    notify();
+    //    try{ wait(); } catch (InterruptedException e){}
+    //} 
+
+    //public void trace(String T)
+    //{ 
+    //    if (false) System.out.println(T);
+    //}
 }
 
