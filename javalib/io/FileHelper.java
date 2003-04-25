392630b149abe2557d63e3ed8aea014e97b9f902
Peter Andersen <datpete@gmail.com>
2003-04-25 10:40:06 +0000

    JVM progress
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14303 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- javalib/io/FileHelper.java
+++ javalib/io/FileHelper.java
@@ -5,4 +5,12 @@ public class FileHelper
     public static int lastModifiedAsSeconds(java.io.File F){
 	return (int)(F.lastModified()/1000);
     }
+
+    public static void setLastModifiedAsSeconds(java.io.File F, int time){
+	F.setLastModified((long)time*1000);
+    }
+
+    public static int lengthAsInt(java.io.File F){
+	return (int)(F.length());
+    }
 }
