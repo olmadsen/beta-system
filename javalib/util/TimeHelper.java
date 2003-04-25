e4304cb00cbe63d0a717da93cd98f8f7fc47746e
Peter Andersen <datpete@gmail.com>
2003-04-25 12:09:27 +0000

    JVM progress
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14305 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- javalib/util/TimeHelper.java
+++ javalib/util/TimeHelper.java
@@ -1,8 +1,11 @@
 package beta;
 
-public class TimeHelper
+public class DateHelper
 {
     public static java.util.Date DateFromSeconds(int seconds){
 	return new java.util.Date((long)seconds*1000);
+    }
+    public static java.util.Date DateNow(){
+	return new java.util.Date();
     }
 }
