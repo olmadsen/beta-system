c56a418bf6b7379b00a7dde033bc42ed54d200f2
Peter Andersen <datpete@gmail.com>
2003-09-04 10:11:42 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14689 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- bin/java2beta.bat
+++ bin/java2beta.bat
@@ -3,14 +3,12 @@
 rem Collect arguments.
 set _opts_=
 :getopts
-if "%1"=="" goto compile
+if "%1"=="" goto java
 set _opts_=%_opts_% %1
 shift
 goto getopts
 
 rem Call the java program
-%BETALIB%\bin\%objdir%\beta.exe %BETAOPTS% %_opts_%
-
-
-java -classpath %%BETALIB%%\javalib\java2beta\classes beta.converter.JavaConverter %_opts_% %%BETALIB%%
+:java
+java -classpath %BETALIB%/javalib/java2beta/classes beta.converter.JavaConverter %_opts_% %BETALIB%
 
