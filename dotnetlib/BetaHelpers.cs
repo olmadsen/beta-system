c5f79fe74f7a279446f65ab7156fc2a26525ce9e
Peter Andersen <datpete@gmail.com>
2003-09-29 12:54:52 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14774 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ dotnetlib/BetaHelpers.cs
@@ -0,0 +1,17 @@
+public class BetaHelpers
+{
+   
+  // static methods
+  public static long int2long(int i){
+    return (long)i;
+  }
+  public static int  long2int(long j){
+    return (int)j;
+  }
+  public static long int2longMul1000(int i){
+    return (long)1000*(long)i;
+  }
+  public static int  long2intDiv1000(long j){
+    return (int)(j/(long)1000);
+  }
+}
