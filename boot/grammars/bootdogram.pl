cb1814253dd5214133cc4ef3478e85072dd969b9
Peter Andersen <datpete@gmail.com>
2003-10-03 11:11:12 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14782 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- boot/grammars/bootdogram.pl
+++ boot/grammars/bootdogram.pl
@@ -20,5 +20,5 @@ print "boot-generator\n";
 system "$betalib/boot/bin/$objdir/gen $grammarname" || die "gen failed\n";
 
 # bobsit
-do "$betalib/boot/grammars/bootbobsit.pl";
+&do_perl("$betalib/boot/grammars/bootbobsit.pl");
 
