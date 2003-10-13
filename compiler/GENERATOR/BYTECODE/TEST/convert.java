44815b35509f216ab8dec9dfd1c9bbd43b81d053
Peter Andersen <datpete@gmail.com>
2003-10-13 12:05:38 +0000

    Bytecode: test program for conversion between primitive integer types Compiler version number: 1322 -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14817 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ compiler/GENERATOR/BYTECODE/TEST/convert.java
@@ -0,0 +1,15 @@
+public class convert
+{
+    int Int64ToInt32(long x){
+	return (int)x;
+    }
+    long Int32ToInt64(int x){
+	return (long)x;
+    }
+    static int staticInt64ToInt32(long x){
+	return (int)x;
+    }
+    static long staticInt32ToInt64(int x){
+	return (long)x;
+    }
+}
