360e79eeee7931b19e112717eae9017899c1b7d0
Peter Andersen <datpete@gmail.com>
2003-10-21 09:42:51 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14855 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- dotnetlib/dotnet2beta/test/pinvoke/helloworld.cs
+++ dotnetlib/dotnet2beta/test/pinvoke/helloworld.cs
@@ -1,10 +1,3 @@
-/* 
-   cl -Fehellolib.dll -Zi -LD hellolib.c -link -DEF:hellolib.def
-   csc -t:library hellowrapper.cs
-   csc -t:exe -r:hellowrapper.dll helloworld.cs
-   helloworld.exe
- */
-
 using System;
 
 public class helloworld
