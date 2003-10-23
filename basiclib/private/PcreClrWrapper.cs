564c51064c35b306606e2cb7330f6e0499cd621d
Peter Andersen <datpete@gmail.com>
2003-10-23 10:27:10 +0000

    CLR pcre implementation
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@14875 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- basiclib/private/PcreClrWrapper.cs
+++ basiclib/private/PcreClrWrapper.cs
@@ -6,24 +6,24 @@ public class PcreClrHelper
 
   [ DllImport("PcreDLL.dll", CallingConvention=CallingConvention.Cdecl) ]
     public static extern int 
-    pcre_compile           (String exp, 
+    pcre_compile           (string exp, 
 			    int options,
-			    ref String errptr,
+			    ref string errptr,
 			    ref int erroffset,
 			    int tableptr);
 
   [ DllImport("PcreDLL.dll", CallingConvention=CallingConvention.Cdecl) ]
     public static extern int 
-    locale_pcre_compile    (String exp, 
+    locale_pcre_compile    (string exp, 
 			    int options,
-			    ref String errptr,
+			    ref string errptr,
 			    ref int erroffset);
 
   [ DllImport("PcreDLL.dll", CallingConvention=CallingConvention.Cdecl) ]
     public static extern int 
     pcre_exec              (int code, 
 			    int extra, 
-			    String subject, 
+			    string subject, 
 			    int length, 
 			    int startOffset, 
 			    int options, 
@@ -34,12 +34,60 @@ public class PcreClrHelper
     public static extern int 
     pcre_study             (int code, 
 			    int options,
-			    ref String errptr);
+			    ref string errptr);
      
   [ DllImport("PcreDLL.dll", CallingConvention=CallingConvention.Cdecl) ]
-    public static extern int[] 
+    public static extern int
     pcre_fullinfo          (int code, 
 			    int extra,
 			    int what,
 			    ref int where);
+
+  /* Fuctions below hide the use of ref parameters,
+   * which is currenly not possible from BETA .
+   */
+
+  static string errtext = null;
+  static int erroffset = 0;
+
+  public static int compile (string exp, int options, int tableptr)
+    {
+      errtext = null;
+      int result = pcre_compile(exp, options, ref errtext, ref erroffset, tableptr);
+      return result;
+    }
+
+  public static int locale_compile (string exp, int options)
+    {
+      errtext = null;
+      int result = locale_pcre_compile(exp, options, ref errtext, ref erroffset);
+      return result;
+    }
+
+  public static int study (int code, int options)
+    {
+      errtext = null;
+      int result = pcre_study(code, options, ref errtext);
+      return result;
+    }
+
+  public static int[] full_info (int code, int extra, int what)
+    {
+      int[] results = new int[2];
+      results[1] = pcre_fullinfo(code, 
+				 extra, 
+				 what, 
+				 ref results[0]);
+      return results;
+    }
+
+  public static string last_error()
+    {
+      return errtext;
+    }
+    
+  public static int last_error_offset()
+    {
+      return erroffset;
+    }
 }
