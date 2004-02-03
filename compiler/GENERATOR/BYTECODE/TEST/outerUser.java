1683abb91ee5e6b2b36c12e4b58728f4c3a655f3
Ole Lehrmann Madsen <olm@cs.au.dk>
2004-02-03 21:13:25 +0000

    outer.bet_outerUser.java_ok Compiler version number: 1389+ -------------------------------------------------------------------------
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@15013 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- compiler/GENERATOR/BYTECODE/TEST/outerUser.java
+++ compiler/GENERATOR/BYTECODE/TEST/outerUser.java
@@ -1,11 +1,23 @@
+/*
+  Compilation: 
+    jbeta outer.bet
+    javac -d jvm -classpath jvm/outer.jar outerUser.java
+  Execution UNIX:
+    java -classpath "jvm/outer.jar:jvm" outerUser
+  Execution Windows:
+    java -classpath "jvm/outer.jar;jvm" BclassUser
+*/ 
 import beta.*;
 
 class outerUser {
     public static void main(String[] args) {
-        beta.betaenv B = new betaenv(null);
-	beta.outer X = new outer(B);
+        betaenv B = new betaenv(null);
+	B.init();
+	outer X = new outer(B);
         outer.inner1 Q = X.new inner1();
-
+	outer.inner1.inner1_2 W = Q.new inner1_2();
+	Q.hello();
+	W.world();
     }
 
 }
