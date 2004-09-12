8ff964def3625cd5f92357809b752cda38af1827
Michael Lassen <henryml@mjolner.dk>
2004-09-12 21:28:25 +0000

    ...
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@15413 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- maclib/external/maclibglue.c
+++ maclib/external/maclibglue.c
@@ -1,24 +1,3 @@
 #include <Carbon/Carbon.h>
 
 
-void CGContextSelectFontGlue (
-   CGContextRef context,
-   const char * name,
-   double size,
-   CGTextEncoding textEncoding
-   ) 
-{
-  CGContextSelectFont(context, name, (float) size, textEncoding);
-}
-
-
-void CGContextShowTextAtPointGlue (
-   CGContextRef context,
-   double x,
-   double y,
-   const char * bytes,
-   size_t length
-   )
-{
-  CGContextShowTextAtPoint(context, (float) x, (float) y, bytes, length);
-}
