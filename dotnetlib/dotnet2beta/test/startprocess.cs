84edcc9758def30d2a31c621570bb367a2993b69
Peter Andersen <datpete@gmail.com>
2004-09-24 13:08:35 +0000

    *** empty log message ***
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@15434 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ dotnetlib/dotnet2beta/test/startprocess.cs
@@ -0,0 +1,12 @@
+// csc -t:exe -r:System.dll startprocess.cs
+
+public class StartProcess
+  {
+    public static void Main()
+        {
+	  System.Diagnostics.Process p = new System.Diagnostics.Process();
+	  p.StartInfo.FileName = "hello.exe";
+	  p.Start();
+	}
+  }
+
