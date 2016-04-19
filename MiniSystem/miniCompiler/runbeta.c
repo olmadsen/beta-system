3dff39be5ad29f310dbcb9d95c2927a9bcc076b1
Ole Lehrmann Madsen <ole.l.madsen@alexandra.dk>
2016-04-19 09:13:27 +0000

    added runbeta.c
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@17307 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ MiniSystem/miniCompiler/runbeta.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "interpreter_c.c"
+void main(int argc, char *argv[])
+{ ObjDesc bc = 0;
+  int descInx = 1;
+  bool isXB = 0;
+  FILE *F;
+  long size ;
+  char *name = "binstore";
+  F = fopen(name, "rb");
+  if( fseek(F, 0, SEEK_END) ){
+      fclose(F);
+      printf("Cannot open file\n");
+    }
+  size =ftell(F);
+  printf("Runbeta: %i %s %i\n",argc,argv[1],size);
+  fseek(F, 0, SEEK_SET);  //same as rewind(f);
+  printf("Rewind\n");
+  bc = malloc(size);
+  fread(bc, size, 1, F);
+  fclose(F); 
+
+  init_interpreter(bc,1283,isXB);
+  bool first = true;
+  getEvent(first); 
+  // for (i=0; i <1000000; i++ ){
+  //getEvent(first);
+  //   first = false;
+  //}
+}
