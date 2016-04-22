664e89f15d8e01371ae8b5ee23f51758f4f514a7
Ole Lehrmann Madsen <ole.l.madsen@alexandra.dk>
2016-04-22 12:59:20 +0000

    clean-up
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@17320 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/miniCompiler/runbeta.c
+++ MiniSystem/miniCompiler/runbeta.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include "interpreter_c.c"
 void main(int argc, char *argv[])
-{ unsigned char* bc;
-  int descInx = 1;
-  bool isXB = 0;
-  FILE *F;
-  long size ;
-  char *name = argv[1];
-  int mainDescInx ;
-  F = fopen(name, "rb");
+{ if (sizeof(argv) < 2) {
+    printf("Usage: runbeta file\n");
+    return;
+  }
+
+  FILE* F = fopen(argv[1], "rb");
   if( fseek(F, 0, SEEK_END) ){
       fclose(F);
       printf("Cannot open file\n");
     }
-  size =ftell(F);
+  int size = ftell(F);
   printf("Runbeta: %i %s %i\n",argc,argv[1],(int)size);
   fseek(F, 0, SEEK_SET);  //same as rewind(f);
-  printf("Rewind\n");
-  bc = (char*)malloc(size * sizeof(char));
+
+  unsigned char* bc = (char*)malloc(size * sizeof(char));
   if (bc == NULL) printf("Malloc error\n");
-  int V;
-  V = fread(bc, 1, size, F);
+  int V = fread(bc, 1, size, F);
   if (V != size) printf("File read error: %i %i\n", (int)size,V);
   fclose(F); 
   
-  int i = 0;
-  /*for (i = 0; i < 25; i++){
-    printf("%u: %u\n",i,bc[i]);
-    }*/
+  bool isXB = 0;
   init_interpreter((ObjDesc)bc,isXB);
   bool first = true;
   getEvent(first); 
