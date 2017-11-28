ed040c6723350af093419aa6df3df7e4a400ec94
Ole Lehrmann Madsen <ole.l.madsen@alexandra.dk>
2017-11-28 11:23:53 +0000

    added init_mmu.S
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@18016 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/VM/arm/armc-09-cstartup.c
+++ MiniSystem/VM/arm/armc-09-cstartup.c
@@ -31,6 +31,8 @@
 extern int __bss_start__;
 extern int __bss_end__;
 
+extern void init_mmu();
+
 extern void kernel_main( unsigned int r0, unsigned int r1, unsigned int atags );
 
 void _cstartup( unsigned int r0, unsigned int r1, unsigned int r2 )
@@ -50,6 +52,8 @@ void _cstartup( unsigned int r0, unsigned int r1, unsigned int r2 )
     */
     while( bss < bss_end )
         *bss++ = 0;
+
+    init_mmu();
 
     /* We should never return from main ... */
     kernel_main( r0, r1, r2 );
