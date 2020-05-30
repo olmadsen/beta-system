7d3da65ee6c5c56b4acda5b6ce7275a90162281f
Ole Lehrmann Madsen <ole.l.madsen@alexandra.dk>
2020-05-30 13:13:24 +0000

    Adsded oldstuff
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@18797 678b6752-a8a0-49ff-a511-7c2a3241fcd4

---
+++ MiniSystem/RaspberryPi/arm-tutorial-rpi-master/part-5/armc-014/build.bat
@@ -0,0 +1,4 @@
+
+
+arm-none-eabi-gcc -O0 -DRPI3 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv8-a -mtune=cortex-a53 -nostartfiles -g -Wl,-T,rpi.x armc-014.c armc-cstubs.c armc-cstartup.c armc-start.S rpi-armtimer.c rpi-aux.c rpi-gpio.c rpi-interrupts.c rpi-mailbox-interface.c rpi-mailbox.c rpi-systimer.c -o kernel.elf
+arm-none-eabi-objcopy kernel.elf -O binary kernel.img
