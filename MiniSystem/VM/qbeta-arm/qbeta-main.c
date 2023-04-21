3ccbb4e4d2d8817c32ed7349e1b73bb358b3c7e3
Ole Lehrmann Madsen <olm@alexandra.dk>
2023-04-21 09:27:44 +0000

    Clean-up
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@20090 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/VM/qbeta-arm/qbeta-main.c
+++ MiniSystem/VM/qbeta-arm/qbeta-main.c
@@ -1,28 +1,6 @@
 #include "header.h"
 extern unsigned char BC[];
 
-#define IO_BASE 0x3f000000
-#define	GP_BASE (IO_BASE + 0x200000)
-#define MU_BASE (IO_BASE + 0x215000)
-
-#define AUX_ENB (*(volatile unsigned *)(MU_BASE + 0x04))
-#define MU_IO   (*(volatile unsigned *)(MU_BASE + 0x40))
-#define MU_LCR  (*(volatile unsigned *)(MU_BASE + 0x4c))
-#define MU_LSR  (*(volatile unsigned *)(MU_BASE + 0x54))
-#define MU_CNTL (*(volatile unsigned *)(MU_BASE + 0x60))
-#define MU_BAUD (*(volatile unsigned *)(MU_BASE + 0x68))
-
-#define GPFSEL1 (*(volatile unsigned *)(GP_BASE + 0x04))
-#define GPPUD   (*(volatile unsigned *)(GP_BASE + 0x94))
-#define GPPUDCLK0   (*(volatile unsigned *)(GP_BASE + 0x98)))
-
-
-void raw_putc(char c)
-{ while (!(MU_LSR & 0x20))
-    ;
-  MU_IO = c;
-}
-
 #include "../interpreter_c.c"
 
 void putint(int V)
@@ -30,8 +8,6 @@ void putint(int V)
   char d[8];
   int i; 
   for (i = 0; i < 8; i++) d[i] = '0';
-  //putch('0');
-  //putch('x');
   for (i = 0; i < 8; i++) {
     d[7 - i] = '0' + V % 10;
     V = V / 10;
