fe00a0989ca240ef24cecfcffee745c2be7a1884
Ole Lehrmann Madsen <olm@alexandra.dk>
2024-04-14 13:40:13 +0000

    UnitIusses clean-up
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@20486 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/qbeta/test/runtest.bat
+++ MiniSystem/qbeta/test/runtest.bat
@@ -8,3 +8,10 @@ cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2 > B_list
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3 > C_list
 
 C:/beta/r5.5/MiniSystem/qbeta/test/testrun1
+
+
+echo "Done"
+
+set /P V="wait:"
+echo V
+cat A_trace.txt B_trace.txt C_trace.txt >trace.txt
