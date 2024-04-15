2b58c4fda3084a2bcef94053f9cce643e668b0f2
Ole Lehrmann Madsen <olm@alexandra.dk>
2024-04-15 11:32:55 +0000

    Clean-up:
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@20488 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/qbeta/test/runtest.bat
+++ MiniSystem/qbeta/test/runtest.bat
@@ -4,6 +4,8 @@ echo %BETALIB%
 set BL=%BETALIB%/MiniSystem/qbeta/test
 echo %BL%
 
+rm A_trace.txt B_trace.txt C_trace.txt trace.txt
+
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2 > B_list 
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3 > C_list
 
