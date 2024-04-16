9d1af32418dbabf41e1a287eaa73a812c6c2cf25
Ole Lehrmann Madsen <olm@alexandra.dk>
2024-04-16 11:35:27 +0000

    Refac of qunithandler
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@20489 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/qbeta/test/runtest.bat
+++ MiniSystem/qbeta/test/runtest.bat
@@ -4,7 +4,7 @@ echo %BETALIB%
 set BL=%BETALIB%/MiniSystem/qbeta/test
 echo %BL%
 
-rm A_trace.txt B_trace.txt C_trace.txt trace.txt
+rm -v A_trace.txt B_trace.txt C_trace.txt trace.txt
 
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2 > B_list 
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3 > C_list
