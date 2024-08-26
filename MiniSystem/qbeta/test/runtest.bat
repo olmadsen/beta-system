1f45ccbf99464234444a1c2d3dd9793e92229d18
Ole Lehrmann Madsen <olm@alexandra.dk>
2024-08-26 07:41:57 +0000

    Added testrun4.bet
    git-svn-id: https://svn.nfit.au.dk/svn/beta/trunk@20686 678b6752-a8a0-49ff-a511-7c2a3241fcd4

--- MiniSystem/qbeta/test/runtest.bat
+++ MiniSystem/qbeta/test/runtest.bat
@@ -6,8 +6,9 @@ echo %BL%
 
 rm -v A_trace.txt B_trace.txt C_trace.txt trace.txt
 
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2
 cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3
+cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2
+cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun4
 
 C:/beta/r5.5/MiniSystem/qbeta/test/testrun1
 
@@ -15,6 +16,6 @@ C:/beta/r5.5/MiniSystem/qbeta/test/testrun1
 echo "Done"
 
 set /P V="wait:"
-cat A_trace.txt B_trace.txt C_trace.txt >trace.txt
+cat A_trace.txt B_trace.txt C_trace.txt D_trace.txt>trace.txt
 ls -lt
 
