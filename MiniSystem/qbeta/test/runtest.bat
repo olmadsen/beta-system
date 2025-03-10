2794c985c08c4bc027d0aef55507341264fd25dc
Ole Lehrmann Madsen <olm@cs.au.dk>
2025-03-10 07:45:37 +0100

    testrun1 ok for newI2F2I

--- MiniSystem/qbeta/test/runtest.bat
+++ MiniSystem/qbeta/test/runtest.bat
@@ -4,13 +4,15 @@ echo %BETALIB%
 set BL=%BETALIB%/MiniSystem/qbeta/test
 echo %BL%
 
+echo %1 %2 %3
+
 rm -v A_trace.txt B_trace.txt C_trace.txt trace.txt
 
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun4
+cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3 %1 %2 
+cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2 %1 %2
+cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun4 %1 %2
 
-C:/beta/r5.5/MiniSystem/qbeta/test/testrun1
+C:/beta/r5.5/MiniSystem/qbeta/test/testrun1 %1 %2
 
 
 echo "Done"
