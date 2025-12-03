851d6a597cff9e8dff7fc8319902f1998a467e12
Ole Lehrmann Madsen <olm@cs.au.dk>
2025-12-03 13:01:17 +0100

    Issues followimg rename of MiniSystem to qBetaSystem, etc

--- qBetaSystem/qbeta/test/runtest.bat
+++ qBetaSystem/qbeta/test/runtest.bat
@@ -1,17 +1,17 @@
 @ECHO OFF
 
 echo %BETALIB%
-set BL=%BETALIB%/MiniSystem/qbeta/test
+set BL=%BETALIB%/qBetaSystem/qbeta/test
 echo %BL%
 
 rm -v A_trace.txt B_trace.txt C_trace.txt D_trace.txt E_trace.txt trace.txt
 
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun3
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun2
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun4
-cygstart C:/beta/r5.5/MiniSystem/qbeta/test/testrun5
+cygstart C:/beta/r5.5/qBetaSystem/qbeta/test/testrun3
+cygstart C:/beta/r5.5/qBetaSystem/qbeta/test/testrun2
+cygstart C:/beta/r5.5/qBetaSystem/qbeta/test/testrun4
+cygstart C:/beta/r5.5/qBetaSystem/qbeta/test/testrun5
 
-C:/beta/r5.5/MiniSystem/qbeta/test/testrun1
+C:/beta/r5.5/qBetaSystem/qbeta/test/testrun1
 
 
 echo "Done"
