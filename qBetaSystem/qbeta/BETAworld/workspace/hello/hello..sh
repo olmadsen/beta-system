9901681ad7f288cebfb68bf4beea41e91eeb7858
Ole Lehrmann Madsen <olm@cs.au.dk>
2026-09-26 15:39:27 +0200

    hello.sh wirh Mac newmlines

--- qBetaSystem/qbeta/BETAworld/workspace/hello/hello..sh
+++ qBetaSystem/qbeta/BETAworld/workspace/hello/hello..sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 set -e
 BASE="$1"
@@ -14,3 +15,21 @@ ld -macos_version_min 26.0 \
    -lSystem \
    -syslibroot "$(xcrun --show-sdk-path)"
 echo "Built hello"
+=======
+#!/bin/bash
+set -e
+BASE="$1"
+echo $BETALIB
+BL="$BETALIB"
+QB="$BL"/qBetaSystem
+QBA="$QB"/VM/MAC-ARM
+echo "$QBA"
+as -g -arch arm64 -o hello.o hello..a
+ld -macos_version_min 26.0 \
+   -o hello\
+      hello.o \
+      "$QBA"/Mac_ARM_lib.o \
+   -lSystem \
+   -syslibroot "$(xcrun --show-sdk-path)"
+echo "Built hello"
+>>>>>>> 7d06fc147 (hello.sh wirh Mac newmlines)
