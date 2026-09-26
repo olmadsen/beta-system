d236fb523e158b44c989198b86040d539eeebe66
Ole Lehrmann Madsen <olm@cs.au.dk>
2026-09-26 15:48:18 +0200

    First shot of a .sh file

--- qBetaSystem/qbeta/BETAworld/workspace/hello/hello..sh
+++ qBetaSystem/qbeta/BETAworld/workspace/hello/hello..sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-set -e
-BASE="$1"
-echo $BETALIB
-BL="$BETALIB"
-QB="$BL"/qBetaSystem
-QBA="$QB"/VM/MAC-ARM
-echo "$QBA"
-as -g -arch arm64 -o hello.o hello..a  
-ld -macos_version_min 26.0 \
-   -o hello \
-      hello.o \
-      "$QBA"/Mac_ARM_lib.o \
-   -lSystem \
-   -syslibroot "$(xcrun --show-sdk-path)"
-echo "Built hello"
+#!/bin/bashset -eBASE="$1"echo $BETALIBBL="$BETALIB"QB="$BL"/qBetaSystemQBA="$QB"/VM/MAC-ARMecho "$QBA"as -g -arch arm64 -o hello.o hello..a  ld -macos_version_min 26.0 \   -o hello \      hello.o \      "$QBA"/Mac_ARM_lib.o \   -lSystem \   -syslibroot "$(xcrun --show-sdk-path)"echo "Built hello"
